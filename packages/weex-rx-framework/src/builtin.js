b2e071794aadfbf07f400de39fb1abaa4d3991ae
元彦 <yuanyan.cao@alibaba-inc.com>
2016-11-07 14:13:47 +0800

    A builtin config

---
+++ packages/weex-rx-framework/src/builtin.js
@@ -0,0 +1,5 @@
+export let BuiltinModulesFactory = {
+  '@universal/rx': require('universal-rx/dist/rx.factory'),
+  '@universal/env': require('universal-env/dist/env.factory'),
+  '@universal/transition': require('universal-transition/dist/transition.factory'),
+};
