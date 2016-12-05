d6f10a01b9e9eb0cdcd4f03ec05386adc34f5a15
元彦 <yuanyan.cao@alibaba-inc.com>
2016-12-05 15:03:34 +0800

    fix: use es6 class make toString property pass in freezed and strict mode

--- packages/runtime-shared/src/url-search-params.js
+++ packages/runtime-shared/src/url-search-params.js
@@ -1,6 +1,6 @@
 // https://github.com/WebReflection/url-search-params
 
-var
+const
   find = /[!'\(\)~]|%20|%00/g,
   plus = /\+/g,
   replace = {
@@ -27,36 +27,6 @@ function decode(str) {
   return decodeURIComponent(str.replace(plus, ' '));
 }
 
-function URLSearchParams(query) {
-  this[secret] = Object.create(null);
-  if (!query) return;
-  if (query.charAt(0) === '?') {
-    query = query.slice(1);
-  }
-  for (var
-    index, value,
-    pairs = (query || '').split('&'),
-    i = 0,
-    length = pairs.length; i < length; i++
-  ) {
-    value = pairs[i];
-    index = value.indexOf('=');
-    if (-1 < index) {
-      this.append(
-        decode(value.slice(0, index)),
-        decode(value.slice(index + 1))
-      );
-    } else if (value.length) {
-      this.append(
-        decode(value),
-        ''
-      );
-    }
-  }
-}
-
-var URLSearchParamsProto = URLSearchParams.prototype;
-
 function isIterable() {
   try {
     return !!Symbol.iterator;
@@ -65,139 +35,156 @@ function isIterable() {
   }
 }
 
-URLSearchParamsProto.append = function append(name, value) {
-  var dict = this[secret];
-  if (name in dict) {
-    dict[name].push('' + value);
-  } else {
-    dict[name] = ['' + value];
+class URLSearchParams {
+  constructor(query) {
+    this[secret] = Object.create(null);
+    if (!query) return;
+    if (query.charAt(0) === '?') {
+      query = query.slice(1);
+    }
+    for (var
+      index, value,
+      pairs = (query || '').split('&'),
+      i = 0,
+      length = pairs.length; i < length; i++
+    ) {
+      value = pairs[i];
+      index = value.indexOf('=');
+      if (-1 < index) {
+        this.append(
+          decode(value.slice(0, index)),
+          decode(value.slice(index + 1))
+        );
+      } else if (value.length) {
+        this.append(
+          decode(value),
+          ''
+        );
+      }
+    }
   }
-};
 
-URLSearchParamsProto.delete = function del(name) {
-  delete this[secret][name];
-};
+  append(name, value) {
+    var dict = this[secret];
+    if (name in dict) {
+      dict[name].push('' + value);
+    } else {
+      dict[name] = ['' + value];
+    }
+  }
 
-URLSearchParamsProto.get = function get(name) {
-  var dict = this[secret];
-  return name in dict ? dict[name][0] : null;
-};
+  delete(name) {
+    delete this[secret][name];
+  }
 
-URLSearchParamsProto.getAll = function getAll(name) {
-  var dict = this[secret];
-  return name in dict ? dict[name].slice(0) : [];
-};
+  get(name) {
+    var dict = this[secret];
+    return name in dict ? dict[name][0] : null;
+  }
 
-URLSearchParamsProto.has = function has(name) {
-  return name in this[secret];
-};
+  getAll(name) {
+    var dict = this[secret];
+    return name in dict ? dict[name].slice(0) : [];
+  }
 
-URLSearchParamsProto.set = function set(name, value) {
-  this[secret][name] = ['' + value];
-};
+  has(name) {
+    return name in this[secret];
+  }
 
-URLSearchParamsProto.forEach = function forEach(callback, thisArg) {
-  var dict = this[secret];
-  Object.getOwnPropertyNames(dict).forEach(function(name) {
-    dict[name].forEach(function(value) {
-      callback.call(thisArg, value, name, this);
+  set(name, value) {
+    this[secret][name] = ['' + value];
+  }
+
+  forEach(callback, thisArg) {
+    var dict = this[secret];
+    Object.getOwnPropertyNames(dict).forEach(function(name) {
+      dict[name].forEach(function(value) {
+        callback.call(thisArg, value, name, this);
+      }, this);
     }, this);
-  }, this);
-};
-
-URLSearchParamsProto.keys = function keys() {
-  var items = [];
-  this.forEach(function(value, name) {
-    items.push(name);
-  });
-  var iterator = {
-    next: function() {
-      var value = items.shift();
-      return {done: value === undefined, value: value};
-    }
-  };
-
-  if (iterable) {
-    iterator[Symbol.iterator] = function() {
-      return iterator;
-    };
   }
 
-  return iterator;
-};
-
-URLSearchParamsProto.values = function values() {
-  var items = [];
-  this.forEach(function(value) {
-    items.push(value);
-  });
-  var iterator = {
-    next: function() {
-      var value = items.shift();
-      return {done: value === undefined, value: value};
-    }
-  };
-
-  if (iterable) {
-    iterator[Symbol.iterator] = function() {
-      return iterator;
+  keys() {
+    var items = [];
+    this.forEach(function(value, name) {
+      items.push(name);
+    });
+    var iterator = {
+      next: function() {
+        var value = items.shift();
+        return {done: value === undefined, value: value};
+      }
     };
+
+    if (iterable) {
+      iterator[Symbol.iterator] = function() {
+        return iterator;
+      };
+    }
+
+    return iterator;
   }
 
-  return iterator;
-};
-
-URLSearchParamsProto.entries = function entries() {
-  var items = [];
-  this.forEach(function(value, name) {
-    items.push([name, value]);
-  });
-  var iterator = {
-    next: function() {
-      var value = items.shift();
-      return {done: value === undefined, value: value};
-    }
-  };
-
-  if (iterable) {
-    iterator[Symbol.iterator] = function() {
-      return iterator;
+  values() {
+    var items = [];
+    this.forEach(function(value) {
+      items.push(value);
+    });
+    var iterator = {
+      next: function() {
+        var value = items.shift();
+        return {done: value === undefined, value: value};
+      }
     };
+
+    if (iterable) {
+      iterator[Symbol.iterator] = function() {
+        return iterator;
+      };
+    }
+
+    return iterator;
   }
 
-  return iterator;
-};
+  entries() {
+    var items = [];
+    this.forEach(function(value, name) {
+      items.push([name, value]);
+    });
+    var iterator = {
+      next: function() {
+        var value = items.shift();
+        return {done: value === undefined, value: value};
+      }
+    };
 
-if (iterable) {
-  URLSearchParamsProto[Symbol.iterator] = URLSearchParamsProto.entries;
+    if (iterable) {
+      iterator[Symbol.iterator] = function() {
+        return iterator;
+      };
+    }
+
+    return iterator;
+  }
+
+  toString() {
+    var dict = this[secret], query = [], i, key, name, value;
+    for (key in dict) {
+      name = encode(key);
+      for (
+        i = 0,
+        value = dict[key];
+        i < value.length; i++
+      ) {
+        query.push(name + '=' + encode(value[i]));
+      }
+    }
+    return query.join('&');
+  }
 }
 
-/*
-URLSearchParamsProto.toBody = function() {
-  return new Blob(
-    [this.toString()],
-    {type: 'application/x-www-form-urlencoded'}
-  );
-};
-*/
-
-URLSearchParamsProto.toJSON = function toJSON() {
-  return {};
-};
-
-URLSearchParamsProto.toString = function toString() {
-  var dict = this[secret], query = [], i, key, name, value;
-  for (key in dict) {
-    name = encode(key);
-    for (
-      i = 0,
-      value = dict[key];
-      i < value.length; i++
-    ) {
-      query.push(name + '=' + encode(value[i]));
-    }
-  }
-  return query.join('&');
-};
+if (iterable) {
+  URLSearchParams.prototype[Symbol.iterator] = URLSearchParams.prototype.entries;
+}
 
 module.exports = URLSearchParams;
