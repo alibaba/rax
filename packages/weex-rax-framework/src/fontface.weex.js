c2c0b918efa04f43ce543b7207cae34524a44773
元彦 <yuanyan.cao@alibaba-inc.com>
2016-12-30 00:40:48 +0800

    feat: add FontFace W3C API

---
+++ packages/weex-rax-framework/src/fontface.weex.js
@@ -0,0 +1,9 @@
+module.exports = function(__weex_require__) {
+  // https://developer.mozilla.org/en-US/docs/Web/API/FontFace
+  function FontFace(family, source) {
+    this.family = family;
+    this.source = source;
+  }
+
+  return FontFace;
+};
