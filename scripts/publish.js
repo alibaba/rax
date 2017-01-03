a83de1f23a2b0c726166bdd20b0bd7dc5c99589b
元彦 <yuanyan.cao@alibaba-inc.com>
2017-01-03 19:35:47 +0800

    feat: update generator dependencies when publish

--- scripts/publish.js
+++ scripts/publish.js
@@ -10,11 +10,25 @@ const execSync = require('child_process').execSync;
 const spawnSync = require('child_process').spawnSync;
 const version = process.argv[2];
 
-const RAX_VERSION_FILE = 'packages/rax/src/version.js';
 
 if (version) {
+  const RAX_VERSION_FILE = 'packages/rax/src/version.js';
   console.log('Update rax version file to', version);
   fs.writeFileSync(RAX_VERSION_FILE, 'export default \'' + version + '\';\n');
+
+  const GENERATOR_DEPENDENCIES_FILE = 'packages/rax-cli/src/generator/templates/package.json';
+  console.log('Update dependencies in generator');
+  const JSONString = fs.readFileSync(GENERATOR_DEPENDENCIES_FILE);
+  const packageJSON = JSON.parse(JSONString);
+
+  const ver = "^" + version;
+  packageJSON.dependencies['rax'] = ver;
+  packageJSON.dependencies['rax-components'] = ver;
+  packageJSON.devDependencies['babel-preset-rax'] = ver;
+  packageJSON.devDependencies['rax-webpack-plugin'] = ver;
+  packageJSON.devDependencies['stylesheet-loader'] = ver;
+
+  fs.writeFileSync(GENERATOR_DEPENDENCIES_FILE, JSON.stringify(packageJSON, null, '  '));
 
   execSync(
     'npm run bootstrap && npm run build && npm run lint && npm run test &&' +
