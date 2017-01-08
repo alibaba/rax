2e7a397f6fe5b2e495a0175daadb345a80454ed4
水澜 <shuilan.cj@taobao.com>
2017-01-08 19:29:34 +0800

    test: add universal-jsonp test

---
+++ packages/universal-jsonp/src/__tests__/index.js
@@ -0,0 +1,33 @@
+import jsonp from '../index';
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 50000;
+
+// See http://doc.jsfiddle.net/use/echo.html
+const URL = 'http://jsfiddle.net/echo/jsonp/';
+
+describe('jsonp in web', () => {
+  it('should fetch jsonp data', (done) => {
+    jsonp(URL, {
+      timeout: 50000
+    }).then(function(response) {
+      return response.json();
+    }).then(function(json) {
+      expect(json).not.toBeNull();
+      done();
+    });
+  });
+
+  it('should fail when time out', (done) => {
+    jsonp(URL, {
+      timeout: 50
+    }).then(function(response) {
+      return response.json();
+    }).then((json) => {
+      expect(json).not.toBeNull();
+      done();
+    }).catch(function(ex) {
+      expect(ex.message).toEqual(`JSONP request to ${URL}? timed out`);
+      done();
+    });
+  });
+});
