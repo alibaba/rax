3726ca7bde9d5a2515c31d270ad82ed70eaabe8c
元彦 <yuanyan.cao@alibaba-inc.com>
2017-01-13 01:28:26 +0800

    test: remove unused

--- packages/rax-webpack-plugin/src/__tests__/index.js
+++ packages/rax-webpack-plugin/src/__tests__/index.js
@@ -4,29 +4,15 @@ import path from 'path';
 import rimraf from 'rimraf';
 import webpack from 'webpack';
 
-function readFile(filepath) {
-  try {
-    return fs.readFileSync(filepath, 'utf-8').trim();
-  } catch (e) {
-    return null;
-  }
-}
-
-const fixturesDirectory = path.join(__dirname, 'fixtures');
-
-const fixtures = fs.readdirSync(fixturesDirectory)
-  .filter(function(fixture) {
-    if (fixture.indexOf('.') == 0) {
-      return false;
-    }
-    if (fs.lstatSync(path.resolve(fixturesDirectory, fixture)).isDirectory()) {
-      return true;
-    }
-    return false;
-  });
-
-
 describe('rax-webpack-plugin', function() {
+
+  function readFile(filepath) {
+    try {
+      return fs.readFileSync(filepath, 'utf-8').trim();
+    } catch (e) {
+      return null;
+    }
+  }
 
   function runWebpack(done, fixture, callback) {
     const outputDirectory = path.join(os.tmpdir(), '__output__');
