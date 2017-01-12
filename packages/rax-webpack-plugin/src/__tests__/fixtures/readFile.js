9042b755139704d0ed5356f8590b1869b11357f5
元彦 <yuanyan.cao@alibaba-inc.com>
2017-01-13 01:25:42 +0800

    test: rework rax-webpack-plugin tests

--- packages/rax-webpack-plugin/src/__tests__/fixtures/readFile.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-
-export default function readFile(filepath) {
-  try {
-    return fs.readFileSync(filepath, 'utf-8').trim();
-  } catch (e) {
-    return null;
-  }
-}
--- packages/rax-webpack-plugin/src/__tests__/index.js
+++ packages/rax-webpack-plugin/src/__tests__/index.js
@@ -4,6 +4,14 @@ import path from 'path';
 import rimraf from 'rimraf';
 import webpack from 'webpack';
 
+function readFile(filepath) {
+  try {
+    return fs.readFileSync(filepath, 'utf-8').trim();
+  } catch (e) {
+    return null;
+  }
+}
+
 const fixturesDirectory = path.join(__dirname, 'fixtures');
 
 const fixtures = fs.readdirSync(fixturesDirectory)
@@ -17,56 +25,142 @@ const fixtures = fs.readdirSync(fixturesDirectory)
     return false;
   });
 
-let outputDirectory = path.join(os.tmpdir(), '__output__');
 
 describe('rax-webpack-plugin', function() {
+
+  function runWebpack(done, fixture, callback) {
+    const outputDirectory = path.join(os.tmpdir(), '__output__');
+    const testDirectory = path.join(__dirname, 'fixtures', fixture);
+    const outputPath = path.join(outputDirectory, fixture);
+    const configFile = path.join(testDirectory, 'webpack.config.js');
+
+    let options;
+
+    if (fs.existsSync(configFile)) {
+      options = require(configFile);
+    } else {
+      done.fail(`fixture '${fixture}' directory not exist webpack.config.js`);
+    }
+
+    options.context = testDirectory;
+
+    if (!options.output) {
+      options.output = {
+        filename: '[name].js'
+      };
+    }
+
+    if (!options.output.path) {
+      options.output.path = outputPath;
+    }
+
+    const expectedDirectory = path.join(testDirectory, 'expected');
+
+    webpack(options, function(err, stats) {
+      expect(err).toBeNull();
+      expect(stats.hasErrors()).toBe(false);
+
+      fs.readdirSync(expectedDirectory).forEach(function(file) {
+        const filePath = path.join(expectedDirectory, file);
+        const actualPath = path.join(outputPath, file);
+        callback(actualPath, filePath);
+      });
+
+      done();
+    });
+  }
+
   afterAll(function(done) {
     rimraf(outputDirectory, function() {
       done();
     });
   });
 
-  fixtures.forEach(function(fixture) {
-    it(fixture, function(done) {
-      const testDirectory = path.join(__dirname, 'fixtures', fixture);
-      let outputPath = path.join(outputDirectory, fixture);
+  it('bundle', function(done){
+    runWebpack(done, 'bundle', function(actualPath, filePath) {
+      const actual = readFile(actualPath);
+      const expected = readFile(filePath);
 
-      const configFile = path.join(testDirectory, 'webpack.config.js');
-      let options;
-
-      if (fs.existsSync(configFile)) {
-        options = require(configFile);
-      } else {
-        done.fail(`fixture '${fixture}' directory not exist webpack.config.js`);
-      }
-
-      options.context = testDirectory;
-
-      if (!options.output) {
-        options.output = {
-          filename: '[name].js'
-        };
-      };
-      if (!options.output.path) {
-        options.output.path = outputPath;
-      };
-
-      webpack(options, function(err, stats) {
-        expect(err).toBeNull();
-        expect(stats.hasErrors()).toBe(false);
-
-        const expectedDirectory = path.join(testDirectory, 'expected');
-        const fixtureTestSuiteFile = path.join(testDirectory, 'test.js');
-        fs.readdirSync(expectedDirectory).forEach(function(file) {
-          const filePath = path.join(expectedDirectory, file);
-          const actualPath = path.join(outputPath, file);
-
-          if (fs.existsSync(fixtureTestSuiteFile)) {
-            require(fixtureTestSuiteFile)(actualPath, filePath);
-          }
-        });
-        done();
-      });
+      expect(actual).toBe(expected);
+      expect(actual).toMatch('// {"framework" : "Rax"}');
     });
   });
+
+  it('bundle-compatible', function(done){
+    runWebpack(done, 'bundle-compatible', function(actualPath, filePath) {
+      const actual = readFile(actualPath);
+      const expected = readFile(filePath);
+
+      expect(actual).toBe(expected);
+      expect(actual).toMatch('// {"framework" : "Rax"}');
+    });
+  });
+
+  it('factory', function(done){
+    runWebpack(done, 'factory', function(actualPath, filePath) {
+      const actual = readFile(actualPath);
+      const expected = readFile(filePath);
+
+      expect(actual).toBe(expected);
+      expect(actual).toMatch(/^module.exports = /);
+    });
+  });
+
+  it('factory', function(done){
+    runWebpack(done, 'factory', function(actualPath, filePath) {
+      const actual = readFile(actualPath);
+      const expected = readFile(filePath);
+
+      expect(actual).toBe(expected);
+      expect(actual).toMatch(/^module.exports = /);
+    });
+  });
+
+  it('factory-factoryGlobals', function(done){
+    runWebpack(done, 'factory-factoryGlobals', function(actualPath, filePath) {
+      const actual = readFile(actualPath);
+      const expected = readFile(filePath);
+
+      expect(actual).toBe(expected);
+      expect(actual).toMatch(/^module.exports = /);
+    });
+  });
+
+  it('function', function(done){
+    runWebpack(done, 'function', function(actualPath, filePath) {
+      const actual = readFile(actualPath);
+      const expected = readFile(filePath);
+
+      expect(actual).toBe(expected);
+    });
+  });
+
+  it('function', function(done){
+    runWebpack(done, 'function', function(actualPath, filePath) {
+      const actual = readFile(actualPath);
+      const expected = readFile(filePath);
+
+      expect(actual).toBe(expected);
+    });
+  });
+
+  it('module', function(done){
+    runWebpack(done, 'module', function(actualPath, filePath) {
+      const actual = readFile(actualPath);
+      const expected = readFile(filePath);
+
+      expect(actual).toBe(expected);
+      expect(actual).toMatch(/^module\.exports = /);
+    });
+  });
+
+  it('umd', function(done){
+    runWebpack(done, 'umd', function(actualPath, filePath) {
+      const actual = readFile(actualPath);
+      const expected = readFile(filePath);
+
+      expect(actual).toBe(expected);
+    });
+  });
+
 });
