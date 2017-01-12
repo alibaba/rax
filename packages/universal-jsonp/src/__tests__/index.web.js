08b6e8fdde1a66d6e354fc76fe3961a74b97076f
水澜 <shuilan.cj@taobao.com>
2017-01-12 19:26:08 +0800

    fix: jsonp test timeout

--- packages/universal-jsonp/src/__tests__/index.web.js
+++ packages/universal-jsonp/src/__tests__/index.web.js
@@ -1,20 +1,24 @@
 import jsonp from '../index';
 
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000;
+const mockReturnedObject = {
+  a: 1
+};
 
-// See http://doc.jsfiddle.net/use/echo.html
-const URL = 'http://jsfiddle.net/echo/jsonp/';
+const URL = 'http://XXX/jsonp/';
 
 describe('jsonp in web', () => {
+
   it('should fetch jsonp data', (done) => {
     jsonp(URL, {
-      timeout: 10000
+      jsonpCallbackFunctionName: 'jsonpCb'
     }).then(function(response) {
       return response.json();
     }).then(function(json) {
-      expect(json).not.toBeNull();
+      expect(json).toEqual(mockReturnedObject);
       done();
     });
+
+    window.jsonpCb(mockReturnedObject);
   });
 
   it('should fail when time out', (done) => {
