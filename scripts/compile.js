da09e0ecf92cc22b8d23a844bfec22f46d0646b8
大果 <liuxiong.lx@alibaba-inc.com>
2017-01-16 14:00:09 +0800

    chore: remove --files option when run compile

--- scripts/compile.js
+++ scripts/compile.js
@@ -6,9 +6,7 @@
  *
  * Example:
  *  compile all packages: node ./scripts/compile.js
- *  compile some packages: node ./scripts/compile.js --packages rax,rax-cli
- *  compile some files: node ./scripts/compile.js --files ./packages/rax/src/index.js,./packages/rax/src/elemnent.js
- *  watch compile: compile some packages: node ./scripts/compile.js --packages rax --watch
+ *  watch compile some packages: node ./scripts/compile.js --watch --packages rax,rax-cli
  */
 'use strict';
 
@@ -115,44 +113,26 @@ function buildFile(file, silent) {
 }
 
 if (args.watch) {
-  // watch files change
-  if (args.files) {
-    const files = args.files.split(',');
+  // watch packages
+  const packages = getPackages(customPackages);
+  const watchPackagesDir = packages.map(dir => path.resolve(dir, 'src'));
 
-    chalk.green(console.log('watch files compile', files));
+  console.log(chalk.green('watch packages compile', packages));
 
-    chokidar.watch(files).on('change', (event, filePath) => {
-      buildFile(path.resolve(__dirname, '../', filePath), false);
-    });
-  } else {
-    // watch packages
-    const packages = getPackages(customPackages);
-    const watchPackagesDir = packages.map(dir => path.resolve(dir, 'src'));
-
-    console.log(chalk.green('watch packages compile', packages));
-
-    chokidar.watch(watchPackagesDir, {
-      ignored: IGNORE_PATTERN
-    }).on('all', (event, filePath) => {
-      if (event !== 'change') {
-        return;
-      }
-      const packageName = filePath.match(/rax\/packages\/([^\/]*)/)[1];
-      const packagePath = path.resolve(__dirname, '../packages/', packageName);
-      process.stdout.write(chalk.bold.inverse(`Compiling package ${packageName} \n`));
-      buildPackage(packagePath);
-      process.stdout.write('\n');
-    });
-  }
-} else {
-  if (args.files) {
-    const files = args.files.split(',');
-    files.forEach(file => {
-      buildFile(path.resolve(file), false);
-    });
-  } else {
-    process.stdout.write(chalk.bold.inverse('Compiling packages\n'));
-    getPackages(customPackages).forEach(buildPackage);
+  chokidar.watch(watchPackagesDir, {
+    ignored: IGNORE_PATTERN
+  }).on('all', (event, filePath) => {
+    if (event !== 'change') {
+      return;
+    }
+    const packageName = filePath.match(/rax\/packages\/([^\/]*)/)[1];
+    const packagePath = path.resolve(__dirname, '../packages/', packageName);
+    process.stdout.write(chalk.bold.inverse(`Compiling package ${packageName} \n`));
+    buildPackage(packagePath);
     process.stdout.write('\n');
-  }
+  });
+} else {
+  process.stdout.write(chalk.bold.inverse('Compiling packages\n'));
+  getPackages(customPackages).forEach(buildPackage);
+  process.stdout.write('\n');
 }
