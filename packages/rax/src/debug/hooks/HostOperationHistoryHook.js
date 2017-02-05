b48a7fb157a15ea242f7aea94a2687497c182afb
boiawang <zhengwang2314@gmail.com>
2017-02-05 23:50:53 +0800

    feat: delete useless comment

--- packages/rax/src/debug/hooks/HostOperationHistoryHook.js
+++ packages/rax/src/debug/hooks/HostOperationHistoryHook.js
@@ -1,17 +1,5 @@
 let DebugID = 1;
 
-// export Operation = {instanceID: DebugID} & (
-//   {type: 'mount', payload: string} |
-//   {type: 'insert child', payload: {toIndex: number, content: string}} |
-//   {type: 'move child', payload: {fromIndex: number, toIndex: number}} |
-//   {type: 'replace children', payload: string} |
-//   {type: 'replace text', payload: string} |
-//   {type: 'replace with', payload: string} |
-//   {type: 'update styles', payload: mixed /* Style Object */} |
-//   {type: 'update attribute', payload: {[name: string]: string}} |
-//   {type: 'remove attribute', payload: string}
-// );
-
 let HostOperationHistoryHook = null;
 
 let history = [];
