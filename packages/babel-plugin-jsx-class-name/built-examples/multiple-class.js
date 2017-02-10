cdc9ed544281a15aadf62b22296d5e0f1ed89ac6
boiawang <zhengwang2314@gmail.com>
2017-02-10 13:56:38 +0800

    fix: remove build-examples

--- packages/babel-plugin-jsx-class-name/built-examples/multiple-class.js
+++
@@ -1,8 +0,0 @@
-import class_name_styles1 from 'foo.css';
-
-class App extends Component {
-  render() {
-    return <div style={[class_name_styles.foo1, class_name_styles.foo2]} />;
-  }
-}
-let class_name_styles = class_name_styles1;
