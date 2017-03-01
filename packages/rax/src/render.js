0283ab16ad14cd2088130a1a0269e62a5a1ffd4c
boiawang <zhengwang2314@gmail.com>
2017-03-01 10:47:13 +0800

    fix: add options default value

--- packages/rax/src/render.js
+++ packages/rax/src/render.js
@@ -4,7 +4,7 @@ import {setRem} from './style/unit';
 import {injectDriver, setDriver} from './driver';
 import Hook from './debug/hook';
 
-function render(element, container, options, callback) {
+function render(element, container, options = {}, callback) {
   // Compatible with `render(element, container, callback)`
   if (typeof options === 'function') {
     callback = options;
