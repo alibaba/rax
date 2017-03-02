7f0e62dc329bb4211773739631926302b7412ef7
亚城 <yacheng.sz@taobao.com>
2017-03-02 10:45:24 +0800

    feat: update

---
+++ packages/rax-gotop/src/index.js
@@ -0,0 +1,205 @@
+/** @jsx createElement */
+import {createElement, Component, findDOMNode, render} from 'rax';
+import View from 'rax-view';
+import Text from 'rax-text';
+import Image from 'rax-image';
+import {isWeex} from 'universal-env';
+import Animated from 'rax-animated';
+
+let scroll;
+
+if (!isWeex) {
+  var raf = window.requestAnimationFrame || window.webkitRequestAnimationFrame || function(c) {
+    setTimeout(c, 1 / 60 * 1e3);
+  };
+  scroll = require('./scroll');
+}
+
+export default class GoTop extends Component {
+  constructor(props) {
+    super(props);
+    let isShow = props.resident ? true : false,
+      bottom = props.bottom || 125;
+
+    if (isWeex) {
+      bottom += 'rem';
+    } else {
+      if (!props.resident) {
+        isShow = true;
+        bottom = new Animated.Value(-100);
+      }
+    }
+    this.state = {
+      isShow: isShow,
+      bottom: bottom
+    };
+  }
+
+  render() {
+    let topNode = '',
+      props = this.props,
+      Container;
+
+    if (isWeex) {
+      topNode = <View id="rx-top" style={styles.topNode} />;
+      Container = View;
+    } else {
+      Container = Animated.View;
+    }
+    return <View>
+      {topNode}
+      <View>
+        <View style={styles.hideNode} onAppear={() => {
+          console.log('appear');
+          if (props.resident) {
+            return;
+          }
+          if (isWeex) {
+            this.state.isShow && this.props.onHide();
+            this.setState({
+              isShow: false
+            });
+          } else {
+            this.hide();
+          }
+        }} onDisAppear={() => {
+          console.log('disappear');
+          if (props.resident) {
+            return;
+          }
+          if (isWeex) {
+            !this.state.isShow && this.props.onShow();
+            this.setState({
+              isShow: true
+            });
+          } else {
+            this.show();
+          }
+        }} />
+      </View>
+      {(() => {
+        if (this.state.isShow) {
+          return <Container style={[styles.container, {
+            bottom: this.state.bottom,
+            width: props.iconWidth,
+            height: props.iconHeight,
+            borderColor: props.borderColor
+          }]} onClick={this.click.bind(this)}>
+            <Image style={{width: '30rem', height: '32.5rem', marginBottom: '4.5rem'}} source={{uri: props.icon}} />
+            <View>
+              <Text style={styles.text}>{props.name}</Text>
+            </View>
+          </Container>;
+        }
+      })()}
+    </View>;
+  }
+
+  click(event) {
+    const {onTop, onPress} = this.props;
+
+    onPress && onPress();
+    if (isWeex) {
+      let dom = require('@weex-module/dom');     
+      let top = findDOMNode('rx-top');
+
+      dom.scrollToElement(top.ref, {
+        offset: 0
+      });
+    } else {
+      event.preventDefault();
+      scroll.scrollToTop({
+        duration: 300
+      });
+    }
+    onTop && onTop();
+  }
+
+  show() {
+    this.startAnimation('show');
+    !this.state.isShowFlag && this.props.onShow();
+    this.setState({
+      isShowFlag: true
+    });
+  }
+
+  hide() {
+    this.startAnimation('hide');
+    this.state.isShowFlag && this.props.onHide();
+    this.setState({
+      isShowFlag: false
+    });
+  }
+
+  startAnimation(type) {
+    let bottom = this.props.bottom || 60;
+    if (!isWeex) {
+      let initValue, toValue;
+      if (type === 'show') {
+        initValue = -100;
+        toValue = bottom;
+
+        if (this.state.isShowFlag) {
+          return;
+        }
+      } else {
+        initValue = bottom;
+        toValue = -100;
+        if (!this.state.isShowFlag) {
+          return;
+        }
+      }
+      this.state.bottom.setValue(initValue);
+      Animated.timing(this.state.bottom, {
+        toValue: toValue,
+        during: 300
+      }).start(() => {
+        if (type === 'hide') {
+        }
+      });
+    }
+  }
+}
+
+let styles = {
+  container: {
+    position: 'fixed',
+    right: '25rem',
+    zIndex: 1001,
+    borderRadius: '45rem',
+    backgroundColor: 'rgba(255, 255, 255, .9)',
+    borderWidth: '2rem',
+    borderStyle: 'solid',
+    color: '#bbb',
+    alignItems: 'center',
+    justifyContent: 'center',
+    lineHeight: 1
+  },
+
+  text: {
+    color: '#5f646e',
+    fontSize: '20rem'
+  },
+  icon: {
+    width: '30',
+    height: '32.5rem',
+    marginBottom: '4.5rem'
+  },
+
+  hideNode: {
+    width: '1rem',
+    height: '1rem',
+    position: 'absolute',
+    top: 0
+  }
+};
+
+GoTop.defaultProps = {
+  name: '顶部',
+  icon: 'https://gw.alicdn.com/tps/TB1UhIvLXXXXXaRXpXXXXXXXXXX-60-66.png',
+  borderColor: 'rgba(0, 0, 0, 0.1)',
+  iconWidth: '90rem',
+  iconHeight: '90rem',
+  onShow: () => {},
+  onHide: () => {}
+};
---
+++ packages/rax-gotop/src/scroll.js
@@ -0,0 +1,160 @@
+import smooth from './smooth';
+import cancelEvents from './cancel-events';
+import events from './scroll-events';
+
+const easing = smooth.defaultEasing;
+const assign = Object.assign;
+
+/*
+ * Function helper
+ */
+const functionWrapper = (value) => {
+  return typeof value === 'function' ? value : () => {
+    return value;
+  };
+};
+
+/*
+ * Sets the cancel trigger
+ */
+
+cancelEvents.register(() => {
+  __cancel = true;
+});
+
+/*
+ * Wraps window properties to allow server side rendering
+ */
+const currentWindowProperties = () => {
+  if (typeof window !== 'undefined') {
+    return window.requestAnimationFrame || window.webkitRequestAnimationFrame;
+  }
+};
+
+/*
+ * Helper function to never extend 60fps on the webpage.
+ */
+const requestAnimationFrameHelper = (() => {
+  return currentWindowProperties() ||
+    function(callback, element, delay) {
+      window.setTimeout(callback, delay || (1000 / 60), new Date().getTime());
+    };
+})();
+
+
+let __currentPositionY = 0;
+let __startPositionY = 0;
+let __targetPositionY = 0;
+let __progress = 0;
+let __duration = 0;
+let __cancel = false;
+
+let __target;
+let __to;
+let __start;
+let __deltaTop;
+let __percent;
+let __delayTimeout;
+
+
+const currentPositionY = () => {
+  let supportPageOffset = window.pageXOffset !== undefined;
+  let isCSS1Compat = ((document.compatMode || '') === 'CSS1Compat');
+  return supportPageOffset ? window.pageYOffset : isCSS1Compat ?
+         document.documentElement.scrollTop : document.body.scrollTop;
+};
+
+const pageHeight = () => {
+  let body = document.body;
+  let html = document.documentElement;
+
+  return Math.max(
+      body.scrollHeight,
+      body.offsetHeight,
+      html.clientHeight,
+      html.scrollHeight,
+      html.offsetHeight
+  );
+};
+
+const animateTopScroll = (timestamp) => {
+  // Cancel on specific events
+  if (__cancel) {
+    return;
+  }
+
+  __deltaTop = Math.round(__targetPositionY - __startPositionY);
+
+  if (__start === null) {
+    __start = timestamp;
+  }
+
+  __progress = timestamp - __start;
+
+  __percent = (__progress >= __duration ? 1 : easing(__progress / __duration));
+
+  __currentPositionY = __startPositionY + Math.ceil(__deltaTop * __percent);
+
+  window.scrollTo(0, __currentPositionY);
+
+  if (__percent < 1) {
+    requestAnimationFrameHelper.call(window, animateTopScroll);
+    return;
+  }
+
+  if (events.registered.end) {
+    events.registered.end(__to, __target, __currentPositionY);
+  }
+
+};
+
+const startAnimateTopScroll = (y, options, to, target) => {
+
+
+  window.clearTimeout(__delayTimeout);
+
+  __start = null;
+  __cancel = false;
+  __startPositionY = currentPositionY();
+  __targetPositionY = options.absolute ? y : y + __startPositionY;
+  __deltaTop = Math.round(__targetPositionY - __startPositionY);
+
+  __duration = functionWrapper(options.duration)(__deltaTop);
+  __duration = isNaN(parseFloat(__duration)) ? 1000 : parseFloat(__duration);
+  __to = to;
+  __target = target;
+
+  if (options && options.delay > 0) {
+    __delayTimeout = window.setTimeout(function animate() {
+      requestAnimationFrameHelper.call(window, animateTopScroll);
+    }, options.delay);
+    return;
+  }
+
+  requestAnimationFrameHelper.call(window, animateTopScroll);
+
+};
+
+const scrollToTop = (options) => {
+  startAnimateTopScroll(0, assign(options || {}, { absolute: true }));
+};
+
+const scrollTo = (toY, options) => {
+  startAnimateTopScroll(toY, assign(options || {}, { absolute: true }));
+};
+
+const scrollToBottom = (options) => {
+  startAnimateTopScroll(pageHeight(), assign(options || {}, { absolute: true }));
+};
+
+const scrollMore = (toY, options) => {
+  startAnimateTopScroll(currentPositionY() + toY, assign(options || {}, { absolute: true }));
+};
+
+export default {
+  animateTopScroll: startAnimateTopScroll,
+  scrollToTop: scrollToTop,
+  scrollToBottom: scrollToBottom,
+  scrollTo: scrollTo,
+  scrollMore: scrollMore,
+};
