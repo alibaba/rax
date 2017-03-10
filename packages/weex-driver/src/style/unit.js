1cdd6951aaf4cd4f0ee2f7aad5ae39e5fcd12f1d
liuxiong.lx <liuxiong.lx@alibaba-inc.com>
2017-03-10 18:09:16 +0800

    feat: split style

---
+++ packages/weex-driver/src/style/unit.js
@@ -0,0 +1,73 @@
+import {getRem} from 'rax';
+
+/**
+ * CSS properties which accept numbers but are not in units of "px".
+ */
+const UNITLESS_NUMBER_PROPS = {
+  animationIterationCount: true,
+  borderImageOutset: true,
+  borderImageSlice: true,
+  borderImageWidth: true,
+  boxFlex: true,
+  boxFlexGroup: true,
+  boxOrdinalGroup: true,
+  columnCount: true,
+  flex: true,
+  flexGrow: true,
+  flexPositive: true,
+  flexShrink: true,
+  flexNegative: true,
+  flexOrder: true,
+  gridRow: true,
+  gridColumn: true,
+  fontWeight: true,
+  lineClamp: true,
+  // We make lineHeight default is px that is diff with w3c spec
+  // lineHeight: true,
+  opacity: true,
+  order: true,
+  orphans: true,
+  tabSize: true,
+  widows: true,
+  zIndex: true,
+  zoom: true,
+  // Weex only
+  lines: true,
+};
+const SUFFIX = 'rem';
+const REM_REG = /[-+]?\d*\.?\d+rem/g;
+
+/**
+ * Is string contains rem
+ * @param {String} str
+ * @returns {Boolean}
+ */
+function isRem(str) {
+  return typeof str === 'string' && str.indexOf(SUFFIX) !== -1;
+}
+
+/**
+ * Calculate rem to pixels: '1.2rem' => 1.2 * rem
+ * @param {String} str
+ * @param {Number} rem
+ * @returns {number}
+ */
+function calcRem(str, rem = getRem()) {
+  return str.replace(REM_REG, function(remValue) {
+    return parseFloat(remValue) * rem + 'px';
+  });
+}
+
+function isUnitNumber(val, prop) {
+  return typeof val === 'number' && !UNITLESS_NUMBER_PROPS[prop];
+}
+
+export function convertUnit(val, prop) {
+  if (prop && isUnitNumber(val, prop)) {
+    return val * getRem() + 'px';
+  } else if (isRem(val)) {
+    return calcRem(val);
+  }
+
+  return val;
+}
