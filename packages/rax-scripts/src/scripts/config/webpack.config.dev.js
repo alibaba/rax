bbc21d9e1fa3a9ebd5c3f850a1a129d5b74ffd1c
宝码 <nongyoubao@alibaba-inc.com>
2017-03-27 13:22:02 +0800

    feat: webpack.config.base && default port 9999

--- packages/rax-scripts/src/scripts/config/webpack.config.dev.js
+++ packages/rax-scripts/src/scripts/config/webpack.config.dev.js
@@ -1,24 +1,51 @@
 'use strict';
-
-const webpackConfig = require('./webpack.config.prod');
+/* eslint no-console: 0 */
+const URL = require('url').URL;
+const address = require('address');
+const qrcode = require('qrcode-terminal');
 const webpack = require('webpack');
 
+const paths = require('./paths');
+const webpackConfigBase = require('./webpack.config.base');
+
+const webpackConfigDev = Object.assign({}, webpackConfigBase);
+
+const ipv4 = address.ip();
+const port = 8080;
+const webUrl = 'http://' + ipv4 + ':' + port;
+const bundleUrl = 'http://' + ipv4 + ':' + port + '/js/index.bundle.js';
+const weexBundleUrl = new URL(bundleUrl);
+
+weexBundleUrl.searchParams.append('wh_weex', true);
+weexBundleUrl.searchParams.append('wh_native', true);
+weexBundleUrl.searchParams.append('_wx_tpl', bundleUrl);
+
+qrcode.generate(webUrl, { small: true });
+console.log('Web: scan above QRCode ' + webUrl + ' or direct open in browser.\n');
+
+qrcode.generate(weexBundleUrl.toString(), { small: true });
+console.log('Weex: scan above QRCode ' + weexBundleUrl.toString() + ' use weex playground.\n');
+
 // enable source map
-webpackConfig.devtool = 'inline-module-source-map';
+webpackConfigDev.devtool = 'inline-module-source-map';
+webpackConfigDev.entry = {
+  'index.bundle': [paths.appIndexJs]
+};
 
 // enable entry point module hot accept.
 const webpackHotDevClient = require.resolve('react-dev-utils/webpackHotDevClient');
 
-Object.keys(webpackConfig.entry).forEach(point => {
-  webpackConfig.entry[point].unshift(webpackHotDevClient);
+Object.keys(webpackConfigDev.entry).forEach(point => {
+  webpackConfigDev.entry[point].unshift(webpackHotDevClient);
 });
 
-webpackConfig.module.loaders.forEach(loader => {
+webpackConfigDev.module.loaders.forEach(loader => {
   if (loader.test.toString() === /\.jsx?$/.toString()) {
     loader.loaders.push(require.resolve('../loaders/module-hot-accept'));
   }
 });
 
-webpackConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
+webpackConfigDev.plugins.push(new webpack.HotModuleReplacementPlugin());
+webpackConfigDev.plugins.push(new webpack.NoErrorsPlugin());
 
-module.exports = webpackConfig;
+module.exports = webpackConfigDev;
--- packages/rax-scripts/src/scripts/utils/parse-options.js
+++ packages/rax-scripts/src/scripts/utils/parse-options.js
@@ -9,7 +9,7 @@ const address = require('address');
  */
 const protocol = process.env.HTTPS === 'true' ? 'https:' : 'http:';
 const host = process.env.HOST || address.ip();
-const port = parseInt(process.env.PORT, 10) || 8080;
+const port = parseInt(process.env.PORT, 10) || 9999;
 
 module.exports = {
   host: host,
