46fdbe0ac2e084ab6f0d79d1d23dd12efab994cf
水澜 <shuilan.cj@taobao.com>
2017-03-27 13:50:46 +0800

    chore: fix examples for icon & event

--- examples/modules/FontFaceDemo.js
+++ examples/modules/FontFaceDemo.js
@@ -8,14 +8,16 @@ import {
 
 class FontFaceDemo extends Component {
   componentDidMount() {
-    var testFontFace = new FontFace('Test', 'url(//at.alicdn.com/t/font_kp254mlq4n9daemi.woff)');
+    var testFontFace = new FontFace('Test', "url('//at.alicdn.com/t/font_lf2urtr9li110pb9.ttf')");
     document.fonts.add(testFontFace);
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.icon}>&#xe600;</Text>
+        <Text style={styles.icon}>{'\uE9f2'}</Text>
+        <Text style={styles.icon}>{'\uE65B'}</Text>
+        <Text style={styles.icon}>{'\uE782'}</Text>
       </View>
     );
   }
