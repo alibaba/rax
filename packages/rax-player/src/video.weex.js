ee8f525dec2317bc5c254ebe197759f081d3b6aa
元彦 <yuanyan.cao@alibaba-inc.com>
2017-04-06 22:44:23 +0800

    fix: use videoplus only in tb client

--- packages/rax-player/src/video.weex.js
+++ packages/rax-player/src/video.weex.js
@@ -5,10 +5,10 @@ import Image from 'rax-image';
 
 let supportVideoPlus = false;
 const weexEnv = typeof WXEnvironment !== 'undefined' ? WXEnvironment : {};
-if (weexEnv.appVersion) {
+// TODO: rework by feature recognition
+if (weexEnv.appName === 'TB' && weexEnv.appVersion) {
   let appVersion = weexEnv.appVersion.split('.');
-  // 版本判断，当android和ios大于等于6.2.0时使用WeexVideoPlus标签
-  if (appVersion[0] > 6 || appVersion[0] == 6 && appVersion[1] >= 2) {
+  if (appVersion[0] >= 6 && appVersion[1] >= 2) {
     supportVideoPlus = true;
   }
 }
@@ -69,7 +69,7 @@ class Video extends Component {
 
   switch = (status) => {
     this.setState({
-      pause: status === 'stop' ? true : false,
+      pause: status === 'stop',
       update: true
     });
   }
@@ -108,14 +108,11 @@ class Video extends Component {
   };
 
   render() {
-    let NativeVideo = 'video';
-    // styles
+    let VideoComponent = 'video';
     let styles = this.calculateStyle();
-    // config
     let poster = this.props.poster || this.props.coverImage;
     let playStatus = this.state.pause ? 'stop' : 'play';
-    let videoSrc = this.props.src;
-    videoSrc = videoSrc.replace(/\/\/|http:\/\/|https:\/\//, location.protocol + '//'); // 解决android下播放器不能播放//前缀视频文件的bug
+    let videoSrc = this.props.src.replace(/\/\/|http:\/\/|https:\/\//, location.protocol + '//');
     let props = {
       ...this.props,
       ...{
@@ -132,16 +129,11 @@ class Video extends Component {
     };
 
     if (supportVideoPlus) {
-      NativeVideo = 'videoplus';
-      let utParams = this.props.utParams || {};
-      if (!utParams.page) {
-        utParams.page = 'RX';
-      }
+      VideoComponent = 'videoplus';
       props = {
         ...this.props,
         ...{
           src: videoSrc,
-          utParams: utParams,
           autoPlay: true, // 因为videoplus现在有个bug，当autoPlay不为true的时候埋点会有问题，所有，当视频开始播放之后就设置autoPlay为true
           onPaused: this.onVideoPause,
           onPlaying: this.onVideoPlay,
@@ -158,7 +150,7 @@ class Video extends Component {
       {
         poster && playStatus == 'stop' ?
           null :
-          <NativeVideo
+          <VideoComponent
             {...props}
           />
       }
@@ -209,14 +201,14 @@ class Video extends Component {
       ...defaultStyles.startBtn,
       ...{
         width: styles.video.width,
-        height: parseInt(styles.video.height) - 75 + 'rem'
+        height: parseInt(styles.video.height) - 75
       }
     };
     styles.startBtnImage = {
       ...defaultStyles.startBtnImage,
       ...{
-        marginLeft: (parseInt(styles.video.width) - parseInt(defaultStyles.startBtnImage.width)) / 2 + 'rem',
-        marginTop: (parseInt(styles.video.height) - parseInt(defaultStyles.startBtnImage.height)) / 2 + 'rem'
+        marginLeft: (parseInt(styles.video.width) - parseInt(defaultStyles.startBtnImage.width)) / 2,
+        marginTop: (parseInt(styles.video.height) - parseInt(defaultStyles.startBtnImage.height)) / 2
       }
     };
     styles.poster = {
@@ -232,7 +224,6 @@ class Video extends Component {
     };
     return styles;
   }
-
 }
 
 export default Video;
