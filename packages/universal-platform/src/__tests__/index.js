d9505c27a9bd1d3d0c46ac11ae9b8107af32b5e2
五晨 <yitong.wyt@alibaba-inc.com>
2017-04-12 11:17:11 +0800

    fix: update universal-platform test case

--- packages/universal-platform/src/__tests__/index.js
+++ packages/universal-platform/src/__tests__/index.js
@@ -1,24 +1,12 @@
 
 jest.autoMockOff();
 
+global.WXEnvironment = {
+  platform: 'ios'
+};
+
 describe('OS', () => {
-  it('should use default platform', () => {
-    global.WXEnvironment = undefined;
-    const Platform = require('../index');
-    const selectOS = Platform.select({
-      ios: 'test',
-      android: 'testAndroid',
-      web: 'testWeb'
-    });
-
-    expect(Platform.OS).toEqual('web');
-    expect(selectOS).toEqual('testWeb');
-  });
-
   it('should use WXEnvironment platform', () => {
-    global.WXEnvironment = {
-      platform: 'ios'
-    };
     const Platform = require('../index');
     const selectOS = Platform.select({
       ios: 'test',
