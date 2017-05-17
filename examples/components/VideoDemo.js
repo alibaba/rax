0c98a0c83ddfb2466db16535286746643e0cc39c
元彦 <yuanyan.cao@alibaba-inc.com>
2017-05-17 20:00:04 +0800

    feat: video exports more api

--- examples/components/VideoDemo.js
+++ examples/components/VideoDemo.js
@@ -1,5 +1,6 @@
 
 import {createElement, Component} from 'rax';
+import Video from 'rax-video';
 import {
   View,
   Text,
@@ -8,22 +9,47 @@ import {
   TextInput,
   Button,
   Switch,
-  Video,
   ScrollView,
   TouchableWithoutFeedback} from 'rax-components';
 
 class VideoDemo extends Component {
+  state = {
+    playControl: null
+  };
   render() {
     return (
       <View style={styles.container}>
+       <Text>controls=false playControl=play|pause</Text>
        <Video
          style={{
            flex: 1,
-           height: '350rem'
+           height: 350
          }}
          autoPlay={false}
+         controls={false}
+         playControl={this.state.playControl}
          src="http://cloud.video.taobao.com/play/u/2780279213/p/1/e/6/t/1/d/ld/36255062.mp4"
         />
+
+        <Button onPress={() => {
+          let playControl = this.state.playControl !== 'play' ? 'play' : 'pause';
+          this.setState({
+            playControl
+          });
+        }}>
+          Play or Pause
+        </Button>
+
+        <Text>muted=true</Text>
+        <Video
+          style={{
+            flex: 1,
+            height: 350
+          }}
+          autoPlay={false}
+          muted={true}
+          src="http://cloud.video.taobao.com/play/u/2780279213/p/1/e/6/t/1/d/ld/36255062.mp4"
+         />
       </View>
     );
   }
