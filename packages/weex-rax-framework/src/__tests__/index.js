f1dcfbf327328dfc6d3e84a254687154c0856d69
playing <deadplaying@gmail.com>
2017-05-22 23:10:42 +0800

    fix: fix test and lint error

--- packages/weex-rax-framework/src/__tests__/index.js
+++ packages/weex-rax-framework/src/__tests__/index.js
@@ -568,7 +568,7 @@ describe('framework', () => {
         {
           url: 'http://path/to/api',
           method: 'GET',
-          type: 'json'
+          type: 'text'
         }
       );
     });
