2c3893150df322084214ef001506fccec4730508
宝码 <nongyoubao@alibaba-inc.com>
2017-05-23 10:45:37 +0800

    feat: add package rax-hot-module-replacement-webpack-plugin

---
+++ packages/rax-hot-module-replacement-webpack-plugin/src/globalTemplate.js
@@ -0,0 +1,26 @@
+module.exports = `
+var global;
+
+if (typeof window !== 'undefined') {
+  global = window;
+} else if (typeof global !== 'undefined') {
+  global = global;
+} else if (typeof self !== 'undefined') {
+  global = self;
+} else {
+  global = {};
+};
+
+
+// reload polyfill
+var isWeex = typeof callNative === 'function';
+
+if (isWeex && typeof location.reload === 'undefined') {
+  var LOCATION_MODULE = '@weex-module/location';
+
+  location.reload = function(forceReload) {
+    var weexLocation = require(LOCATION_MODULE);
+    weexLocation.reload(forceReload);
+  };
+};
+`;
