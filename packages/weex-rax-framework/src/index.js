25d72ede72780b58c5a4e18bbb88cf8de9390839
亚城 <yacheng.sz@taobao.com>
2017-09-14 16:32:01 +0800

    feat: fireEvent params

--- packages/weex-rax-framework/src/index.js
+++ packages/weex-rax-framework/src/index.js
@@ -438,7 +438,7 @@ export function getRoot(instanceId) {
   return document.toJSON ? document.toJSON() : {};
 }
 
-function fireEvent(doc, ref, type, e, domChanges) {
+function fireEvent(doc, ref, type, e, domChanges, params) {
   if (Array.isArray(ref)) {
     ref.some((ref) => {
       return fireEvent(doc, ref, type, e) !== false;
@@ -449,7 +449,7 @@ function fireEvent(doc, ref, type, e, domChanges) {
   const el = doc.getRef(ref);
 
   if (el) {
-    const result = doc.fireEvent(el, type, e, domChanges);
+    const result = doc.fireEvent(el, type, e, domChanges, params);
     updateFinish(doc);
     return result;
   }
@@ -471,8 +471,8 @@ export function receiveTasks(instanceId, tasks) {
     tasks.forEach(task => {
       let result;
       if (task.method === 'fireEvent') {
-        let [nodeId, type, data, domChanges] = task.args;
-        result = fireEvent(document, nodeId, type, data, domChanges);
+        let [nodeId, type, data, domChanges, params] = task.args;
+        result = fireEvent(document, nodeId, type, data, domChanges, params);
       } else if (task.method === 'callback') {
         let [uid, data, ifKeepAlive] = task.args;
         result = document.taskCenter.callback(uid, data, ifKeepAlive);
