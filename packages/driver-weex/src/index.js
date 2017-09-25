5562e03d592213737e72f79ecf1e45cffa9a96aa
亚城 <yacheng.sz@taobao.com>
2017-09-25 11:22:29 +0800

    fix: addEventListener in driver-weex

--- packages/driver-weex/src/index.js
+++ packages/driver-weex/src/index.js
@@ -136,7 +136,7 @@ const Driver = {
   },
 
   addEventListener(node, eventName, eventHandler, props) {
-    let params = props && props[eventName + 'EventParams'];
+    let params = props ? props[eventName + 'EventParams'] : null;
     return node.addEvent(eventName, eventHandler, params);
   },
 
