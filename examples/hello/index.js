9841bd1043815d08cee4c2cf4cc569377843d197
元彦 <yuanyan@yuanyan.local>
2017-11-07 17:19:44 +0800

    fix: link version incorrect

--- examples/hello/index.js
+++ examples/hello/index.js
@@ -4,26 +4,17 @@ import Image from 'rax-image';
 import Link from 'rax-link';
 
 class Hello extends Component {
+  state = {
+    fontColor: '#333333',
+  };
+
+  handleClick(){
+    console.log('clicked');
+  }
   render() {
-    return <Text style={{color: '#333333'}}>
-      <Text style={{fontWeight: 'bold'}}>
-        Hello world,
-      </Text>
-      <Text style={{color: 'red'}}>
-        Rax
-      </Text>!
-      Rax <Text style={{textDecoration: 'underline'}}>website</Text>:
-      <Link href="//taobao.com" style={{color: 'blue'}}>
-        <Image
-          source={{uri: 'https://gw.alicdn.com/tfs/TB1g6AvPVXXXXa7XpXXXXXXXXXX-215-215.png'}}
-          style={{
-            width: 100,
-            height: 100,
-          }}
-        />
-        Click here
-      </Link>.
-    </Text>;
+    return (<p onClick={this.handleClick} style={{fontWeight: 'bold', color: this.state.fontColor}}>
+      Hello {this.props.name}!
+    </p>);
   }
 }
 
