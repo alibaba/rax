be9dbc5b6cf6acfef46f109d053d1f59bc258f88
亚城 <narutosun2@163.com>
2018-03-02 10:10:12 +0800

    feat: add needLayout

--- packages/universal-transition/src/index.js
+++ packages/universal-transition/src/index.js
@@ -22,7 +22,7 @@ export default function transition(node, styles, options, callback) {
       timingFunction: options.timingFunction || 'linear',
       delay: options.delay || 0,
       duration: options.duration || 0,
-      // needLayout: options.needLayout || 
+      needLayout: options.needLayout || false
     }, callback || function() {});
   } else if (isWeb) {
     const duration = options.duration || 0; // ms
