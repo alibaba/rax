dd6a84abe656bd2c4b121b9e9d331c51e44b676f
xiaoqi.huxq <xiaoqi.huxq@alibaba-inc.com>
2018-03-29 16:07:08 +0800

    fix: update test case

--- packages/rax-tab-panel/src/__tests__/TabPanel.js
+++ packages/rax-tab-panel/src/__tests__/TabPanel.js
@@ -2,14 +2,19 @@ import {createElement, Component} from 'rax';
 import renderer from 'rax-test-renderer';
 import View from 'rax-view';
 import Text from 'rax-text';
-import {TabController, TabPanel} from '../index';
+import {TabController, TabPanel, TabPanelLink, TabPanelView} from '../index';
 
 
 class TabPanelTest extends Component {
   render() {
     return (<TabController>
     		<TabPanel>
-    			<Text>1</Text>
+    			<TabPanelLink href={'//github.com'}>1</TabPanelLink>
+    			<TabPanelView><Text>1</Text></TabPanelView>
+    		</TabPanel>
+    		<TabPanel>
+    			<TabPanelLink href={'//github.com'}>2</TabPanelLink>
+    			<TabPanelView><Text>1</Text></TabPanelView>
     		</TabPanel>
     	</TabController>);
   }
@@ -20,6 +25,6 @@ describe('TabPanel', () => {
     const component = renderer.create(<TabPanelTest />);
     let tree = component.toJSON();
     expect(tree.tagName).toEqual('DIV');
-    // expect(tree.children[0].tagName).toEqual('SPAN');
+    expect(tree.children[0].tagName).toEqual('DIV');
   });
 });
