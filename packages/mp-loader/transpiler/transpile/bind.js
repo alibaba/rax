18dd5d70357b6958ee049de0541f4e5938d7bd17
卓凌 <zhuoling.lcl@alibaba-inc.com>
2018-08-15 17:23:15 +0800

    fix: computed properties

--- packages/mp-loader/transpiler/transpile/bind.js
+++ packages/mp-loader/transpiler/transpile/bind.js
@@ -1,8 +1,9 @@
 const { getAndRemoveAttr, addAttr, normalizeMustache } = require('../helpers');
 
 const IS_BIND_REG = /\W*\{\{/;
+const IS_DETECTIVE = /^a\:/;
 function transformNode(el) {
-  const { attrsList } = el;
+  const { attrsList, attrsMap } = el;
 
   if (el.hasOwnProperty('key') && IS_BIND_REG.test(el.key)) {
     const exp = normalizeMustache(el.key, el);
@@ -12,10 +13,23 @@ function transformNode(el) {
   if (!attrsList) {
     return;
   }
-  attrsList.forEach(transformAttr);
+
+  const list = Object.keys(attrsMap);
+  for (let i = 0, l = list.length; i < l; i++) {
+    transformAttr({
+      name: list[i],
+      value: attrsMap[list[i]]
+    });
+  }
 
   function transformAttr(attr) {
     const { name, value } = attr;
+    if (name === 'class' || name === 'style') {
+      return;
+    }
+    if (IS_DETECTIVE.test(name)) {
+      return;
+    }
     if (IS_BIND_REG.test(value)) {
       const exp = getAndRemoveAttr(el, name);
       addAttr(el, name, exp, '');
