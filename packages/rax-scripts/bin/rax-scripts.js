c33ff5c2010267553bf64e014be1795f191f02fa
noyobo <nongyoubao@alibaba-inc.com>
2018-08-23 16:15:34 +0800

    chore: format rax-scripts

--- packages/rax-scripts/bin/rax-scripts.js
+++ packages/rax-scripts/bin/rax-scripts.js
@@ -17,7 +17,7 @@ var commands = argv._;
 
 if (commands.length === 0) {
   console.error(
-    'You did not pass any commands, did you mean to run `rax-script build`?'
+    'You did not pass any commands, did you mean to run `rax-script build`?',
   );
   process.exit(1);
 }
@@ -25,24 +25,22 @@ if (commands.length === 0) {
 const script = commands[0];
 
 /* eslint no-console: 0 */
-var result = spawn.sync(
-  'node', [require.resolve('../lib/' + script)], {
-    cwd: process.cwd(),
-    stdio: 'inherit'
-  }
-);
+var result = spawn.sync('node', [require.resolve('../lib/' + script)], {
+  cwd: process.cwd(),
+  stdio: 'inherit',
+});
 
 if (result.signal === 'SIGKILL') {
   console.log(
     'The build failed because the process exited too early. ' +
-    'This probably means the system ran out of memory or someone called ' +
-    '`kill -9` on the process.'
+      'This probably means the system ran out of memory or someone called ' +
+      '`kill -9` on the process.',
   );
 } else if (result.signal === 'SIGTERM') {
   console.log(
     'The build failed because the process exited too early. ' +
-    'Someone might have called `kill` or `killall`, or the system could ' +
-    'be shutting down.'
+      'Someone might have called `kill` or `killall`, or the system could ' +
+      'be shutting down.',
   );
   process.exit(1);
 }
