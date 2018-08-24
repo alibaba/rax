1bbf0ced3c196b1df7b6e8b96c820c5bc2a1de4a
noyobo <nongyoubao@alibaba-inc.com>
2018-08-24 15:36:55 +0800

    feat: rax-build task

--- packages/rax-scripts/bin/rax-scripts-build.js
+++ packages/rax-scripts/bin/rax-scripts-build.js
@@ -1,27 +1,28 @@
 #!/usr/bin/env node
 'use strict';
-
-const spawn = require('cross-spawn');
-
 /* eslint no-console: 0 */
-var result = spawn.sync('node', [require.resolve('../lib/build.js')], {
-  cwd: process.cwd(),
-  stdio: 'inherit',
-});
+const program = require('commander');
+const optionsAttachToEnv = require('../lib/config/optionsAttachToEnv');
 
-if (result.signal === 'SIGKILL') {
-  console.log(
-    'The build failed because the process exited too early. ' +
-      'This probably means the system ran out of memory or someone called ' +
-      '`kill -9` on the process.',
-  );
-} else if (result.signal === 'SIGTERM') {
-  console.log(
-    'The build failed because the process exited too early. ' +
-      'Someone might have called `kill` or `killall`, or the system could ' +
-      'be shutting down.',
-  );
-  process.exit(1);
-}
+program
+  .option(
+    '--type <type>',
+    'set application type, Enum: ["rax", "miniapp"].',
+    /^(rax|miniapp)$/i,
+    'rax',
+  )
+  .option('-p, --port <port>', 'set server port', 9999)
+  .option('--host <host>', 'set server host')
+  .option('--dir <dir>', 'set project path')
+  .option('--https', 'enabled https protocol', false)
+  .option('--debug', 'enabled debug mode', false)
+  .action((cmd) => {
+    optionsAttachToEnv(cmd);
+    if (program.type == 'rax') {
+      require('../src/build')();
+    } else if (program.type == 'miniapp') {
+      require('../src/miniapp-build')();
+    }
+  });
 
-process.exit(result.status);
+program.parse(process.argv);
