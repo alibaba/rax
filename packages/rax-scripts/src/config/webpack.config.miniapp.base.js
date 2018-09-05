4ac67beb21bf0d12daa0e1d293b8cde6ac1c1d8a
noyobo <nongyoubao@alibaba-inc.com>
2018-09-05 21:37:34 +0800

    feat: miniapp webpack config

---
+++ packages/rax-scripts/src/config/webpack.config.miniapp.base.js
@@ -0,0 +1,146 @@
+'use strict';
+/* eslint no-console: 0 */
+const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
+const colors = require('chalk');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
+
+const pathConfig = require('./path.config');
+
+const babelOptions = {
+  presets: [
+    require.resolve('babel-preset-es2015'),
+    require.resolve('babel-preset-rax'),
+  ],
+  plugins: [require.resolve('rax-hot-loader/babel')],
+};
+
+const publicPath = '/';
+const publicUrl = '';
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  context: process.cwd(),
+  target: 'web',
+  entry: {},
+  output: {
+    path: pathConfig.appBuild,
+    filename: 'js/[name].js',
+    publicPath: publicPath,
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.jsx', '.html', '.vue'],
+  },
+  externals: [
+    function(context, request, callback) {
+      if (/^@(core|schema)\//.test(request)) {
+        return callback(null, `commonjs2 ${request}`);
+      }
+      callback();
+    },
+  ],
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
+        PUBLIC_URL: JSON.stringify(publicUrl),
+      },
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: pathConfig.appPublic, // copy static assets in public folder
+        to: pathConfig.appBuild,
+        ignore: ['.*', '*.html'],
+      },
+      {
+        from: pathConfig.appManifest, // cppy manifest.json
+        to: pathConfig.appBuild,
+      },
+    ]),
+    new CaseSensitivePathsPlugin(),
+    new webpack.ProgressPlugin(function(percentage, msg) {
+      const stream = process.stderr;
+      if (stream.isTTY && percentage < 0.71) {
+        stream.cursorTo(0);
+        stream.write(`webpack: ${msg}...`);
+        stream.clearLine(1);
+      } else if (percentage === 1) {
+        console.log('');
+        console.log(colors.green('webpack: bundle build is now finished.'));
+      }
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          {
+            loader: require.resolve('babel-loader'),
+            options: babelOptions,
+          },
+        ],
+      },
+      {
+        test: /manifest\.json$/,
+        type: 'javascript/auto',
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          {
+            loader: require.resolve('babel-loader'),
+            options: babelOptions,
+          },
+          {
+            loader: require.resolve('miniapp-manifest-loader'),
+          },
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: require.resolve('sfc-loader'),
+            options: {
+              builtInRuntime: false,
+              preserveWhitespace: false,
+            },
+          },
+        ],
+        exclude: [pathConfig.appHtml],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: require.resolve('image-source-loader'),
+          },
+        ],
+      },
+    ],
+  },
+  optimization: {
+    namedModules: true,
+    mergeDuplicateChunks: true,
+    splitChunks: {
+      cacheGroups: {
+        atag: {
+          test: /sfc\-atag\-beta/,
+          name: 'miniapp-components',
+          chunks: 'all',
+          minSize: 0,
+          minChunks: 1,
+          reuseExistingChunk: true,
+          enforce: true,
+          priority: -10,
+        },
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor',
+          chunks: 'all',
+          priority: -20,
+        },
+      },
+    },
+  },
+};
---
+++ packages/rax-scripts/src/config/webpack.config.miniapp.prod.js
@@ -0,0 +1,80 @@
+/* eslint-disable no-console */
+
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const webpackMerge = require('webpack-merge');
+
+const pathConfig = require('./path.config');
+const webpackConfigBase = require('./webpack.config.miniapp.base');
+
+const registerServiceWorker = `<script>
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+      console.log('SW registered');
+    }).catch(registrationError => {
+      console.log('SW registration failed: ', registrationError);
+    });
+  });
+}
+</script>`;
+
+const webpackConfigProd = webpackMerge(webpackConfigBase, {
+  entry: {
+    'index.min': [pathConfig.appManifest],
+  },
+  plugins: [
+    new CleanWebpackPlugin([pathConfig.appBuild]),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: pathConfig.appHtml,
+      registerServiceWorker: registerServiceWorker,
+    }),
+
+    new ManifestPlugin({
+      fileName: 'asset-manifest.json',
+    }),
+    new SWPrecacheWebpackPlugin({
+      dontCacheBustUrlsMatching: /\.\w{8}\./,
+      filename: 'service-worker.js',
+      logger(message) {
+        if (message.indexOf('Total precache size is') === 0) {
+          return;
+        }
+        if (message.indexOf('Skipping static resource') === 0) {
+          return;
+        }
+        console.log(message);
+      },
+      minify: true,
+      staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/],
+    }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        uglifyOptions: {
+          compress: {
+            unused: false,
+            warnings: false,
+          },
+          output: {
+            // eslint-disable-next-line camelcase
+            ascii_only: true,
+            comments: 'some',
+            beautify: false,
+          },
+          mangle: true,
+        },
+      }),
+    ],
+  },
+});
+
+module.exports = webpackConfigProd;
