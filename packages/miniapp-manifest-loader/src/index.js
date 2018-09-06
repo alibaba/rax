5e8a2edb304ab58df40ed7e8927d924d04b0ad0c
noyobo <nongyoubao@alibaba-inc.com>
2018-09-06 11:24:18 +0800

    fix: avoid page name syntax error

--- packages/miniapp-manifest-loader/src/index.js
+++ packages/miniapp-manifest-loader/src/index.js
@@ -2,28 +2,29 @@ module.exports = function(source) {
   let manifestJson = typeof source === 'string' ? JSON.parse(source) : source;
 
   const pages = manifestJson.pages;
-  const pagesImport = Object.keys(pages).map((page) => {
-    return `import Page${page} from './${pages[page]}';`;
-  });
 
-  const pagesMap = `{${Object.keys(pages)
+  const pagesMap = `{
+${Object.keys(pages)
     .map((page) => {
-      return `${page}: Page${page}`;
+      return `"${page}": _requireReturnDefault(require('./${pages[page]}'))`;
     })
-    .join(',')}
-  }`;
+    .join(',\n')}
+}`;
 
   // Ensure the miniapp-web-renderer absolute path
   const miniappWebRendererPath = require.resolve('miniapp-web-renderer');
 
-  const manifestCode = JSON.stringify(manifestJson)
+  const manifestCode = JSON.stringify(manifestJson, null, 2)
     .replace(/\u2028/g, '\\u2028')
     .replace(/\u2029/g, '\\u2029');
   return `
 import { render } from '${miniappWebRendererPath}';
-${pagesImport.join('\n')}
+
+function _requireReturnDefault(obj) { return obj && obj.__esModule ? obj.default : obj; }
 
 const pagesMap = ${pagesMap};
+
 const manifestData = ${manifestCode};
+
 render(manifestData, pagesMap)`;
 };
