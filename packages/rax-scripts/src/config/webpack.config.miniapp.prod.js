0a1e3b1626e95a4ab708d8376a42137bd2ef0bf0
noyobo <nongyoubao@alibaba-inc.com>
2018-09-06 10:30:22 +0800

    chore: optimize config

--- packages/rax-scripts/src/config/webpack.config.miniapp.prod.js
+++ packages/rax-scripts/src/config/webpack.config.miniapp.prod.js
@@ -1,5 +1,4 @@
 /* eslint-disable no-console */
-
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
