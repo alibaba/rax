1f8bd4b9dc6022917f48721abe90ba8df08e33c0
元彦 <yuanyan@users.noreply.github.com>
2018-09-07 18:15:20 +0800

    refactor: page router

--- packages/miniapp-web-renderer/src/index.js
+++ packages/miniapp-web-renderer/src/index.js
@@ -9,7 +9,7 @@ import ErrorPage from './components/ErrorPage';
 
 BrowserDriver.setTagPrefix('a-');
 
-function getPathnameByLocation(location) {
+function getPathnameFromLocation(location) {
   let { pathname = '' } = location;
   pathname = pathname.replace(/^\//, '');
   return pathname;
@@ -25,7 +25,6 @@ export default {
    * @param {function} pagesMap.value page component
    * @example
    *  render({name: "demo"}, { home: HomeComponent })
-   *
    */
   render(manifest, pagesMap) {
     function miniappRender(Component, props = {}) {
@@ -54,33 +53,27 @@ export default {
           miniappRender(Component);
         })
         .catch((error) => {
-          console.warn(`Not found PageComponent when location pathname is: "${pathname}"`);
-          console.error(error);
+          console.error(`Page "${pathname}" not found.`);
           miniappRender(ErrorPage, { history });
         });
     }
 
     const history = createHashHistory({ hashType: 'hashbang' });
     history.listen((location, action) => {
-      const pathname = getPathnameByLocation(location);
-      if (pathname) {
-        routerRender(pathname);
-      } else {
-        console.warn(`history action: ${action}, but can not found pathname in location`);
-      }
+      const pathname = getPathnameFromLocation(location);
+      routerRender(pathname);
     });
 
     // @hack Simulation of the navigator
     // eslint-disable-next-line camelcase
-    window.__renderer_to_worker__ = function __renderer_to_worker__(navigate) {
-      const { navigateTo } = navigate;
+    window.__renderer_to_worker__ = function __renderer_to_worker__({ navigateTo }) {
       history.push(navigateTo);
     };
 
-    const initPathname = getPathnameByLocation(history.location);
+    const pathnameInitValue = getPathnameFromLocation(history.location);
 
-    if (initPathname) {
-      routerRender(initPathname);
+    if (pathnameInitValue) {
+      routerRender(pathnameInitValue);
     } else {
       // default render of the manifest homepage field.
       history.push(manifest.homepage);
