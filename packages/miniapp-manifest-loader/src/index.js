d4ff0f09925e55cb381461494c7731eace5655be
元彦 <yuanyan@users.noreply.github.com>
2018-09-07 13:37:07 +0800

    chore: rework

--- packages/miniapp-manifest-loader/src/index.js
+++ packages/miniapp-manifest-loader/src/index.js
@@ -1,30 +1,29 @@
 module.exports = function(source) {
-  let manifestJson = typeof source === 'string' ? JSON.parse(source) : source;
-
+  const manifestJson = typeof source === 'string' ? JSON.parse(source) : source;
   const pages = manifestJson.pages;
-
-  const pagesMap = `{
-${Object.keys(pages)
-    .map((page) => {
-      return `"${page}": _requireReturnDefault(require('./${pages[page]}'))`;
-    })
-    .join(',\n')}
-}`;
-
-  // Ensure the miniapp-web-renderer absolute path
-  const miniappWebRendererPath = require.resolve('miniapp-web-renderer');
-
-  const manifestCode = JSON.stringify(manifestJson, null, 2)
-    .replace(/\u2028/g, '\\u2028')
-    .replace(/\u2029/g, '\\u2029');
+  // Resolve to miniapp-web-renderer local absolute path
+  const webRendererPathname = require.resolve('miniapp-web-renderer');
+  
   return `
-import { render } from '${miniappWebRendererPath}';
+import { render } from '${webRendererPathname}';
 
 function _requireReturnDefault(obj) { return obj && obj.__esModule ? obj.default : obj; }
 
-const pagesMap = ${pagesMap};
-
-const manifestData = ${manifestCode};
-
-render(manifestData, pagesMap)`;
+const pagesMap = {
+  ${
+    Object.keys(pages).map((page) => {
+      return `  "${page}" : _requireReturnDefault(require('./${pages[page]}'))`;
+    }).join(',\n')
+  }
 };
+
+const manifestData = ${
+  JSON.stringify(manifestJson, null, 2)
+    .replace(/\u2028/g, '\\u2028')
+    .replace(/\u2029/g, '\\u2029')
+};
+
+render(manifestData, pagesMap);
+`;
+
+}
