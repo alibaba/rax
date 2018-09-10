72cefdd617ef08f9dbbbcba7babaa0cc1aee92b1
卓凌 <zhuoling.lcl@alibaba-inc.com>
2018-09-10 21:53:21 +0800

    feat: native video downgrade & update custom-elements-es5-adapter

--- packages/atag/src/components/video/index.js
+++ packages/atag/src/components/video/index.js
@@ -1,8 +1,9 @@
+import shouldDowngradeNativeView from 'shared/utils/shouldDowngradeNativeView';
 import NativeVideo from './video.native';
 import WebVideo from './video.web';
 
 const isWeb = !/AliApp/.test(navigator.userAgent);
-const isDowngrade = false;
+const isDowngrade = shouldDowngradeNativeView();
 if (isWeb || isDowngrade) {
   customElements.define(WebVideo.is, WebVideo);
 } else {
