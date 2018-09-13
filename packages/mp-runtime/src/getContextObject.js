aaf1ca741d989b7f4643f99a621bb1286acdbc55
卓凌 <zhuoling.lcl@alibaba-inc.com>
2018-09-13 20:38:00 +0800

    fix: getApp as global runtime fn

---
+++ packages/mp-runtime/src/getContextObject.js
@@ -0,0 +1,9 @@
+/**
+ * Get current runtime context object
+ */
+export default function getContextObject() {
+  return typeof global !== 'undefined' ? global
+    : typeof window !== 'undefined' ? window
+      : typeof self !== 'undefined' ? self
+        : (new Function('return this'))() // eslint-disable-line
+}
