c7473827270e97d952beab541439c25a33d89776
fushen.jzw <fushen.jzw@alibaba-inc.com>
2018-09-18 12:11:05 +0800

    fix: the case of scroll-view and swiper nesting

--- packages/atag/src/shared/utils/index.js
+++ packages/atag/src/shared/utils/index.js
@@ -5,11 +5,16 @@ import throttle from './throttle';
 import easeInOutCubic from './easeInOutCubic';
 import handlerSubmit from './formHandler';
 
+import { GestureEventListeners } from '@polymer/polymer/lib/mixins/gesture-event-listeners.js';
+import * as Gestures from '@polymer/polymer/lib/utils/gestures.js';
+
 export {
   injectStyle,
   afterNextRender,
   throttle,
   easeInOutCubic,
-  handlerSubmit
+  handlerSubmit,
+  GestureEventListeners,
+  Gestures
 };
 export { isIOS, isAndroid, isTB } from './device';
