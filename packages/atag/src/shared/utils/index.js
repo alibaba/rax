f589e6c25f2e2097d5e720e0061dd1bf25694bae
fushen.jzw <fushen.jzw@alibaba-inc.com>
2018-09-18 12:27:25 +0800

    fix: code style

--- packages/atag/src/shared/utils/index.js
+++ packages/atag/src/shared/utils/index.js
@@ -1,15 +1,11 @@
-import injectStyle from './injectStyle';
-
-import { afterNextRender } from '@polymer/polymer/lib/utils/render-status';
 import throttle from './throttle';
 import easeInOutCubic from './easeInOutCubic';
 import handlerSubmit from './formHandler';
-
+import { afterNextRender } from '@polymer/polymer/lib/utils/render-status';
 import { GestureEventListeners } from '@polymer/polymer/lib/mixins/gesture-event-listeners.js';
 import * as Gestures from '@polymer/polymer/lib/utils/gestures.js';
 
 export {
-  injectStyle,
   afterNextRender,
   throttle,
   easeInOutCubic,
