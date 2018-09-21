17b3ce82b2d80eb31d188f241ab4d081ade97701
元彦 <yuanyan.cao@alibaba-inc.com>
2018-09-21 15:09:21 +0800

    refactor: move utils into shared

--- packages/atag/src/shared/utils/easeInOutCubic.js
+++
@@ -1,3 +0,0 @@
-export default function easeInOutCubic(t) {
-  return t < .5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
-}
--- packages/atag/src/shared/utils/shouldDowngradeNativeView.js
+++
@@ -1,9 +0,0 @@
-const embedViewManager =
-  typeof __embed_view_manager__ !== "undefined" ? __embed_view_manager__ : null; // eslint-disable-line
-
-/**
- * check whether native view is supported
- */
-export default function shouldDowngradeNativeView() {
-  return embedViewManager ? embedViewManager.shouldDowngrade() : false;
-}
