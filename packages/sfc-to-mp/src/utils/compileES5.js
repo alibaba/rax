0eed1bb00be334b8eaa9d3f77db61f38a91e1177
noyobo <nongyoubao@alibaba-inc.com>
2018-09-29 16:03:23 +0800

    refactor: babel.config.js

--- packages/sfc-to-mp/src/utils/compileES5.js
+++ packages/sfc-to-mp/src/utils/compileES5.js
@@ -1,11 +1,11 @@
-const getBabelOptions = require('./getBabelOptions');
 const { transformSync } = require('@babel/core');
+const babelConfig = require('../config/babel.config');
 
 module.exports = function compileES5(str, opts) {
   try {
     const { code, map, ast } = transformSync(
       str,
-      Object.assign({}, getBabelOptions(), opts)
+      Object.assign({}, babelConfig, opts)
     );
     return { code, map, ast };
   } catch (err) {
