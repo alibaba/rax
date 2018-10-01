297e7e33f1a2224483016b1e93c04e92e093fc4f
noyobo <nongyoubao@alibaba-inc.com>
2018-10-01 16:11:15 +0800

    fix: sfc-runtime path

--- packages/sfc-to-mp/src/config/vendors.js
+++ packages/sfc-to-mp/src/config/vendors.js
@@ -2,5 +2,5 @@ module.exports = {
   coreApp: require.resolve('../helpers/coreApp'),
   createApp: require.resolve('../helpers/createApp'),
   createPage: require.resolve('../helpers/createPage'),
-  sfc: require.resolve('../../../sfc-runtime/lib/sfc'),
+  sfc: require.resolve('sfc-runtime/lib/sfc'),
 };
