be425b210eecc1d2f7b82fd9c21be0ccec79d7df
卓凌 <zhuoling.lcl@alibaba-inc.com>
2018-10-24 22:51:46 +0800

    refactor: remote procedure

--- packages/driver-worker/src/platform/web/RemoteESSyncHandler.js
+++ packages/driver-worker/src/platform/web/RemoteESSyncHandler.js
@@ -21,25 +21,66 @@ export default class RemoteESSyncHandler {
   /**
    * Query a global variable's val
    * payload: { key: 'location.href[0]' }
-   * returns: { value }
+   * returns: value
    */
   query({ key, processId }) {
     try {
-      let i = 0, ref = global, current = '';
-      while (key[i]) {
-        if (key[i] === '.' || key[i] === '[') {
-          ref = ref[current];
-          current = '';
-        } else if (key[i] === ']' || key[i] === ' ') {
-          // Skip
-        } else {
-          current += key[i];
-        }
-        i++;
-      }
-      this.resolve(processId, { value: ref[current] });
+      const { value } = this.resolveMemberExpression(method);
+      this.resolve(processId, value);
     } catch(err) {
       this.reject(processId, 'Can not read value of ' + key);
     }
+  }
+
+  /**
+   * Call a method
+   * payload: { method: 'location.replace', args: ['/foo.html'] }
+   * returns: executed result
+   */
+  call({ processId, method, args }) {
+    try {
+      const { scope, value } = this.resolveMemberExpression(method);
+      this.resolve(processId, value.apply(scope, args));
+    } catch(err) {
+      this.reject(processId, 'Can not call ' + expression);
+    }
+  }
+
+  assign({ processid, key, value }) {
+    // todo
+  }
+
+  /**
+   * Execute a procedure
+   * payload: {
+   *   procedures: [
+   *     { type: 'call', method: 'document.createElement', args: ['div'], as: '#container' },
+   *     { type: 'call', method: 'document.createElement', args: ['span'], as: '#child' },
+   *     { type: 'assign', key: '#child.innerHTML', value: 'hello world' },
+   *     { type: 'call', '#container.appendChild', args: ['#child'] },
+   *   ]
+   * }
+   * returns: executed result
+   */
+  procedure({ processId, }) {
+    // todo
+  }
+
+  resolveMemberExpression(exp, scope = global) {
+    let i = 0, ref = scope, current = '';
+    while (exp[i]) {
+      if (exp[i] === '.' || exp[i] === '[') {
+        ref = ref[current];
+        current = '';
+      } else if (exp[i] === ']' || exp[i] === ' ') {
+        // Skip
+      } if (exp[i] === '#') {
+        ref = scope;
+      } else {
+        current += exp[i];
+      }
+      i++;
+    }
+    return { scope: ref, value: ref[current] };
   }
 }
