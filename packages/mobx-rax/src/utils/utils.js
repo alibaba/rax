5970e7bf7c93031ecf40de19e3e2f3f0fe31ae95
元彦 <yuanyan.cao@alibaba-inc.com>
2018-10-25 19:43:38 +0800

    chore: move mobx-rax and rax-redux to packages dir

---
+++ packages/mobx-rax/src/utils/utils.js
@@ -0,0 +1,5 @@
+export function isStateless(component) {
+  // `function() {}` has prototype, but `() => {}` doesn't
+  // `() => {}` via Babel has prototype too.
+  return !(component.prototype && component.prototype.render);
+}
---
+++ packages/rax-redux/README.md
@@ -0,0 +1,1 @@
+# rax-redux [![npm](https://img.shields.io/npm/v/rax-redux.svg)](https://www.npmjs.com/package/rax-redux)
