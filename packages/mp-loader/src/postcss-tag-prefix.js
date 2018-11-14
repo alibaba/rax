a9d4d13eb2a1cd16ce0661a536303168a8125dfd
卓凌 <zhuoling.lcl@alibaba-inc.com>
2018-11-14 19:45:39 +0800

    fix: postcss-tag-prefix

--- packages/mp-loader/src/postcss-tag-prefix.js
+++ packages/mp-loader/src/postcss-tag-prefix.js
@@ -1,6 +1,42 @@
 const postcss = require('postcss');
 const selectorParser = require('postcss-selector-parser');
 
+const validTags = {
+  audio: true,
+  button: true,
+  canvas: true,
+  checkbox: true,
+  'checkbox-group': true,
+  icon: true,
+  label: true,
+  picker: true,
+  'picker-view': true,
+  'picker-view-column': true,
+  radio: true,
+  slider: true,
+  text: true,
+  video: true,
+  image: true,
+  input: true,
+  map: true,
+  'radio-group': true,
+  swiper: true,
+  'swiper-item': true,
+  textarea: true,
+  view: true,
+  form: true,
+  navigator: true,
+  progress: true,
+  'scroll-view': true,
+  switch: true,
+  'web-view': true,
+  page: true
+};
+
+function isValidTag(tagName) {
+  return validTags.hasOwnProperty(tagName);
+}
+
 module.exports = postcss.plugin('postcss-tag-prefix', function(options) {
   const prefix = options && options.prefix || 'a-';
 
@@ -10,7 +46,7 @@ module.exports = postcss.plugin('postcss-tag-prefix', function(options) {
       if (rule.selector) {
         rule.selector = selectorParser(function(selectors) {
           selectors.walk(selector => {
-            if (selector.type === 'tag') {
+            if (selector.type === 'tag' && isValidTag(selector.value)) {
               selector.value = prefix + selector.value;
             }
           });
