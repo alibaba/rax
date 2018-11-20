55431ab330a6d4163f7d66ad59bb06d049013763
卓凌 <zhuoling.lcl@alibaba-inc.com>
2018-11-20 12:39:55 +0800

    feat: support miniapp builder postcss resolver

--- packages/miniapp-builder/src/config/getMiniProgramConfig.js
+++ packages/miniapp-builder/src/config/getMiniProgramConfig.js
@@ -2,10 +2,45 @@ const { join } = require('path');
 const babelConfig = require('./babelConfig');
 const WebpackMiniProgramPlugin = require('../plugins/WebpackMiniProgramPlugin');
 
+const ABSOLUTE_REG = /^\//;
+const RELATIVE_REG = /^\./;
+const STYLE_EXT = '.acss';
+
 const babelLoaderConfig = {
   loader: require.resolve('babel-loader'),
   options: babelConfig,
 };
+
+/**
+ * Resolve postcss file path
+ * eg.
+ *   @import "./button.acss"; // relative path
+ *   @import "/button.acss"; // absolute path to project
+ *   @import "third-party/button.acss"; // npm package
+ */
+function styleResolver(id, basedir, importOptions) {
+
+  let filePath;
+  if (ABSOLUTE_REG.test(id)) {
+    filePath = join(importOptions.root, id);
+  } else if (RELATIVE_REG.test(id)) {
+    filePath = join(basedir, id);
+  } else {
+    filePath = require.resolve(id, {
+      paths: [join(importOptions.root, 'node_modules')]
+    });
+  }
+
+  /**
+   * Allow to ignore extension, default to .acss
+   */
+  if (!/\.acss$/.test(filePath)) {
+    filePath = filePath + STYLE_EXT;
+  }
+
+  return filePath;
+}
+
 module.exports = (projectDir, opts) => {
   return {
     output: {
@@ -38,6 +73,7 @@ module.exports = (projectDir, opts) => {
               loader: require.resolve('postcss-loader'),
               options: {
                 plugins: [
+                  require('postcss-import')({ resolve: styleResolver }),
                   require('../plugins/PostcssPluginRpx2rem'),
                   require('../plugins/PostcssPluginTagPrefix'),
                   require('autoprefixer')
