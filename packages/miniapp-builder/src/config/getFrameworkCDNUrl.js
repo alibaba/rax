846c91cb6f048d1d1b549e8761896ee0af8d0afd
卓凌 <zhuoling.lcl@alibaba-inc.com>
2018-11-29 01:46:19 +0800

    feat: add auto update framework version

--- packages/miniapp-builder/src/config/getFrameworkCDNUrl.js
+++ packages/miniapp-builder/src/config/getFrameworkCDNUrl.js
@@ -1,4 +1,4 @@
-const FRAMEWORK_VERSION = exports.FRAMEWORK_VERSION = require('./frameworkVersion');
+let FRAMEWORK_VERSION = exports.FRAMEWORK_VERSION = require('./frameworkVersion');
 
 const FRAMEWORK_CDN_PREFIX = 'https://g.alicdn.com/code/npm/miniapp-framework/';
 const FRAMEWORK_CDN_SUFFIX = '/dist';
@@ -9,6 +9,10 @@ const FRAMEWORK_RENDERER_VIEW = '/native/renderer.html';
 
 const FRAMEWORK_H5_MASTER = '/h5/master.js';
 const FRAMEWORK_H5_VIEW = '/h5/master.html';
+
+exports.updateFrameworkVersion = function(version) {
+  FRAMEWORK_VERSION = exports.FRAMEWORK_VERSION = version;
+}
 
 const getNativeRendererUrl = exports.getNativeRendererUrl = function(version) {
   return FRAMEWORK_CDN_PREFIX + version + FRAMEWORK_CDN_SUFFIX + FRAMEWORK_RENDERER_VIEW;
