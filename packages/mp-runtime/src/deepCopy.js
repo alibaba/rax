da35f76017c1f16958da3ac546ec1e20780bb2d5
卓凌 <zhuoling.lcl@alibaba-inc.com>
2018-11-29 21:22:53 +0800

    fix: data should not be modified the second render

---
+++ packages/mp-runtime/src/deepCopy.js
@@ -0,0 +1,37 @@
+/**
+ * Deep Copy from rfdc(really fast deep copy)
+ */
+export default function deepCopy(o) {
+  if (typeof o !== "object" || o === null) return o;
+  if (o instanceof Date) return new Date(o);
+  if (Array.isArray(o)) return cloneArray(o, deepCopy);
+  const o2 = {};
+  for (var k in o) {
+    var cur = o[k];
+    if (typeof cur !== "object" || cur === null) {
+      o2[k] = cur;
+    } else if (cur instanceof Date) {
+      o2[k] = new Date(cur);
+    } else {
+      o2[k] = deepCopy(cur);
+    }
+  }
+  return o2;
+}
+
+function cloneArray(a, fn) {
+  const keys = Object.keys(a);
+  const a2 = new Array(keys.length);
+  for (var i = 0; i < keys.length; i++) {
+    const k = keys[i];
+    const cur = a[k];
+    if (typeof cur !== "object" || cur === null) {
+      a2[k] = cur;
+    } else if (cur instanceof Date) {
+      a2[k] = new Date(cur);
+    } else {
+      a2[k] = fn(cur);
+    }
+  }
+  return a2;
+}
