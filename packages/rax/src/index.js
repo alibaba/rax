95c27f0556867d57093bcd58ff09276214184855
元彦 <yuanyan.cao@alibaba-inc.com>
2018-12-03 13:00:50 +0800

    feat: export default

--- packages/rax/src/index.js
+++ packages/rax/src/index.js
@@ -1,21 +1,31 @@
 import './debug/devtools';
 import {createElement, cloneElement, isValidElement, createFactory} from './element';
 import {useState, useContext, useEffect, useLayoutEffect, useRef, useCallback, useMemo, useReducer, useImperativeMethods} from './hooks';
+import Component from './component';
+import PureComponent from './purecomponent';
+import PropTypes from './proptypes';
+import render from './render';
+import hydrate from './hydrate';
+import createPortal from './createPortal';
+import findDOMNode from './findDOMNode';
+import unmountComponentAtNode from './unmountComponentAtNode';
+import findComponentInstance from './findComponentInstance';
+import setNativeProps from './setNativeProps';
+import version from './version';
+import Children from './children';
+import createContext from './createContext';
+import createRef from './createRef';
 
-export {createElement, cloneElement, isValidElement, createFactory};
-export {useState, useContext, useEffect, useLayoutEffect, useRef, useCallback, useMemo, useReducer, useImperativeMethods};
-export Component from './component';
-export PureComponent from './purecomponent';
-export PropTypes from './proptypes';
-export render from './render';
-export hydrate from './hydrate';
-export createPortal from './createPortal';
-export findDOMNode from './findDOMNode';
-export unmountComponentAtNode from './unmountComponentAtNode';
-export findComponentInstance from './findComponentInstance';
-export setNativeProps from './setNativeProps';
-export version from './version';
-export Children from './children';
-export createContext from './createContext';
-export createRef from './createRef';
+export {
+  createElement, cloneElement, isValidElement, createFactory,
+  useState, useContext, useEffect, useLayoutEffect, useRef, useCallback, useMemo, useReducer, useImperativeMethods,
+  createRef, Component, PureComponent, PropTypes, render, hydrate, createPortal, createContext, version,
+  findDOMNode, unmountComponentAtNode, findComponentInstance, setNativeProps, Children
+};
 
+export default {
+  createElement, cloneElement, isValidElement, createFactory,
+  useState, useContext, useEffect, useLayoutEffect, useRef, useCallback, useMemo, useReducer, useImperativeMethods,
+  createRef, Component, PureComponent, PropTypes, render, hydrate, createPortal, createContext, version,
+  findDOMNode, unmountComponentAtNode, findComponentInstance, setNativeProps, Children
+};
