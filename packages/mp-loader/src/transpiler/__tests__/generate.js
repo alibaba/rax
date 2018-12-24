08cfc2a96d3718404d5b09c9d8686ab775efe4d2
卓凌 <zhuoling.lcl@alibaba-inc.com>
2018-12-24 21:48:50 +0800

    test: add mp loader test for template builder
    fix: no default space between tags

---
+++ packages/mp-loader/src/transpiler/__tests__/generate.js
@@ -0,0 +1,10 @@
+const generate = require('../generate');
+const modules = require('../transpileModules');
+
+const transpilerOptions = { modules };
+
+describe('Transpiler generate', () => {
+  it('should use a:for with other rules', () => {
+
+  });
+});
