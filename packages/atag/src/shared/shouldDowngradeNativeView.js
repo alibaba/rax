b51de758f913ba6db87884efdab61825db1c5f43
卓凌 <zhuoling.lcl@alibaba-inc.com>
2018-12-29 11:59:39 +0800

    fix: atag native view downgrade

--- packages/atag/src/shared/shouldDowngradeNativeView.js
+++ packages/atag/src/shared/shouldDowngradeNativeView.js
@@ -1,6 +1,7 @@
 /* global __embed_view_manager__ */
 import getEnvironmentObject from './getEnvironmentObject';
 
+const IOS8_REG = /iPhone OS 8/;
 const isIOS = /iPhone|iPod|iPad/i.test(navigator.userAgent);
 const embedViewManager =
   typeof __embed_view_manager__ !== 'undefined' ? __embed_view_manager__ : null;
@@ -12,7 +13,15 @@ const embedViewManager =
  */
 export default function shouldDowngradeNativeView() {
   if (isIOS) {
-    return embedViewManager ? embedViewManager.shouldDowngrade() : false;
+    /**
+     * iOS8 not support native view render.
+     */
+    const isIOS8 = IOS8_REG.test(navigator.userAgent);
+    if (isIOS8) {
+      return true;
+    } else {
+      return embedViewManager ? embedViewManager.shouldDowngrade() : false;
+    }
   } else {
     const env = getEnvironmentObject();
     // `isDowngrade` may be string or boolean, due to version of Apps.
