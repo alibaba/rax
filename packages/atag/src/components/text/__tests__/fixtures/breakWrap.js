29623cc8be8760c4d9965828681d63328e05fe1c
卓凌 <zhuoling.lcl@alibaba-inc.com>
2019-01-11 12:09:33 +0800

    test: add test fixture for break

---
+++ packages/atag/src/components/text/__tests__/fixtures/breakWrap.js
@@ -0,0 +1,27 @@
+const timeout = 10e3; // 10s
+/**
+ * Fix text to replace both '\n' and '\\n' to <br />
+ */
+describe('Text fixture break warp.', () => {
+  let page;
+  beforeAll(async() => {
+    page = await global.__BROWSER__.newPage();
+    await page.goto('http://localhost:9002/components/text/__tests__/index.html');
+  }, timeout);
+
+  it ('should replace \\n to br tag', async () => {
+    let text = await page.evaluate(() => {
+      const node = document.querySelector('.break-wrap-with-text-break');
+      return node.innerHTML;
+    });
+    expect(text).toEqual('This is a paragraph with <br> break lines.');
+  });
+
+  it ('should replace LF to br tag', async () => {
+    let text = await page.evaluate(() => {
+      const node = document.querySelector('.break-wrap-with-lf');
+      return node.innerHTML;
+    });
+    expect(text).toEqual('This is a paragraph with <br> break lines.');
+  });
+});
