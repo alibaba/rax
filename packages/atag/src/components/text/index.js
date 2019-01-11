36818bbad6308dedc95a2367a9b12023196f9d5a
卓凌 <zhuoling.lcl@alibaba-inc.com>
2019-01-11 18:58:51 +0800

    feat: atag text function

--- packages/atag/src/components/text/index.js
+++ packages/atag/src/components/text/index.js
@@ -1,4 +1,6 @@
 import { PolymerElement } from '@polymer/polymer';
+import { FlattenedNodesObserver } from '@polymer/polymer/lib/utils/flattened-nodes-observer';
+import debounce from '../../shared/debounce';
 
 const style = document.createElement('style');
 style.innerText = `
@@ -15,10 +17,85 @@ style.innerText = `
   }
 `;
 document.head.appendChild(style);
+const spaces = {
+  ensp: ' ',
+  emsp: ' ',
+  nbsp: ' ',
+};
+const continuousSpaceReg = /[\u0020\u2002\u2023\f\r\t\v]+/g;
+const spaceReg = /[\u0020\u2002\u2023\f\r\t\v]/g;
+const stripLFReg = /^\n|\n$/;
 
 export default class TextElement extends PolymerElement {
   static get is() {
     return 'a-text';
+  }
+
+  static get properties() {
+    return {
+      selectable: {
+        type: Boolean,
+        /**
+         * Reflect to attribute to apply CSS style.
+         */
+        reflectToAttribute: true,
+        value: false,
+      },
+      /**
+       * Allow continuous spaces
+       *  @enum space: false, ensp, emsp, nbsp
+       */
+      space: {
+        type: String,
+        value: 'false',
+      },
+    }
+  }
+
+  constructor() {
+    super();
+    this._observeChildren = debounce(this._observeChildren);
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.childrenObserver = new FlattenedNodesObserver(this, this._observeChildren);
+
+    if (this.childNodes.length > 0) {
+      for (let i = 0, l = this.childNodes.length; i < l; i++) {
+        if (this.childNodes[i].nodeType === document.TEXT_NODE) {
+          this.childNodes[i].textContent = this._processText(this.childNodes[i].textContent);
+        }
+      }
+    }
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    this.childrenObserver.disconnect();
+  }
+
+  _processText(text) {
+    text = text.replace(stripLFReg, '')
+    if (this.space === 'false') {
+      // by default space equal false
+      return text.replace(continuousSpaceReg, ' ');
+    } else {
+      const space = spaces[this.space];
+      if (space === undefined) return text; // illegal space perperty
+      return text.replace(spaceReg, space);
+    }
+  }
+
+  _observeChildren({ addedNodes, target }) {
+    if (this === target && addedNodes.length > 0) {
+      for (let i = 0; i < addedNodes.length; i++) {
+        const node = addedNodes[i];
+        if (node.nodeType === document.TEXT_NODE) {
+          node.textContent = this._processText(node.textContent);
+        }
+      }
+    }
   }
 }
 
