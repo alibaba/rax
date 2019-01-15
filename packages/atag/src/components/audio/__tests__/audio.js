ceefb512d90bc64a1561988ca10b97ccd2879c4b
卓凌 <zhuoling.lcl@alibaba-inc.com>
2019-01-15 18:00:01 +0800

    test: demo for audio test

--- packages/atag/src/components/audio/__tests__/audio.js
+++ packages/atag/src/components/audio/__tests__/audio.js
@@ -1,20 +1,22 @@
 const timeout = 10000;
 
 describe('audio', () => {
-  let page;
-  beforeAll(async() => {
-    page = await global.__BROWSER__.newPage();
-    await page.goto('http://localhost:9002/components/audio/__tests__/index.html');
-  }, timeout);
+    let page;
+    beforeAll(async() => {
+      page = await global.__BROWSER__.newPage();
+      await page.goto('http://localhost:9002/components/audio/__tests__/index.html');
+    }, timeout);
 
-  afterAll(async() => {
-    await page.close();
-  });
+    afterAll(async() => {
+      await page.close();
+    });
 
-  it('should load without error', async() => {
-    let title = await page.evaluate(() => document.title);
-    expect(title).toContain('Atag Demo');
-  });
-},
-timeout
+    it('should load without error', async() => {
+      const audioConstructor = await page.evaluate(() => {
+        return document.getElementById('audio').constructor.name;
+      });
+      expect(audioConstructor).toEqual('Audio');
+    });
+  },
+  timeout
 );
