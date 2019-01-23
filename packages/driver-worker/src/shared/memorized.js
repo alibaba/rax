73d0899f85e0c7b70666489a58dd4f68cc23b75b
卓凌 <zhuoling.lcl@alibaba-inc.com>
2019-01-23 22:18:07 +0800

    test: add test for memorized

--- packages/driver-worker/src/shared/memorized.js
+++ packages/driver-worker/src/shared/memorized.js
@@ -1,3 +1,4 @@
+const hasOwnProperty = Object.prototype.hasOwnProperty;
 /**
  * Create a memorized version of pure function.
  * @note arguments must be stringify, object or function is not acceptable.
@@ -7,7 +8,8 @@ export default function memorized(fn) {
   const cache = Object.create(null);
   return function cachedFn(...args) {
     const cacheKey = args.join('|');
-    let hit = cache[cacheKey];
-    return hit || (cache[cacheKey] = fn.apply(this, args));
+    return hasOwnProperty.call(cache, cacheKey)
+      ? cache[cacheKey]
+      : cache[cacheKey] = fn.apply(this, args);
   };
 }
