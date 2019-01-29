486c5f311c3e7379f63ab2fe5d17bf811a7ef88e
卓凌 <zhuoling.lcl@alibaba-inc.com>
2019-01-29 21:08:04 +0800

    test: and fix canvas element

---
+++ packages/atag/src/components/canvas/__tests__/canvas.js
@@ -0,0 +1,33 @@
+describe('canvas', () => {
+  const pages = [];
+
+  async function createPage() {
+    const page = await global.__BROWSER__.newPage();
+    await page.setViewport(global.__VIEWPORT__);
+    await page.goto('http://localhost:9002/components/canvas/__tests__/canvas.html');
+    pages.push(page);
+    return page;
+  }
+
+  afterAll(async () => {
+    for (let i = 0, l = pages.length; i < l; i++) {
+      await pages[i].close();
+    }
+  });
+
+  it('default size', async() => {
+    const page = await createPage();
+    // Default width/height is 300/225
+    expect(await page.$eval('#canvas', el => el.width)).toEqual('300');
+    expect(await page.$eval('#canvas', el => el.height)).toEqual('225');
+  });
+
+  it('getContext', async() => {
+    const page = await createPage();
+    // Default width/height is 300/225\
+    const context2d = await page.evaluate(`
+      document.getElementById('canvas').getContext()
+    `);
+    expect(context2d).not.toBeNull();
+  });
+});
