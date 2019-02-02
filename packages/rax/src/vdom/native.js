dba1fe41a086294df9d1ae315dcb93f469da6b56
元彦 <yuanyan.cao@alibaba-inc.com>
2019-02-02 17:25:47 +0800

    refactor: not need removeAllEventListener in driver

--- packages/rax/src/vdom/native.js
+++ packages/rax/src/vdom/native.js
@@ -118,7 +118,6 @@ class NativeComponent {
       if (!notRemoveChild) {
         Host.driver.removeChild(this._nativeNode, this._parent);
       }
-      Host.driver.removeAllEventListeners(this._nativeNode);
     }
 
     this.unmountChildren(notRemoveChild);
