ff3b170fef0244729778822486a5568f70d4c2f2
zhengwang.z <zhengwang2314@gmail.com>
2019-02-28 12:17:52 +0800

    test: add animation tests

--- packages/atag/src/components/animation/__tests__/animation.js
+++ packages/atag/src/components/animation/__tests__/animation.js
@@ -48,9 +48,6 @@ describe('animation', () => {
       expect(await page.$eval('#animation', el => el.timeline.currentValue.list.map(num => {
         return Math.round(num * 10) / 10;
       }))).toEqual([0.2, 0.4, 0.8]);
-      const animationFirstChild = await page.$('#animation-first-child');
-      const text = await page.evaluate(node => node.innerHTML, animationFirstChild);
-      expect(await page.$eval('#animation-first-child', el => el)).toEqual(null);
     }, 200);
 
     setTimeout(async() => {
@@ -68,5 +65,37 @@ describe('animation', () => {
       expect(await page.$eval('#animation', el => el.timeline.currentValue.color)).toEqual('green');
       done();
     }, 1200);
+  });
+
+  it('default animation pan props', async() => {
+    const page = await createPage();
+
+    expect(await page.$eval('#animation-pan', el => el.eventType)).toEqual('pan');
+  });
+
+  it('default animation button props', async() => {
+    const page = await createPage();
+
+    expect(await page.$eval('#animation-button', el => el.eventOrigin)).toEqual('button');
+    expect(await page.$eval('#animation-button', el => el.eventType)).toEqual('click');
+    expect(await page.$eval('#animation-button', el => el.timeline.iterations)).toEqual(2);
+    expect(await page.$eval('#animation-button', el => el.timeline.totalDuration)).toEqual(5);
+    expect(await page.$eval('#animation-button', el => el.timeline.easing)).toEqual('ease-in');
+  });
+
+  it('animation button click', async(done) => {
+    const page = await createPage();
+    const button = await page.$('#button');
+    const buttonBox = await page.$('#animation-button-box');
+    const buttonText = await page.$('#animation-button-text');
+    const rect = await button.boundingBox();
+
+    await page.mouse.click(rect.x + rect.width / 2, rect.y + rect.height / 2);
+    setTimeout(async() => {
+      expect(await page.$eval('#animation-button', el => el.timeline.currentValue.bgColor)).toEqual('rgba(254, 255, 1, 1)');
+      expect(await page.$eval('#animation-button', el => Math.round(el.timeline.currentValue.width * 10) / 10)).toEqual(0.3);
+      expect(await page.$eval('#animation-button', el => Math.round(el.timeline.currentValue.opacity * 1000) / 1000)).toEqual(0.003);
+      done();
+    }, 210);
   });
 });
