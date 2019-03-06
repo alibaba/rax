30fba1f9f3e7f898baf08520b1c2b8eaed573d51
yacheng.sz <yacheng.sz@taobao.com>
2019-03-06 15:44:50 +0800

    fix: test

--- packages/rax-cli/src/generator/component/src/index.js
+++ packages/rax-cli/src/generator/component/src/index.js
@@ -1,8 +1,4 @@
 import {Component, createElement, PropTypes, render} from 'rax';
-import {isWeex, isWeb} from 'universal-env';
-import View from 'rax-view';
-import Text from 'rax-text';
-import Image from 'rax-image';
 
 const styles = {
   container: {
@@ -19,9 +15,9 @@ const styles = {
 export default class extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.containerBody}>hello world</Text>
-      </View>
+      <div style={styles.container}>
+        <div style={styles.containerBody}>hello world</div>
+      </div>
     );
   }
 }
