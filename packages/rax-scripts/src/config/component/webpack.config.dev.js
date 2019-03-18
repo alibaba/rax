4a1ab607754559ba85d12e068c03ba2602f6d155
yacheng.sz <yacheng.sz@taobao.com>
2019-03-18 10:40:03 +0800

    feat: merge master

--- packages/rax-scripts/src/config/component/webpack.config.dev.js
+++ packages/rax-scripts/src/config/component/webpack.config.dev.js
@@ -1,37 +1,24 @@
 'use strict';
 
 /* eslint no-console: 0 */
-const address = require('address');
-const qrcode = require('qrcode-terminal');
 const webpack = require('webpack');
 const webpackMerge = require('webpack-merge');
 
 const pathConfig = require('../path.config');
 const webpackConfigBase = require('./webpack.config.base');
-const envConfig = require('../env.config');
-
-const ipv4 = address.ip();
-const port = envConfig.port;
-const webUrl = 'http://' + ipv4 + ':' + port;
-const bundleUrl = 'http://' + ipv4 + ':' + port + '/js/index.bundle.js';
-const weexBundleUrl = `${bundleUrl}?_wx_tpl=${bundleUrl}`;
-
-qrcode.generate(webUrl, { small: true });
-console.log('Web: scan above QRCode ' + webUrl + ' or direct open in browser.\n');
-
-qrcode.generate(weexBundleUrl, { small: true });
-console.log('Weex: scan above QRCode ' + weexBundleUrl + ' use weex playground.\n');
 
 // enable source map
 const webpackConfigDev = webpackMerge(webpackConfigBase, {
   devtool: 'inline-module-source-map',
   entry: {
-    'index.bundle': [pathConfig.componentIndexJs],
+    index: [pathConfig.appIndexJs],
   },
   output: {
     publicPath: '/',
   },
-  plugins: [new webpack.NoEmitOnErrorsPlugin()], // Only work on web
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+  ],
 });
 
 Object.keys(webpackConfigDev.entry).forEach((point) => {
