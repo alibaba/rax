c7ddae305fe7d0135bc94e1066501ff89c8c1b4b
卓凌 <zhuoling.lcl@alibaba-inc.com>
2019-03-21 12:12:42 +0800

    feat: bind property methods

--- packages/transform-jsx-mp/helpers/component.js
+++ packages/transform-jsx-mp/helpers/component.js
@@ -1,32 +1,73 @@
-const lifecycleMapping = {
-  // componentWillMount: '',
+const INSTANCE = '__rax_instance__';
+const CYCLE_MAP = {
   componentDidMount: 'didMount',
   componentWillReceiveProps: 'didUpdate',
   componentWillMount: 'didUnmount',
 };
 
+function hasOwn(object, prop) {
+  return Object.hasOwnProperty.call(object, prop);
+}
+
+/**
+ * Bridge setState directly to setData. `this` is bound to component instance.
+ * @param particialState {Object|Function}
+ * @param callback? {Function}
+ */
+function setState(particialState, callback) {
+  return this.setData(particialState, callback);
+}
+
+/**
+ * Bridge to forceUpdate.
+ * @param callback? {Function}
+ */
+function forceUpdate(callback) {
+  return setState.call(this, {}, callback);
+}
+
+/**
+ * Bridge from RaxComponent Klass to MiniApp Component constructor.
+ * @param Klass {RaxComponent}
+ * @return {Object} MiniApp Component constructor's first arg.
+ */
 export function createComponent(Klass) {
-  const instance = new Klass();
-  const klassPrototype = Klass.prototype;
-  const componentConfig = {
-    mixins: Klass.mixins,
-    props: Klass.defaultProps,
+  const { prototype: klassPrototype, mixins, defaultProps } = Klass;
+
+  /**
+   * Expose config of component.
+   */
+  const config = {
+    mixins,
+    props: defaultProps,
     data() {
-      return { ...instance.state };
+      const instance = new Klass({}, null);
+      config[INSTANCE] = instance;
+      return instance.state;
     },
-    methods: {},
+    methods: {
+      setState,
+      forceUpdate,
+    },
   };
 
-  const protoProperties = Object.keys(klassPrototype);
-  for (let i = 0, l = protoProperties.length; i < l; i++) {
-    const key = protoProperties[i];
-    if (typeof klassPrototype[key] === 'function') {
-      if (lifecycleMapping[key]) {
-        componentConfig[lifecycleMapping[key]] = klassPrototype[key];
+  /**
+   * Bind function class methods into methods or cycles.
+   */
+  const classMethods = Object.getOwnPropertyNames(klassPrototype);
+  for (let i = 0, l = classMethods.length; i < l; i++) {
+    const methodName = classMethods[i];
+    if ('constructor' === methodName) continue;
+
+    if (typeof klassPrototype[methodName] === 'function') {
+      const fn = klassPrototype[methodName];
+      if (hasOwn(CYCLE_MAP, methodName)) {
+        config[CYCLE_MAP[methodName]] = fn;
       } else {
-        componentConfig.methods[key] = klassPrototype[key];
+        config.methods[methodName] = fn;
       }
     }
   }
-  return componentConfig;
+
+  return config;
 }
