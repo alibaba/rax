1eadda14525404c668cfee71ca444ae35c4d2332
卓凌 <zhuoling.lcl@alibaba-inc.com>
2019-03-26 20:19:19 +0800

    test: add test for unsupported JSX stynax

--- packages/jsx2mp/transformer/__tests__/transformJSX.js
+++ packages/jsx2mp/transformer/__tests__/transformJSX.js
@@ -23,6 +23,40 @@ describe('transformJSX', () => {
       expect(jsCode).toEqual(getFileContent(expectedJSPath));
     });
   }
+
+  it('should throw error with JSX inline method ref.', () => {
+    expect(() => {
+      transformJSX(`
+        import { Component } from 'rax';
+        export default class extends Component {
+          render() {
+            return (
+              <view>
+                {foo.map(this.renderItem)
+              </view>
+            );
+          }
+        }
+      `);
+    }).toThrowError();
+  });
+
+  it('should throw error with JSX + IIFE', () => {
+    expect(() => {
+      transformJSX(`
+        import { Component } from 'rax';
+        export default class extends Component {
+          render() {
+            return (
+              <view>
+                {(function(fn) {fn()})(this.renderItem)
+              </view>
+            );
+          }
+        }
+      `);
+    }).toThrowError();
+  });
 });
 
 /**
