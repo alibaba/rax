e1fd3b9682fcc90d1811c58b59efee49c6d94a77
水澜 <shuilan.cj@taobao.com>
2019-03-27 15:12:31 +0800

    refactor: custom cli table style for better display in ci

--- scripts/bench/src/print.js
+++ scripts/bench/src/print.js
@@ -1,28 +1,46 @@
-const Table = require('cli-table');
-const chalk = require('chalk');
+const Table = require("cli-table");
+const chalk = require("chalk");
 
 module.exports = function(data, frameworks, benchmarks) {
-  const table = new Table();
+  const table = new Table({
+    chars: {
+      top: "",
+      "top-mid": "",
+      "top-left": "",
+      "top-right": "",
+      bottom: "",
+      "bottom-mid": "",
+      "bottom-left": "",
+      "bottom-right": "",
+      left: "",
+      "left-mid": "",
+      mid: " ",
+      "mid-mid": "",
+      right: "",
+      "right-mid": "",
+      middle: "│"
+    }
+  });
 
   Object.keys(data).map(type => {
     const rowHeader = [chalk.white.bold(type)];
-    frameworks.map((framework) => {
+    frameworks.map(framework => {
       rowHeader.push(chalk.white.bold(framework));
     });
     table.push(rowHeader);
 
     const benchmarksResult = data[type];
 
-    Object.keys(benchmarksResult).map((benchmark) => {
+    Object.keys(benchmarksResult).map(benchmark => {
       const result = benchmarksResult[benchmark];
 
       const benchmarkInfo = benchmarks[benchmark];
       const title = benchmarkInfo.label;
       const row = [chalk.gray(title)];
 
-      row[title] = frameworks.map((framework) => {
+      row[title] = frameworks.map(framework => {
         if (!result[framework]) {
-          row.push('-');
+          row.push("-");
         } else {
           const mean = result[framework].mean.toFixed(2);
           if (result[framework].warning) {
