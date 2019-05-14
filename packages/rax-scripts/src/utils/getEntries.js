8109e8fc0c3a3af3e1b8f5b9e7f5956c9834446f
水澜 <shuilan.cj@taobao.com>
2019-05-14 15:24:33 +0800

    feat: get entries for pwa

--- packages/rax-scripts/src/utils/getEntries.js
+++ packages/rax-scripts/src/utils/getEntries.js
@@ -1,7 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+const pathConfig = require('../config/path.config');
+
 module.exports = () => {
-  return {
-    'index': './src/pages/index.js',
-    // 'foo': 'pages/foo.js',
-    // 'bar': 'pages/bar.js'
-  };
+  const appDirectory = pathConfig.appDirectory;
+  const appSrc = pathConfig.appSrc;
+
+  const pages = {};
+
+  const files = fs.readdirSync(path.join(appSrc, 'pages'));
+  files.map((file) => {
+    const absolutePath = path.join(appSrc, 'pages', file);
+    const pathStat = fs.statSync(absolutePath);
+
+    if (pathStat.isDirectory()) {
+      const relativePath = path.relative(appDirectory, absolutePath);
+      pages[file] = './' + path.join(relativePath, 'index.js');
+    }
+  });
+
+  return pages;
 };
