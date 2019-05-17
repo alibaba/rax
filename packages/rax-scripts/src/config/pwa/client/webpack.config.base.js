bc8ad1726883bc6cbb06a17002d07b6cc094951a
yangfan <18767120422@163.com>
2019-05-17 19:02:34 +0800

    feat: add webpack demo loader

--- packages/rax-scripts/src/config/pwa/client/webpack.config.base.js
+++ packages/rax-scripts/src/config/pwa/client/webpack.config.base.js
@@ -1,12 +1,22 @@
 'use strict';
 
+const qs = require('querystring');
 const webpackMerge = require('webpack-merge');
 const webpackConfigBase = require('../webpack.config.base');
 const getEntries = require('../../../utils/getPWAEntries');
 
+const entryLoader = require.resolve('./entry.loader.js');
+
+let entry = {};
+const entries = getEntries();
+Object.keys(entries).forEach((key) => {
+  // TODO: read config file set ssr value
+  entry[key] = `${entryLoader}?${qs.stringify({ ssr: true })}!${entries[key]}`;
+});
+
 const webpackConfig = webpackMerge(webpackConfigBase, {
   target: 'web',
-  entry: getEntries(), // client loader
+  entry,
   output: {
     filename: 'client/[name].js'
   }
