ec023d7c7adb50c73121dc249750610f5198bf7d
jason.yf <jason.yf@alibaba-inc.com>
2019-05-20 15:26:53 +0800

    fix: loader undefined

--- packages/rax-scripts/src/config/pwa/client/webpack.config.base.js
+++ packages/rax-scripts/src/config/pwa/client/webpack.config.base.js
@@ -2,16 +2,15 @@
 
 const qs = require('querystring');
 const webpackMerge = require('webpack-merge');
-
-const ssrWebpackPlugin = require.resolve('rax-ssr-webpack-plugin');
 const webpackConfigBase = require('../webpack.config.base');
 const getEntries = require('../../../utils/getPWAEntries');
 
 let entry = {};
 const entries = getEntries();
+const entryLoader = require.resolve('rax-ssr-webpack-plugin/lib/EntryLoader');
 Object.keys(entries).forEach((key) => {
   // TODO: read config file set ssr value
-  entry[key] = `${ssrWebpackPlugin.entryLoader}?${qs.stringify({ ssr: true })}!${entries[key]}`;
+  entry[key] = `${entryLoader}?${qs.stringify({ ssr: true })}!${entries[key]}`;
 });
 
 const webpackConfig = webpackMerge(webpackConfigBase, {
