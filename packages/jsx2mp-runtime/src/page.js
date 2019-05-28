3639e896f3a60fb82e7f6c54cc9a37fb248eb4b5
ruiwang.wr <ruiwang.wr@alibaba-inc.com>
2019-05-28 14:18:34 +0800

    feat: page runtime
    Revert "feat: page runtime"
    
    This reverts commit e120a717b7e3ff46c630a71b9aa419d029dde763.
    
    refactor: git revert

--- packages/jsx2mp-runtime/src/page.js
+++ packages/jsx2mp-runtime/src/page.js
@@ -1,106 +1,198 @@
+import { MiniComponent } from './baseComponent';
+import Host from './host';
+import { Current } from './current-owner';
+
 const PAGE_EVENTS = [
-  'onBack',
-  'onKeyboardHeight',
+  'onShareAppMessage',
+  'onTitleClick',
   'onOptionMenuClick',
   'onPopMenuClick',
-  'onPullIntercept',
   'onPullDownRefresh',
-  'onTitleClick',
   'onTabItemTap',
+  'onPageScroll',
+  'onReachBottom',
   'beforeTabItemTap',
   'onResize'
 ];
-
-/**
- * Bridge from Rax page component class to MiniApp Component constructor.
- * @param Klass {RaxComponent} Rax page component.
- * @return {Object} MiniApp Page constructor's config.
- */
-export function createPage(Klass) {
-  const { prototype: klassPrototype, defaultProps } = Klass;
-  const props = klassPrototype.props = klassPrototype.props || {};
-  const instance = new Klass(props, null);
-  const data = Object.assign({}, defaultProps, props, instance.state);
-  let _internal; // AppX Page instance object.
-
-  function invokeMethod(method, args = []) {
-    if (typeof instance[method] === 'function') {
-      return instance[method].apply(instance, args);
-    }
-  }
-
-  const { methods, events } = getClassMethods(Klass);
-  const config = {
-    data,
-    events,
-    props: Object.assign({}, props, defaultProps),
-
-    /**
-     * Bridge setState directly to setData. `this` is bound to component instance.
-     * @param particialState {Object|Function}
-     * @param callback? {Function}
-     */
-    setState(particialState, callback) {
-      if (typeof instance.render === 'function') {
-        const dynamicState = instance.render();
-        return this.setData(Object.assign({}, particialState, dynamicState), callback);
-      } else {
-        return this.setData(particialState, callback);
-      }
-    },
-
-    /**
-     * Bridge to forceUpdate.
-     * @param callback? {Function}
-     */
-    forceUpdate(callback) {
-      return this.setState({}, callback);
-    },
-
-    onLoad(query) {
-      _internal = this;
-      props.query = query;
-
-      invokeMethod('componentWillMount');
-      this.forceUpdate(); // Call render.
-    },
-    onShow() {
-      invokeMethod('onShow');
-    },
-    onReady() {
-      invokeMethod('componentDidMount');
-    },
-    onHide() {
-      invokeMethod('onHide');
-    },
-    onUnload() {
-      invokeMethod('componentWillUnmount');
-    },
-    onReachBottom() {
-      invokeMethod('onReachBottom');
-    },
-    onShareAppMessage(options) {
-      return invokeMethod('onShareAppMessage', [options]);
-    },
+const SPECIAL_PAGE_EVENTS = ['onBack', 'onKeyboardHeight', 'onPullIntercept'];
+const PAGE_CICYLES = [
+  'componentDidMount',
+  'componentDidUpdate',
+  'componentWillMount',
+  'componentWillReceiveProps',
+  'componentWillUnmount',
+  'componentWillUpdate',
+  'shouldComponentUpdate'
+];
+function triggerCicyleEvent(instance, cicyleName, args = {}) {
+  instance[cicyleName] &&
+    typeof instance[cicyleName] === 'function' &&
+    instance[cicyleName].call(instance, ...args);
+}
+function bindEvents(args) {
+  const { componentConfig, handleEventsList } = args;
+  handleEventsList.forEach(eventName => {
+    processEvent(eventName, componentConfig);
+  });
+}
+function processEvent(eventHandlerName, obj) {
+  obj[eventHandlerName] = function(event) {
+    const scope = this.miniInstance;
+    const args = [];
+    // TODO: Analysis args from event function, need compile support
+    return scope[eventHandlerName].apply(scope, args);
   };
-
-  return Object.assign(config, methods);
+}
+function addLeadingSlash(path) {
+  return path.charAt(0) === '/' ? path : '/' + path;
 }
 
-function getClassMethods(Klass) {
-  const { prototype: klassPrototype } = Klass;
-  const classMethods = Object.getOwnPropertyNames(klassPrototype);
-  const events = {};
-  const methods = {};
-  for (let i = 0, l = classMethods.length; i < l; i++) {
-    const methodName = classMethods[i];
-    const fn = klassPrototype[methodName];
-    if ('constructor' === methodName) continue;
-    if (PAGE_EVENTS.indexOf(methodName) > -1) {
-      events[methodName] = fn;
-    } else if (typeof klassPrototype[methodName] === 'function') {
-      methods[methodName] = fn;
-    }
+function getCurrentPageUrl() {
+  // eslint-disable-next-line no-undef
+  const pages = getCurrentPages();
+  const currentPage = pages[pages.length - 1];
+  return addLeadingSlash(currentPage.route || currentPage.__route__);
+}
+/**
+ * bind scope from componentClass to miniApp and init global instance.
+ * @param options {options} miniApp page options.
+ * @param ComponentClass {ComponentClass} current componentClass.
+ * @param isClass {isClass} is a Class or a Function.
+ *
+ */
+function initInstance(options = {}, ComponentClass, isClass) {
+  if (isClass) {
+    this.miniInstance = new ComponentClass({});
+  } else {
+    this.miniInstance = ComponentClass;
   }
-  return { events, methods };
+  this.miniInstance.scopeInit(this);
+  this.miniInstance.miniRoute = {
+    params: options,
+    // eslint-disable-next-line no-undef
+    path: getCurrentPageUrl()
+  };
+  this.miniInstance._isReady = true;
+}
+/**
+ * synchronize methods in the rax component to the miniApp
+ * @param componentConfig {componentConfig} miniApp config.
+ * @param ComponentClass {ComponentClass} current componentClass.
+ * @param isClass {isClass} is a Class or a Function.
+ * @param baseComponent {baseComponent} function Component is necessary.
+ */
+function initMethods(componentConfig, componentClass, isClass, baseComponent) {
+  let classMethods = [];
+  let currentClass = {};
+  const handleEventsList = [];
+  if (isClass) {
+    classMethods = Object.getOwnPropertyNames(componentClass.prototype);
+    currentClass = componentClass;
+  } else {
+    classMethods = Object.keys(componentClass());
+    currentClass = baseComponent;
+  }
+  classMethods.forEach(methodName => {
+    if ('constructor' === methodName || PAGE_CICYLES.indexOf(methodName) > -1)
+      return;
+    const fn = isClass
+      ? componentClass.prototype[methodName]
+      : componentClass()[methodName];
+    if (PAGE_EVENTS.indexOf(methodName) > -1) {
+      componentConfig[methodName] = fn;
+    } else if (SPECIAL_PAGE_EVENTS.indexOf(methodName) > -1) {
+      componentConfig.events[methodName] = fn;
+    } else {
+      isClass
+        ? componentClass[methodName] = fn
+        : baseComponent[methodName] = fn;
+      handleEventsList.push(methodName);
+    }
+  });
+  return handleEventsList;
+}
+/**
+ * Bridge from Rax page component class to MiniApp Component constructor.
+ * @param ComponentClass {RaxComponent} Rax page component.
+ * @return {Object} MiniApp Page constructor's config.
+ */
+export function createPage(ComponentClass) {
+  const isClass = ComponentClass.prototype.isClassComponent;
+  let handleEventsList = [];
+  if (isClass) {
+    const componentInstance = new ComponentClass({});
+    const { prototype: componentPrototype, defaultProps } = ComponentClass;
+    const { props, state } = componentInstance;
+    const classMethods = Object.getOwnPropertyNames(componentPrototype);
+    const initData = Object.assign({}, defaultProps, props, state);
+    const componentConfig = {
+      data: initData,
+      state: state,
+      props: Object.assign({}, props, defaultProps),
+      events: {}
+    };
+    handleEventsList = initMethods(componentConfig, ComponentClass, isClass);
+    Object.assign(componentConfig, {
+      onLoad(options = {}) {
+        initInstance.apply(this, [options, ComponentClass, isClass]);
+        triggerCicyleEvent(this.miniInstance, 'componentWillMount');
+      },
+      onReady() {
+        triggerCicyleEvent(this.miniInstance, 'componentDidMount');
+      },
+      onUnload() {
+        triggerCicyleEvent(this.miniInstance, 'componentWillUnmount');
+      },
+      onShow() {
+        triggerCicyleEvent(this.miniInstance, 'componentDidShow');
+      },
+      onHide() {
+        triggerCicyleEvent(this.miniInstance, 'componentDidHide');
+      }
+    });
+    bindEvents({
+      componentConfig,
+      handleEventsList
+    });
+    return componentConfig;
+  } else {
+    const baseComponent = new MiniComponent();
+    const componentClass = ComponentClass;
+    Host.current = baseComponent;
+    const componentConfig = {
+      events: {},
+      data: baseComponent._state
+    };
+    const classMethods = Object.keys(componentClass());
+    handleEventsList = initMethods(
+      componentConfig,
+      ComponentClass,
+      isClass,
+      baseComponent
+    );
+    Object.assign(componentConfig, {
+      onLoad(options = {}) {
+        this.miniInstance = Object.assign(...componentClass(), baseComponent);
+        initInstance.apply(this, [options, this.miniInstance, isClass]);
+        triggerCicyleEvent(this.miniInstance, 'componentWillMount');
+      },
+      onReady() {
+        triggerCicyleEvent(this.miniInstance, 'componentDidMount');
+      },
+      onUnload() {
+        triggerCicyleEvent(this.miniInstance, 'componentWillUnmount');
+      },
+      onShow() {
+        triggerCicyleEvent(this.miniInstance, 'componentDidShow');
+      },
+      onHide() {
+        triggerCicyleEvent(this.miniInstance, 'componentDidHide');
+      }
+    });
+    bindEvents({
+      componentConfig,
+      handleEventsList
+    });
+    return componentConfig;
+  }
 }
