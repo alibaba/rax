5c29d10b9ce633aa3e8e8bb3e0f132b5078a3b90
卓凌 <zhuoling.lcl@alibaba-inc.com>
2019-06-03 16:05:37 +0800

    fix: identifier event handlers

--- packages/jsx-compiler/src/modules/element.js
+++ packages/jsx-compiler/src/modules/element.js
@@ -50,7 +50,13 @@ function transformTemplate(ast) {
       case 'Identifier': {
         if (node.expression.name === 'undefined') {
           path.replaceWith(t.stringLiteral(createBinding(node.expression.name)));
-        } else {
+        } else if (isEventHandler(genExpression(parentPath.node.name))) {
+          // <tag onClick={handleClick} />
+          // => <tag onClick="_e0" />
+          const id = applyEventHandler();
+          dynamicValue[id] = node.expression;
+          path.replaceWith(t.stringLiteral(id));
+        } else  {
           const id = applyDynamicValue();
           dynamicValue[id] = node.expression;
           path.replaceWith(t.stringLiteral(createBinding(id)));
