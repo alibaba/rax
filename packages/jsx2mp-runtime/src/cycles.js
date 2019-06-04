ca5a149106600df12800cbf7da8e904afc165d41
卓凌 <zhuoling.lcl@alibaba-inc.com>
2019-06-04 15:13:11 +0800

    refactor: jsx2mp-runtime

---
+++ packages/jsx2mp-runtime/src/cycles.js
@@ -0,0 +1,9 @@
+export const COMPONENT_WILL_MOUNT = 'componentWillMount';
+export const RENDER = 'render';
+export const COMPONENT_DID_MOUNT = 'componentDidMount';
+export const COMPONENT_WILL_UNMOUNT = 'componentWillUnmount';
+export const COMPONENT_WILL_RECEIVE_PROPS = 'componentWillReceiveProps';
+export const COMPONENT_WILL_UPDATE = 'componentWillUpdate';
+export const COMPONENT_DID_UPDATE = 'componentDidUpdate';
+export const ON_SHOW = 'onShow';
+export const ON_HIDE = 'onHide';
