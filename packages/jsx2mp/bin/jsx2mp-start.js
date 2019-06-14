7751ad0e41e1b16ef713cc1818571ed231e4a993
ZeroLing <i@zeroling.com>
2019-06-14 14:51:28 +0800

    feat: add build component for jsx2mp (#1090)

--- packages/jsx2mp/bin/jsx2mp-start.js
+++ packages/jsx2mp/bin/jsx2mp-start.js
@@ -3,12 +3,20 @@ const { resolve } = require('path');
 const program = require('commander');
 
 program
+  .option('--entry <entry>', 'set entry of component', 'index')
+  .option('--type <type>', 'set type of project|component', 'project')
+  .option('--dist <dist>', 'set export path', 'dist')
   .action((cmd) => {
     const workDirectory = resolve(process.env.CWD || process.cwd());
-    const distDirectory = resolve(workDirectory, 'dist');
-
+    const distDirectory = resolve(workDirectory, cmd.dist);
     const enableWatch = true;
-    require('..')(workDirectory, distDirectory, enableWatch);
+
+    require('..')(workDirectory, distDirectory, {
+      enableWatch,
+      type: cmd.type,
+      dist: cmd.dist,
+      entry: cmd.entry,
+    });
   });
 
 program.parse(process.argv);
