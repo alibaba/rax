46db12631a271823a231b4032ac3cdc5395e6877
yangfan <18767120422@163.com>
2019-06-19 13:44:52 +0800

    fix: remove unnecessary util

--- packages/rax-scripts/src/utils/getAppConfig.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const pathConfig = require('../config/path.config');
-
-module.exports = () => {
-  if (fs.existsSync(pathConfig.appConfig)) {
-    return require(pathConfig.appConfig);
-  }
-};
