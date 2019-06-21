e16f708bdd3a95f3f789b10810ddfa4c7499b851
元彦 <yuanyan.cao@alibaba-inc.com>
2019-06-21 17:02:49 +0800

    chore: remove weex template in cli

--- packages/rax-cli/src/generator/weexapp/src/index.jsx
+++
@@ -1,5 +0,0 @@
-import {createElement, render} from 'rax';
-import * as DriverWeex from 'driver-weex';
-import App from './App';
-
-render(<App name="world" />, DriverWeex.createBody(), { driver: DriverWeex });
