d7ef60d751e45dd4f521478c1c506a423f3f8839
猫橘 <changhui.qch@alibaba-inc.com>
2019-06-26 10:26:10 +0800

    fix: remove space

--- packages/rax-cli/src/generator/component/src/weex/index.tsx
+++ packages/rax-cli/src/generator/component/src/weex/index.tsx
@@ -2,7 +2,7 @@ import { createElement } from 'rax';
 import { Props } from '../types';
 
 const MyComponent = (props: Props) => {
-  return <text {...props} >Hello World</text>;
+  return <text {...props}>Hello World</text>;
 };
 
 export default MyComponent;
