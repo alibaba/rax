c8ab6051caaaa3218e06d6657f3046110f7be24f
激辣 <wc178711@alibaba-inc.com>
2019-06-26 12:08:41 +0800

    fix: react  compatibility

--- packages/rax-dom/src/index.js
+++ packages/rax-dom/src/index.js
@@ -1,21 +1,31 @@
 import { render } from 'rax';
-import DriverDOM from 'driver-dom';
+import * as DriverDOM from 'driver-dom';
 import hydrate from 'rax-hydrate';
 import unmountComponentAtNode from 'rax-unmount-component-at-node';
 import findDOMNode from 'rax-find-dom-node';
 import createPortal from 'rax-create-portal';
 
-export default {
-  render: (element, container, callback) => render(
-    element,
-    container,
-    {
-      driver: DriverDOM
-    },
-    callback
-  ),
+const raxDomRender = (element, container, callback) => render(
+  element,
+  container,
+  {
+    driver: DriverDOM,
+  },
+  callback,
+);
+
+export {
+  raxDomRender as render,
   hydrate,
   unmountComponentAtNode,
   findDOMNode,
-  createPortal
+  createPortal,
+};
+
+export default {
+  render: raxDomRender,
+  hydrate,
+  unmountComponentAtNode,
+  findDOMNode,
+  createPortal,
 };
