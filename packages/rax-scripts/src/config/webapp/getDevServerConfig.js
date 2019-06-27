83804eedbc6010bb9b60237f9448b40dd402067f
水澜 <shuilan.cj@taobao.com>
2019-06-27 14:24:47 +0800

    fix: duplicate genEtries

--- packages/rax-scripts/src/config/webapp/getDevServerConfig.js
+++ packages/rax-scripts/src/config/webapp/getDevServerConfig.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const pathConfig = require('../path.config');
 const appConfig = require('../app.config');
-const getEntries = require('../../utils/getEntries');
+const { getEntries } = require('../index');
 
 module.exports = function getDevServerConfig() {
   const appBuild = pathConfig.appBuild;
