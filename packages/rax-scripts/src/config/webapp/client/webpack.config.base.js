a69a4dee3d1e2f6391e74f164648cacc853da4e4
水澜 <shuilan.cj@taobao.com>
2019-07-03 10:32:03 +0800

    fix: assets load in ssr (#1119)
    * fix: assets in ssr
    
    * fix: dev server config
    
    * fix: duplicate genEtries
    
    * chore: update version
    
    * refactor: save assets manifest to .temp
    
    * chore: unused dependencies
    
    * chore: rename
    
    * refactor: map->foreach
    
    * fix: dependencies
    
    * chore: add temp to gitignore
    
    * refactor: map->foreach
    
    * docs: add readme for ssr-dev-server

--- packages/rax-scripts/src/config/webapp/client/webpack.config.base.js
+++ packages/rax-scripts/src/config/webapp/client/webpack.config.base.js
@@ -1,12 +1,14 @@
 'use strict';
 
+const fs = require('fs');
 const qs = require('querystring');
 const webpackMerge = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-const pathConfig = require('../../path.config');
+const AssetsManifestPlugin = require('rax-pwa-webpack-plugin/lib/AssetsManifestPlugin').default;
 const getWebpackConfigBase = require('../webpack.config.base');
+const pathConfig = require('../../path.config');
 const appConfig = require('../../app.config');
+const envConfig = require('../../env.config');
 const { getEntries } = require('../../index');
 
 const ClientLoader = require.resolve('rax-pwa-webpack-plugin/lib/ClientLoader');
@@ -28,13 +30,22 @@ const webpackConfigBase = getWebpackConfigBase({
   target: 'web'
 });
 
+if (!fs.existsSync(pathConfig.tempDirectory)) {
+  fs.mkdirSync(pathConfig.tempDirectory);
+}
+
 const webpackConfig = webpackMerge(webpackConfigBase, {
   target: 'web',
   entry: entryMap,
   output: {
     filename: 'client/[name].js'
   },
-  plugins: appConfig.ssr ? [] : [
+  plugins: appConfig.ssr ? [
+    new AssetsManifestPlugin({
+      dist: pathConfig.assetsManifest,
+      publicPath: envConfig.publicPath
+    })
+  ] : [
     // Generates an `index.html` file with the <script> injected.
     new HtmlWebpackPlugin({
       inject: true,
--- packages/rax-ssr-dev-server/index.js
+++ packages/rax-ssr-dev-server/index.js
@@ -1,17 +1,12 @@
 const fs = require('fs');
-const path = require('path');
 const express = require('express');
 const RaxServer = require('rax-server');
 const devMiddleware = require('webpack-dev-middleware');
 const hotMiddleware = require('webpack-hot-middleware');
 
-const { getEntries } = require('../config/');
-const appConfig = require('../config/app.config');
-const envConfig = require('../config/env.config');
-const pathConfig = require('../config/path.config');
-
 class DevServer {
-  constructor(compiler) {
+  constructor(compiler, options) {
+    this.options = options;
     this.setupApp(compiler);
   }
 
@@ -23,23 +18,25 @@ class DevServer {
 
     const server = new RaxServer();
 
-    const entries = getEntries();
+    const {
+      pagesManifest,
+    } = this.options;
 
-    Object.keys(entries).map(entry => {
+    Object.keys(pagesManifest).forEach(page => {
       // _document, _shell
-      if (entry.indexOf('_') > -1) {
+      if (page.indexOf('_') > -1) {
         return;
       }
 
-      router.get(`/${entry}`, (req, res) => {
-        const pageConfig = this.getPageConfig(res, entry, entries);
+      router.get(`/${page}`, (req, res) => {
+        const pageConfig = this.getPageConfig(res, page);
         server.render(req, res, pageConfig);
       });
     });
 
-    if (entries.index) {
+    if (pagesManifest.index) {
       router.get('/', (req, res) => {
-        const pageConfig = this.getPageConfig(res, 'index', entries);
+        const pageConfig = this.getPageConfig(res, 'index');
         server.render(req, res, pageConfig);
       });
     }
@@ -67,35 +64,51 @@ class DevServer {
     this.app.listen(port, callback);
   }
 
-  getPageConfig(res, page, entries) {
-    const pageConfig = {
-      page,
-      component: this.loadComponent(page, res),
-      scripts: [`${envConfig.publicPath}client/${page}.js`],
-      document: {
-        title: appConfig.title
-      }
+  getPageConfig(res, page) {
+    const {
+      appConfig = {},
+      pagesManifest,
+      assetsManifest,
+      assetsManifestPath
+    } = this.options;
+
+    let assets = assetsManifest || {};
+    if (assetsManifestPath) {
+      const assetsContent = fs.readFileSync(assetsManifestPath, res);
+      assets = JSON.parse(assetsContent);
     };
 
-    if (entries._shell) {
-      pageConfig.shell = {
-        component: this.loadComponent('_shell', res)
-      };
-    }
-
-    if (entries._document) {
-      pageConfig.document.component = this.loadComponent('_document', res);
-    }
+    const pageConfig = {
+      page,
+      ...assets[page],
+      component: this.loadComponent(page, res),
+      document: {
+        title: appConfig.title,
+        component: pagesManifest._document ? this.loadComponent('_document', res) : null
+      },
+      shell: {
+        component: pagesManifest._shell ? this.loadComponent('_shell', res) : null
+      }
+    };
 
     return pageConfig;
   }
 
   loadComponent(page, res) {
-    const fs = res.locals.fs;
-    const bundlePath = path.join(pathConfig.appBuild, 'server', `${page}.js`);
-    const bundleContent = fs.readFileSync(bundlePath, 'utf8');
+    const {
+      pagesManifest
+    } = this.options;
+
+    const bundlePath = pagesManifest[page];
+    const bundleContent = this.readFileSyncFromWebpack(bundlePath, res);
     const mod = eval(bundleContent);
+
     return interopDefault(mod);
+  }
+
+  readFileSyncFromWebpack(filePath, res) {
+    const fs = res.locals.fs;
+    return fs.readFileSync(filePath, 'utf8');
   }
 }
 
