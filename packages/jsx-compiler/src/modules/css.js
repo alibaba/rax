0fa23e3ed90e1009f4ca71854fcba6442e93f8ec
卓凌 <zhuoling.lcl@alibaba-inc.com>
2019-07-04 16:27:44 +0800

    feat: support compile css file to css object

--- packages/jsx-compiler/src/modules/css.js
+++ packages/jsx-compiler/src/modules/css.js
@@ -1,12 +1,22 @@
 const t = require('@babel/types');
 const { readFileSync } = require('fs-extra');
+const { relative } = require('path');
+const stylesheetLoader = require('stylesheet-loader');
 const moduleResolve = require('../utils/moduleResolve');
 const traverse = require('../utils/traverseNodePath');
 
 /**
  * Add and convert style file to result.
  * 1. convert `rem` directly to `rpx`
- * 2. add `style` to result object.
+ * 2. if named imported:
+ *    import styles from './style.css';
+ *    ->
+ *    Transform and generate style.css to style.css.js
+ * 3. if anoymous imported
+ *    import './style.css';
+ *    ->
+ *    Transform rpx and generate same name css files.
+ * ---
  */
 module.exports = {
   parse(parsed, code, options) {
@@ -18,12 +28,14 @@ module.exports = {
       if (isFilenameCSS(rawPath)) {
         const resolvedPath = moduleResolve(options.filePath, rawPath);
         if (resolvedPath) {
-          cssFileMap[rawPath] = true;
+          const isAnomyousImport = imported[rawPath].length === 0;
           parsed.cssFiles.push({
             rawPath,
             filename: resolvedPath,
             content: readFileSync(resolvedPath, 'utf-8'),
+            type: isAnomyousImport ? 'cssFile' : 'cssObject',
           });
+          if (isAnomyousImport) cssFileMap[rawPath] = true; // For removing import statement.
         }
       }
     });
@@ -40,9 +52,15 @@ module.exports = {
   },
   generate(ret, parsed, options) {
     ret.style = ret.style || '';
+    ret.assets = ret.assets || {};
     if (parsed.cssFiles) {
       parsed.cssFiles.forEach((cssFile) => {
-        ret.style += convertCSSUnit(cssFile.content);
+        if (cssFile.type === 'cssObject') {
+          const relativePath = relative(options.cwd, cssFile.filename);
+          ret.assets[relativePath + '.js'] = createCSSModule(cssFile.content);
+        } else if (cssFile.type === 'cssFile') {
+          ret.style += convertCSSUnit(cssFile.content);
+        }
       });
     }
   },
@@ -55,4 +73,9 @@ function isFilenameCSS(path) {
 function convertCSSUnit(raw, originExt = 'rem', targetExt = 'rpx') {
   const regexp = new RegExp(originExt, 'g');
   return raw.replace(regexp, targetExt); // Maybe could use postcss plugin instead.
+}
+
+function createCSSModule(content) {
+  const loaderContext = {};
+  return stylesheetLoader.call(loaderContext, content);
 }
