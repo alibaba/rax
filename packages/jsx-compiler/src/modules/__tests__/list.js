3d05a197dd8afd4d05c4a55321f1709ec8ea5f8f
卓凌 <zhuoling.lcl@alibaba-inc.com>
2019-07-17 17:16:41 +0800

    fix: list

--- packages/jsx-compiler/src/modules/__tests__/list.js
+++ packages/jsx-compiler/src/modules/__tests__/list.js
@@ -26,7 +26,7 @@ describe('Transform list', () => {
     `);
     const { dynamicValue } = _transformList(ast, adapter);
 
-    expect(genCode(ast).code).toEqual('<View><block a:for="{{_l0}}" a:for-item="_item0" a:for-index="_index0"><item data-value="{{_l0[_index0].val}}" data-key="{{_l0[_index0].idx}}" /></block></View>');
+    expect(genCode(ast).code).toEqual('<View><block a:for={_l0} a:for-item="_item0" a:for-index="_index0"><item data-value={_l0[_index0].val} data-key={_l0[_index0].idx} /></block></View>');
     expect(genDynamicAttrs(dynamicValue)).toEqual(`{ _l0: arr.map((val, idx) => ({ val: val, idx: idx })) }`)
   });
 
@@ -38,7 +38,7 @@ describe('Transform list', () => {
     `);
     const { dynamicValue } = _transformList(ast, adapter);
 
-    expect(genCode(ast).code).toEqual('<View><block a:for="{{_l0}}" a:for-item="_item0" a:for-index="_index0"><item data-value="{{_l0[_index0].val}}" data-key="{{_l0[_index0].idx}}" /></block></View>');
+    expect(genCode(ast).code).toEqual('<View><block a:for={_l0} a:for-item="_item0" a:for-index="_index0"><item data-value={_l0[_index0].val} data-key={_l0[_index0].idx} /></block></View>');
     expect(genDynamicAttrs(dynamicValue)).toEqual(`{ _l0: arr.map((val, idx) => { return { val: val, idx: idx }; }) }`)
   });
 
@@ -50,8 +50,8 @@ describe('Transform list', () => {
     `);
     const { dynamicValue } = _transformList(ast, adapter);
 
-    expect(genCode(ast).code).toEqual('<View><block a:for="{{_l0}}" a:for-item="_item0" a:for-index="_index0"><item onClick={props.onClick} data-arg-context="this" data-arg-0="{{item}}" data-arg-1="{{1}}" /></block></View>');
-    expect(genDynamicAttrs(dynamicValue)).toEqual(`{ _l0: arr.map((val, idx) => ({ props: props })) }`)
+    expect(genCode(ast).code).toEqual('<View><block a:for={_l0} a:for-item="_item0" a:for-index="_index0"><item onClick={props.onClick} data-arg-context="this" data-arg-0={_l0[_index0].item} data-arg-1="{{1}}" /></block></View>');
+    expect(genDynamicAttrs(dynamicValue)).toEqual(`{ _l0: arr.map((val, idx) => ({ val: val, idx: idx })) }`)
   });
 
   it('bind loop variable', () => {
@@ -60,8 +60,10 @@ describe('Transform list', () => {
     `);
     const { dynamicValue } = _transformList(ast, adapter);
 
-    expect(genCode(ast).code).toEqual('<View><block a:for="{{_l0}}" a:for-item="_item0" a:for-index="_index0"><image source="{{ uri: item.picUrl }}" resizeMode={resizeMode} /></block></View>');
-    expect(genDynamicAttrs(dynamicValue)).toEqual(`{ _l0: arr.map((val, idx) => ({})) }`)
+    expect(genCode(ast).code).toEqual(`<View><block a:for={_l0} a:for-item="_item0" a:for-index="_index0"><image source={{
+      uri: _l0[_index0].item.picUrl
+    }} resizeMode={resizeMode} /></block></View>`);
+    expect(genDynamicAttrs(dynamicValue)).toEqual(`{ _l0: arr.map((item, idx) => ({ item: item, idx: idx })) }`)
   });
 
   it('bind loop item', () => {
@@ -69,8 +71,9 @@ describe('Transform list', () => {
         couponList.map(coupon => <Coupon coupon={coupon} onClick={this.handleClick.bind(this, coupon)} />)
       }</View>`;
     const ast = parseExpression(raw);
-    _transformList(ast, adapter);
-    expect(genCode(ast).code).toEqual('<View class="coupon-list"><block a:for="{{couponList}}" a:for-item="coupon" a:for-index="index"><Coupon coupon="{{coupon}}" onClick={this.handleClick} data-arg-context="this" data-arg-0="{{coupon}}" /></block></View>');
+    const { dynamicValue } = _transformList(ast, adapter);
+    expect(genCode(ast).code).toEqual('<View class="coupon-list"><block a:for={_l0} a:for-item="_item0" a:for-index="_index0"><Coupon coupon={_l0[_index0].coupon} onClick={this.handleClick} data-arg-context="this" data-arg-0={_l0[_index0].coupon} /></block></View>');
+    expect(genDynamicAttrs(dynamicValue)).toEqual(`{ _l0: couponList.map(coupon => ({ coupon: coupon })) }`)
   });
 
   it('loop elements', () => {
@@ -78,7 +81,9 @@ describe('Transform list', () => {
       return <Text>{idx}</Text>;
     })}</View>`;
     const ast = parseExpression(raw);
-    _transformList(ast, adapter);
-    expect(genCode(ast, { concise: true }).code).toEqual('<View><block a:for="{{[1, 2, 3]}}" a:for-item="val" a:for-index="idx"><Text>{{ idx }}</Text></block></View>');
+    const { dynamicValue } =_transformList(ast, adapter);
+
+    expect(genCode(ast, { concise: true }).code).toEqual('<View><block a:for={_l0} a:for-item="_item0" a:for-index="_index0"><Text>{_l0[_index0].idx}</Text></block></View>');
+    expect(genDynamicAttrs(dynamicValue)).toEqual(`{ _l0: [1, 2, 3].map((val, idx) => { return { val: val, idx: idx }; }) }`)
   });
 });
--- packages/jsx-compiler/src/modules/list.js
+++ packages/jsx-compiler/src/modules/list.js
@@ -9,118 +9,121 @@ const genExpression = require('../codegen/genExpression');
 function transformList(ast, adapter) {
   let iters = 0;
   const dynamicValue = {};
+  let fnScope;
+
+  function traverseFunction(path) {
+    fnScope = path.scope;
+  }
 
   traverse(ast, {
-    CallExpression(path) {
-      const { node, parentPath } = path;
-      const { callee, arguments: args } = node;
+    ArrowFunctionExpression: { enter: traverseFunction, },
+    FunctionExpression: { enter: traverseFunction },
+    CallExpression: {
+      exit(path) {
+        const { node, parentPath } = path;
+        const { callee, arguments: args } = node;
 
-      if (parentPath.parentPath.isJSXElement()) {
-        if (t.isMemberExpression(callee)) {
-          if (t.isIdentifier(callee.property, { name: 'map' })) {
-            const iterId = iters++;
-            const replacedIter = '_l' + iterId;
-            dynamicValue[replacedIter] = node;
+        if (parentPath.parentPath.isJSXElement()) {
+          if (t.isMemberExpression(callee)) {
+            if (t.isIdentifier(callee.property, { name: 'map' })) {
+              const iterId = iters++;
+              const replacedIter = '_l' + iterId;
+              dynamicValue[replacedIter] = node;
 
-            // { foo.map(fn) }
-            let childNode = null;
-            const itemName = '_item' + iterId;
-            const indexName = '_index' + iterId;
-            const scope = `${replacedIter}[${indexName}]`;
+              // { foo.map(fn) }
+              let childNode = null;
+              const itemName = '_item' + iterId;
+              const indexName = '_index' + iterId;
+              const scope = `${replacedIter}[${indexName}]`;
 
-            if (t.isFunction(args[0])) {
-              const properties = [];
-              traverse(path, {
-                Identifier(path) {
-                  const { node, parentPath } = path;
-                  if (parentPath.isJSXExpressionContainer()) {
-                    // <view foo={bar} />
-                    properties.push(t.objectProperty(node, node));
-                    parentPath.replaceWith(t.stringLiteral(createBinding(`${scope}.${node.name}`)));
-                  } else if (false) {
-                    // <view foo={{ bar }} />
-                  } else if (parentPath.isMemberExpression() && parentPath.node.object === node && parentPath.node !== callee) {
-                    // <view foo={{ bar: item.bar }} />
-                    properties.push(t.objectProperty(node, node));
-                    parentPath.replaceWith((t.identifier(scope)));
-                  }
-                },
-              });
+              if (t.isFunction(args[0])) {
+                const properties = [];
+                args[0].params.forEach((id, index) => {
+                  if (!t.isIdentifier(id)) return;
+                  const originalName = id.name;
+                  const replacement = `${scope}.${id.name}`;
+                  fnScope.rename(id.name, replacement);
 
-              // { foo.map(() => {}) }
-              let returnEl;
-              if (t.isBlockStatement(args[0].body)) {
-                // () => { return xxx }
-                const returnElPath = getReturnElementPath(args[0].body).get('argument');
-                returnEl = returnElPath.node;
-                returnElPath.replaceWith(t.objectExpression(properties));
-              } else {
-                // () => (<jsx></jsx)
-                const returnElPath = path.get('arguments')[0].get('body');
-                returnEl = returnElPath.node;
-                returnElPath.replaceWith(t.objectExpression(properties));
+                  // Reset params to original.
+                  const key = args[0].params[index] = t.identifier(originalName);
+                  properties.push(t.objectProperty(key, key));
+                });
+
+                // { foo.map(() => {}) }
+                let returnEl;
+                if (t.isBlockStatement(args[0].body)) {
+                  // () => { return xxx }
+                  const returnElPath = getReturnElementPath(args[0].body).get('argument');
+                  returnEl = returnElPath.node;
+                  returnElPath.replaceWith(t.objectExpression(properties));
+                } else {
+                  // () => (<jsx></jsx)
+                  const returnElPath = path.get('arguments')[0].get('body');
+                  returnEl = returnElPath.node;
+                  returnElPath.replaceWith(t.objectExpression(properties));
+                }
+
+                childNode = returnEl;
+
+                parentPath.replaceWith(
+                  createJSX('block', {
+                    [adapter.for]: t.jsxExpressionContainer(t.identifier(replacedIter)),
+                    [adapter.forItem]: t.stringLiteral(itemName),
+                    [adapter.forIndex]: t.stringLiteral(indexName),
+                  }, [childNode])
+                );
+              } else if (t.isIdentifier(args[0]) || t.isMemberExpression(args[0])) {
+                // { foo.map(this.xxx) }
+                throw new Error(`The syntax conversion for ${genExpression(node)} is currently not supported. Please use inline functions.`);
               }
-
-              childNode = returnEl;
-
-              parentPath.replaceWith(
-                createJSX('block', {
-                  [adapter.for]: t.stringLiteral(createBinding(replacedIter)),
-                  [adapter.forItem]: t.stringLiteral(itemName),
-                  [adapter.forIndex]: t.stringLiteral(indexName),
-                }, [childNode])
-              );
-            } else if (t.isIdentifier(args[0]) || t.isMemberExpression(args[0])) {
-              // { foo.map(this.xxx) }
-              throw new Error(`The syntax conversion for ${genExpression(node)} is currently not supported. Please use inline functions.`);
+            } else {
+              throw new Error(`Syntax conversion using ${genExpression(node)} in JSX templates is currently not supported, and can be replaced with static templates or state calculations in advance.`);
             }
-          } else {
+          } else if (t.isIdentifier(callee)) {
+            // { foo(args) }
             throw new Error(`Syntax conversion using ${genExpression(node)} in JSX templates is currently not supported, and can be replaced with static templates or state calculations in advance.`);
+          } else if (t.isFunction(callee)) {
+            throw new Error(`Currently using IIFE in JSX templates is not supported: ${genExpression(node)} 。`);
           }
-        } else if (t.isIdentifier(callee)) {
-          // { foo(args) }
-          throw new Error(`Syntax conversion using ${genExpression(node)} in JSX templates is currently not supported, and can be replaced with static templates or state calculations in advance.`);
-        } else if (t.isFunction(callee)) {
-          throw new Error(`Currently using IIFE in JSX templates is not supported: ${genExpression(node)} 。`);
-        }
-      } else if (parentPath.parentPath.isJSXAttribute()) {
-        if (t.isMemberExpression(callee) && t.isIdentifier(callee.property, { name: 'bind' })) {
-          // <tag onClick={props.onClick.bind(this, item)} />
-          // => <tag onClick={props.onClick.bind(this, item)} />
-          // parentPath => JSXContainerExpression
-          // parentPath.parentPath => JSXAttribute
-          // parentPath.parentPath.parentPath => JSXOpeningElement
-          const attributes = parentPath.parentPath.parentPath.node.attributes;
-          if (Array.isArray(args)) {
-            args.forEach((arg, index) => {
-              if (index === 0) {
-                // first arg is `this` context.
-                const strValue = t.isThisExpression(arg) ? 'this' : createBinding(genExpression(arg, {
-                  concise: true,
-                  comments: false,
-                }));
-                attributes.push(
-                  t.jsxAttribute(
-                    t.jsxIdentifier('data-arg-context'),
-                    t.stringLiteral(strValue)
-                  )
-                );
-              } else {
-                attributes.push(
-                  t.jsxAttribute(
-                    t.jsxIdentifier('data-arg-' + (index - 1)),
-                    t.stringLiteral(createBinding(genExpression(arg, {
-                      concise: true,
-                      comments: false,
-                    })))
-                  )
-                );
-              }
-            });
+        } else if (parentPath.parentPath.isJSXAttribute()) {
+          if (t.isMemberExpression(callee) && t.isIdentifier(callee.property, { name: 'bind' })) {
+            // <tag onClick={props.onClick.bind(this, item)} />
+            // => <tag onClick={props.onClick.bind(this, item)} />
+            // parentPath => JSXContainerExpression
+            // parentPath.parentPath => JSXAttribute
+            // parentPath.parentPath.parentPath => JSXOpeningElement
+            const attributes = parentPath.parentPath.parentPath.node.attributes;
+            if (Array.isArray(args)) {
+              args.forEach((arg, index) => {
+                if (index === 0) {
+                  // first arg is `this` context.
+                  const strValue = t.isThisExpression(arg) ? 'this' : createBinding(genExpression(arg, {
+                    concise: true,
+                    comments: false,
+                  }));
+                  attributes.push(
+                    t.jsxAttribute(
+                      t.jsxIdentifier('data-arg-context'),
+                      t.stringLiteral(strValue)
+                    )
+                  );
+                } else {
+                  attributes.push(
+                    t.jsxAttribute(
+                      t.jsxIdentifier('data-arg-' + (index - 1)),
+                      t.stringLiteral(createBinding(genExpression(arg, {
+                        concise: true,
+                        comments: false,
+                      })))
+                    )
+                  );
+                }
+              });
+            }
+            path.replaceWith(callee.object);
           }
-          path.replaceWith(callee.object);
         }
-      }
+      },
     },
   });
 
