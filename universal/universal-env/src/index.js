20b0e56a1b9d435943e86ee4ea617fd5d2e48f73
balloonzzq <zqzhuang888@qq.com>
2019-07-18 20:01:19 +0800

    fix: Migration project deletion

--- universal/universal-env/src/index.js
+++
@@ -1,5 +0,0 @@
-// https://www.w3.org/TR/html5/webappapis.html#dom-navigator-appcodename
-export const isWeb = typeof navigator === 'object' && (navigator.appCodeName === 'Mozilla' || navigator.product === 'Gecko');
-export const isNode = typeof process !== 'undefined' && !!(process.versions && process.versions.node);
-export const isWeex = typeof callNative === 'function' || typeof WXEnvironment === 'object' && WXEnvironment.platform !== 'Web';
-export const isReactNative = typeof __fbBatchedBridgeConfig !== 'undefined';
