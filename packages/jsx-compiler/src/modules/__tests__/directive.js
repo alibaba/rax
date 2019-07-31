3f56a60b8b0c8f78081928a1ba579f6a24b2b57c
orifake <349758699@qq.com>
2019-07-31 12:27:58 +0800

    fix: add directive test case

--- packages/jsx-compiler/src/modules/__tests__/directive.js
+++ packages/jsx-compiler/src/modules/__tests__/directive.js
@@ -15,6 +15,22 @@ describe('Directives', () => {
     });
   });
 
+  describe('nested list', () => {
+    it('simple', () => {
+      const ast = parseExpression(`
+        <View x-for={val in array}>
+          <View key={index} x-for={(item,index) in array.items}>{item}</View>
+        </View>
+      `);
+      const dynamicValue = _transformList(ast);
+      expect(genExpression(ast).replace('\n', '').replace(/\s\s+/g, ''))
+        .toEqual('<View a:for="{{array}}" a:for-item="val"><View key={index} a:for="{{array.items}}" a:for-item="item" a:for-index="index">{item}</View></View>');
+      expect(dynamicValue.array.name).toEqual('array');
+      expect(dynamicValue.item).toEqual(undefined);
+      expect(dynamicValue.index).toEqual(undefined);
+    });
+  });
+
   describe('condition', () => {
     it('simple', () => {
       const ast = parseExpression(`
