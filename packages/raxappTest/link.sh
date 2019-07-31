06e045884a34eca8b26c0d08e60a6a0583957162
Orange-C <741434157@qq.com>
2019-07-31 17:01:36 +0800

    fix: fix link

--- packages/raxappTest/link.sh
+++ packages/raxappTest/link.sh
@@ -1,2 +1,12 @@
+cd ../universal-app-shell-loader
+tnpm i rax
+cd ../rax-plugin-app
+tnpm i rax
+
+cd ../rax-plugin-app
+tnpm link ../universal-app-shell-loader
+
+cd ../raxappTest
 tnpm link ../rax-scripts
 tnpm link ../rax-plugin-app
+tnpm link ../universal-app-runtime
