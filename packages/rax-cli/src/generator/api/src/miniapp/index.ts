7b2a14094e22bfce0146ba83bfb02f78039f3c09
Orange-C <oraaangecolin@gmail.com>
2019-08-01 19:59:12 +0800

    fix: fix api template

--- packages/rax-cli/src/generator/api/src/miniapp/index.ts
+++
@@ -1,9 +0,0 @@
-import { Options } from '../types';
-
-// declare const my: any;
-
-const func = (options: Options): Promise<any> => {
-  return Promise.resolve(options);
-};
-
-export default func;
