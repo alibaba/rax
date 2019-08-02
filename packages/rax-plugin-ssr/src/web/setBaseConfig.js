3c36ccda080be1d9795532370d6d3457f84f36e6
水澜 <shuilan.cj@taobao.com>
2019-08-02 16:56:59 +0800

    refactor: web config in ssr

--- packages/rax-plugin-ssr/src/web/setBaseConfig.js
+++ packages/rax-plugin-ssr/src/web/setBaseConfig.js
@@ -1,37 +1,5 @@
 'use strict';
 
-const path = require('path');
-const AssetsManifestPlugin = require('rax-pwa-webpack-plugin/lib/AssetsManifestPlugin').default;
-const { RaxPWAPlugin } = require('rax-pwa-webpack-plugin');
-
 module.exports = (config, rootDir) => {
-  config.plugin('raxpwa')
-    .use(RaxPWAPlugin, [{
-      pathConfig: {
-        appDirectory: rootDir,
-        appSrc: path.resolve(rootDir, 'src'),
-        appShell: path.resolve(rootDir, 'src/shell/index.js'),
-        appDocument: path.resolve(rootDir, 'src/document/index.js'),
-        appHtml: path.resolve(rootDir, 'public/index.html'),
-      },
-      appConfig: {
-        title: 'test_title',
-        pages: {
-          home: {
-            path: '/home',
-            title: 'homePage',
-          }
-        },
-        spa: true,
-        ssr: true
-      }
-    }]);
-
-  config.plugin('assetsManifest')
-    .use(AssetsManifestPlugin, [{
-      dist: path.resolve(rootDir, '.temp/assets_manifest.json'),
-      publicPath: path.resolve(rootDir, 'public')
-    }]);
-
   return config;
 };
