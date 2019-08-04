78ed252507787c64316899063a41c0ffc29179ca
水澜 <shuilan.cj@taobao.com>
2019-08-04 10:00:58 +0800

    fix: hrm in ssr

--- packages/rax-plugin-ssr/src/index.js
+++ packages/rax-plugin-ssr/src/index.js
@@ -2,11 +2,13 @@ const SSRDevServer = require('rax-ssr-dev-server');
 
 const getSSRBuildConfig = require('./ssr/getBuildConfig');
 const getSSRDevConfig = require('./ssr/getDevConfig');
-
+const setWebBaseConfig = require('./web/setBaseConfig');
 
 // can‘t clone webpack chain object
 module.exports = ({ chainWebpack, registerConfig, setDevServer, rootDir, onHook, log }) => {
   chainWebpack((config, { command }) => {
+    setWebBaseConfig(config, rootDir);
+
     let ssrConfig;
     if (command === 'build') {
       ssrConfig = getSSRBuildConfig(rootDir);
