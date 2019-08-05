02c5326be7ac512d11c8295234ca84e6fae39616
卓凌 <zhuoling.lcl@alibaba-inc.com>
2019-08-05 19:56:26 +0800

    feat: export jsx2mp-cli

--- packages/jsx2mp-cli/index.js
+++ packages/jsx2mp-cli/index.js
@@ -1,27 +1,46 @@
 const MemFs = require('memory-fs');
-const config = require('./webpack.config');
+const getWebpackConfig = require('./getWebpackConfig');
+const mergeWebpack = require('webpack-merge');
 const webpack = require('webpack');
 const spinner = require('./utils/spinner');
 const consoleClear = require('console-clear');
 const chalk = require('chalk');
 
-function setEnv(type) {
-  process.env.JSX2MP_ENV = type;
-}
-
-function build() {
-  setEnv('build');
+/**
+ * Start jsx2mp build.
+ * @param options
+ */
+function build(options = {}) {
+  const { afterCompiled } = options;
+  let config = getWebpackConfig({ mode: 'build' });
+  if (options.webpackConfig) {
+    config = mergeWebpack(config, options.webpackConfig);
+  }
   const compiler = webpack(config);
   compiler.outputFileSystem = new MemFs();
-  compiler.run(handleCompiled);
+  compiler.run((...args) => {
+    handleCompiled(...args);
+    afterCompiled && afterCompiled(...args);
+  });
 }
 
-function watch() {
-  setEnv('dev');
+/**
+ * Start webpack watch mode.
+ * @param options
+ */
+function watch(options = {}) {
+  const { afterCompiled } = options;
+  let config = getWebpackConfig({ mode: 'watch' });
+  if (options.webpackConfig) {
+    config = mergeWebpack(config, options.webpackConfig);
+  }
   const compiler = webpack(config);
   const watchOpts = {};
   compiler.outputFileSystem = new MemFs();
-  compiler.watch(watchOpts, handleCompiled);
+  compiler.watch(watchOpts, (...args) => {
+    handleCompiled(...args);
+    afterCompiled && afterCompiled(...args);
+  });
 }
 
 function handleCompiled(err, stats) {
@@ -35,15 +54,17 @@ function handleCompiled(err, stats) {
   if (stats.hasErrors()) {
     const errors = stats.compilation.errors;
     consoleClear();
+    spinner.fail('Failed to compile.\n');
     for (let e of errors) {
-      spinner.fail('Failed to compile.\n\n' + e.error.message + '\n');
+      console.log(e.error.message + '\n');
       if (process.env.DEBUG === 'true') {
         console.log(e.error.stack);
       }
-      console.log(chalk.yellow('You can set debug mode to show error stacks.'));
     }
+    console.log(chalk.yellow('You can set environment `DEBUG=true` to show error stacks.'));
   }
 }
 
 exports.build = build;
 exports.watch = watch;
+exports.getWebpackConfig = getWebpackConfig;
