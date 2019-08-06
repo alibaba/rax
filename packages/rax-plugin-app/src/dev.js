937e0ff7315cf8f4a297b15d09f28b7edb4ec9a5
Orange-C <oraaangecolin@gmail.com>
2019-08-06 11:14:30 +0800

    feat: add mp

---
+++ packages/rax-plugin-app/src/dev.js
@@ -0,0 +1,97 @@
+const path = require('path');
+const chalk = require('chalk');
+const consoleClear = require('console-clear');
+
+const getMpOuput = require('./config/miniapp/getOutputPath');
+
+module.exports = ({ chainWebpack, registerConfig, context, onHook }, options = {}) => {
+  const { targets = [] } = options;
+
+  let normalInfo = null;
+
+  let emitDevLog = false;
+
+  function devCompileLog({ url, err, stats }) {
+    consoleClear(true);
+
+    if (err) {
+      console.error(err.stack || err);
+      if (err.details) {
+        console.error(err.details);
+      }
+      return;
+    }
+    if (stats.hasErrors()) {
+      const errors = stats.compilation.errors;
+      for (let e of errors) {
+        console.log(chalk.red(`    ${errors.indexOf(e) + 1}. ${e.error.message} \n`));
+        if (process.env.DEBUG === 'true') {
+          console.log(e.error.stack);
+        }
+      }
+      console.log(chalk.yellow('Set environment `DEBUG=true` to see detail error stacks.'));
+      return;
+    }
+
+    console.log(chalk.green('Rax development server has been started:'));
+    console.log();
+
+    if (~targets.indexOf('web')) {
+      console.log(chalk.green('[Web] Development server at:'));
+      console.log('   ', chalk.underline.white(url));
+      console.log();
+    }
+
+    if (~targets.indexOf('weex')) {
+      console.log(chalk.green('[Weex] Development server at:'));
+      console.log('   ', chalk.underline.white(`${url}/weex/index.js?wh_weex=true`));
+      console.log();
+    }
+
+    if (~targets.indexOf('miniapp')) {
+      console.log(chalk.green('[Miniapp] Watching file changes at:'));
+      console.log('   ', chalk.underline.white(getMpOuput(context)));
+      console.log();
+    }
+  }
+
+  if (targets.length === 1 && targets[0] === 'miniapp') {
+    const mpDev = require('./config/miniapp/dev');
+    onHook('after.dev', devCompileLog);
+  } else {
+    targets.forEach(target => {
+      if (target === 'weex' || target === 'web') {
+        const getBase = require(`./config/${target}/getBase`);
+        const setDev = require(`./config/${target}/setDev`);
+
+        registerConfig(target, getBase(context));
+
+        chainWebpack((config) => {
+          setDev(config.getConfig(target), context);
+        });
+      }
+    });
+
+    onHook('after.devCompile', async({ url, err, stats }) => {
+      const mpBuild = require('./config/miniapp/build');
+      let mpBuildErr = null;
+      const mpInfo = await mpBuild(context);
+
+      if (mpInfo.err || mpInfo.stats.hasErrors()) {
+        mpBuildErr = mpInfo;
+      }
+
+      if (mpBuildErr) {
+        err = mpBuildErr.err;
+        stats = mpBuildErr.stats;
+      }
+
+      devCompileLog({
+        url,
+        err,
+        stats
+      });
+    });
+  }
+};
+
--- packages/rax-plugin-app/src/index.js
+++ packages/rax-plugin-app/src/index.js
@@ -1,37 +1,20 @@
-const chalk = require('chalk');
 const deepmerge = require('deepmerge');
-const defaultUserConfig = require('./config/defaultUserConfig');
 
-module.exports = ({ chainWebpack, registerConfig, context, onHook }, options = {}) => {
+const defaultUserConfig = require('./config/defaultUserConfig');
+const build = require('./build');
+const dev = require('./dev');
+
+module.exports = (api, options) => {
+  const { context } = api;
+  const { command } = context;
   // set default config
   context.userConfig = deepmerge(context.userConfig, defaultUserConfig);
 
-  const { targets = [] } = options;
+  if (command === 'build') {
+    build(api, options);
+  }
 
-  targets.forEach(target => {
-    if (target === 'weex' || target === 'web') {
-      const getBase = require(`./config/${target}/getBase`);
-      const setDev = require(`./config/${target}/setDev`);
-      const setBuild = require(`./config/${target}/setBuild`);
-
-      registerConfig(target, getBase(context));
-
-      chainWebpack((config, { command }) => {
-        if (command === 'dev') {
-          setDev(config.get(target), context);
-        }
-
-        if (command === 'build') {
-          setBuild(config.get(target), context);
-        }
-      });
-
-      if (target === 'weex') {
-        onHook('after.dev', ({url}) => {
-          console.log(chalk.green('[Weex] Starting the development server at:'));
-          console.log('   ', chalk.underline.white(`${url}/weex/index.js?wh_weex=true`));
-        });
-      }
-    }
-  });
+  if (command === 'dev') {
+    dev(api, options);
+  }
 };
