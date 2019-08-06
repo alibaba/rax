d30e595cc290801f5513e7eb5b18d3114e9b069d
Orange-C <oraaangecolin@gmail.com>
2019-08-06 16:27:41 +0800

    feat: update app template

--- packages/rax-cli/src/generator/app/src/app.js
+++ packages/rax-cli/src/generator/app/src/app.js
@@ -1,12 +1,8 @@
 import { createElement } from 'rax';
-import { useAppEffect} from '@core/app';
 import { useRouter } from '@core/router';
 
 export default function App(props) {
   const { routerConfig } = props;
-  useAppEffect('launch', (options) => {
-
-  });
 
   const { Router } = useRouter(routerConfig);
 
--- packages/rax-cli/src/generator/app/src/pages/home/index.jsx
+++ packages/rax-cli/src/generator/app/src/pages/home/index.jsx
@@ -1,31 +1,9 @@
-import './index.css';
 import { createElement } from 'rax';
-import { usePageEffect } from '@core/page';
-// app should use Link in @core/router rather than rax-link
-import { push, Link } from '@core/router';
-import Text from 'rax-text';
-import View from 'rax-view';
-import Image from 'rax-image';
+
+import Main from '../../components/Main';
 
 export default function Home() {
-  usePageEffect('show', () => {
-
-  });
-
-  usePageEffect('hide', () => {
-
-  });
-
   return (
-    <View>
-      <View className="header">
-
-      </View>
-      <Text>hello, world</Text>
-      <Link to="/about">Link to about</Link>
-      <View onClick={() => push('/about')}>
-        <Text>Push to about</Text>
-      </View>
-    </View>
+    <Main />
   );
 }
