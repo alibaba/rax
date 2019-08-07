d661069140c35b476eeb2482a3635df76f331657
Orange-C <oraaangecolin@gmail.com>
2019-08-07 19:44:46 +0800

    fix: change mp build dir

--- packages/rax-plugin-app/src/config/miniapp/getOutputPath.js
+++ packages/rax-plugin-app/src/config/miniapp/getOutputPath.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = (context) => {
   const { rootDir, userConfig } = context;
   const { outputDir } = userConfig;
-  const outputPath = path.resolve(rootDir, 'build-miniapp');
+  const outputPath = path.resolve(rootDir, outputDir, 'miniapp');
 
   return outputPath;
 };
