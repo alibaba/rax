899d806d5a1ecef2270c0be19e8657e4d4867bf1
Orange-C <oraaangecolin@gmail.com>
2019-08-13 20:39:46 +0800

    feat: add web mpa

--- packages/rax-plugin-app/src/config/web/setEntry.js
+++ packages/rax-plugin-app/src/config/web/setEntry.js
@@ -2,11 +2,19 @@ const path = require('path');
 const fs = require('fs-extra');
 
 const hmrClient = require.resolve('../../hmr/webpackHotDevClient.entry');
+
+const MulitPageLoader = require.resolve('../../loaders/MulitPageLoader');
 const UNIVERSAL_APP_SHELL_LOADER = require.resolve('universal-app-shell-loader');
 
-module.exports = (config, rootDir, userConfig, isDev = false) => {
-  const appEntry = path.resolve(rootDir, 'src/app.js');
+function getDepPath(rootDir, com) {
+  if (com[0] === '/') {
+    return path.join(rootDir, 'src', com);
+  } else {
+    return path.resolve(rootDir, 'src', com);
+  }
+};
 
+module.exports = (config, rootDir, userConfig, isDev = false) => {
   if (userConfig.spa === false) {
     // MPA
     let routes = [];
@@ -18,15 +26,18 @@ module.exports = (config, rootDir, userConfig, isDev = false) => {
       throw new Error('routes in app.json must be array');
     }
 
-    routes.forEach((route, index) => {
+    routes.forEach((route) => {
       const entryConfig = config.entry(route.component.replace(/pages\/([^\/]*)\/index/g, (str, $) => $));
       if (isDev) {
         entryConfig.add(hmrClient);
       }
-      entryConfig.add(`${UNIVERSAL_APP_SHELL_LOADER}?type=web&isMultiPageWebApp=true&routeIndex=${index}!${appEntry}`);
+
+      const pageEntry = getDepPath(rootDir, route.component);
+      entryConfig.add(`${MulitPageLoader}?type=web!${pageEntry}`);
     });
   } else {
     // SPA
+    const appEntry = path.resolve(rootDir, 'src/app.js');
     const entryConfig = config.entry('index');
     if (isDev) {
       entryConfig.add(hmrClient);
