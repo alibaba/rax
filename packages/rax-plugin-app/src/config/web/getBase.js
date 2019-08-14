fceab0e7f7d34d1889b91e4a5b23644a979cb7e5
Orange-C <oraaangecolin@gmail.com>
2019-08-14 20:52:16 +0800

    fix: fix plugin args

--- packages/rax-plugin-app/src/config/web/getBase.js
+++ packages/rax-plugin-app/src/config/web/getBase.js
@@ -1,14 +1,11 @@
 'use strict';
 
-const serverRender = require('rax-server-renderer');
 const UniversalDocumentPlugin = require('../../plugins/UniversalDocumentPlugin');
 const PWAAppShellPlugin = require('../../plugins/PWAAppShellPlugin');
 const getWebpackBase = require('../getWebpackBase');
 const setEntry = require('../setEntry');
 
 module.exports = (context) => {
-  const { rootDir } = context;
-
   const config = getWebpackBase(context);
   setEntry(config, context, 'web');
 
@@ -16,16 +13,12 @@ module.exports = (context) => {
 
   config.plugin('document')
     .use(UniversalDocumentPlugin, [{
-      rootDir,
       path: 'src/document/index.jsx',
-      render: serverRender.renderToString,
     }]);
 
   config.plugin('PWAAppShell')
     .use(PWAAppShellPlugin, [{
-      rootDir,
       path: 'src/shell/index.jsx',
-      render: serverRender.renderToString,
     }]);
 
   return config;
