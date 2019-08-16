33013cbf9cea1fcb9f0c30da1e151cd5d3d0cb71
卓凌 <zhuoling.lcl@alibaba-inc.com>
2019-08-16 17:30:20 +0800

    chore: replace colors with chalk

--- packages/jsx2mp-cli/utils/npm.js
+++ packages/jsx2mp-cli/utils/npm.js
@@ -1,13 +1,13 @@
 const { printLog } = require('./log');
 const { spawnSync } = require('child_process');
-const colors = require('colors');
+const chalk = require('chalk');
 
 /**
  * npm install
  * @param path {String} Absolute path to a text file.
  */
 const invokeNpmInstall = function(path) {
-  printLog(colors.green('Execute'), 'npm install --production');
+  printLog(chalk.green('Execute'), 'npm install --production');
   return spawnSync('npm', ['install', '--production'], {
     cwd: path,
     env: process.env,
