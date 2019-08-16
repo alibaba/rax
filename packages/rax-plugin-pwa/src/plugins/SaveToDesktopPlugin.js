ce7db182c2a4c376cca7af413f6ce9b5ccc2abc2
yangfan <18767120422@163.com>
2019-08-16 17:54:26 +0800

    feat: support ios

--- packages/rax-plugin-pwa/src/plugins/SaveToDesktopPlugin.js
+++ packages/rax-plugin-pwa/src/plugins/SaveToDesktopPlugin.js
@@ -11,9 +11,8 @@ const defaultManifest = {
   display: 'standalone'
 };
 
-const iconSizes = [96, 128, 192, 256, 384, 512];
+const iconSizes = [96, 128, 180, 256, 512];
 const supportedMimeTypes = [jimp.MIME_PNG, jimp.MIME_JPEG, jimp.MIME_BMP];
-
 
 module.exports = class SaveToDesktopPlugin {
   constructor(options) {
@@ -22,44 +21,58 @@ module.exports = class SaveToDesktopPlugin {
 
   apply(compiler) {
     const { manifest, context } = this.options
+
+    if (!manifest) {
+      // Exit if no manifest config
+      return;
+    }
     const { rootDir, userConfig } = context;
     const publicPath = userConfig.publicPath || '/';
-    // exit if no manifest config
-    if (!manifest) return;
+    let tags = `<link rel="manifest" href="${publicPath}web/manifest.json" />`;
 
     compiler.hooks.emit.tapAsync(PLUGIN_NAME, (compilation, callback) => {
-      // process image
       const icons = [];
       const iconFile = path.join(rootDir, manifest.icon || '');
       const mimeType = mime.getType(iconFile);
+      
       if (!manifest.icon) throw new Error('Can not find icon in manifest');
       if (!existsSync(iconFile)) throw new Error(`File '${iconFile}' is not exists, please check.`);
       if (!supportedMimeTypes.includes(mimeType)) throw new Error(`File '${iconFile}' is not support, supported MIME types are "image/png", "image/jpeg", "image/bmp".`);
+      // Process image
       jimp.read(iconFile, (err, img) => {
         if (err) throw new IconError(`It was not possible to read '${iconFile}'.`)
+        // Resize image to specific size
         iconSizes.forEach((currentSize) => {
           const dimensions = `${currentSize}x${currentSize}`;
-          const fileName = `icon_${dimensions}.${mime.getExtension(mimeType)}`;
+          const fileName = `public/icon_${dimensions}.${mime.getExtension(mimeType)}`;
+          const iconPublicUrl = publicPath + fileName;
+          // Resize image 
           img.resize(currentSize, currentSize).getBuffer(mimeType, (resizeErr, buffer) => {
             if (resizeErr) throw new Error(`It was not possible to retrieve buffer of '${iconFile}'.`);
             compilation.assets[fileName] = new RawSource(buffer);
           });
-
+          // Add icon
           icons.push({
-            src: publicPath + fileName,
+            src: iconPublicUrl,
             sizes: dimensions,
             type: mimeType
-          })
+          });
+          // Write tags for iOS
+          tags += `<link rel="apple-touch-icon" sizes="${dimensions}" href="${iconPublicUrl}">`;
         });
 
-        // write manifest.json
+        // Write manifest.json
         const manifestJsonValue = {
           ...defaultManifest,
           ...manifest,
           icons,
         };
         delete manifestJsonValue.icon;
-        compilation.assets['manifest.json'] = new RawSource(JSON.stringify(manifestJsonValue));
+        compilation.assets['web/manifest.json'] = new RawSource(JSON.stringify(manifestJsonValue));
+        // Generate Html tags        
+        compilation.assets['index.html'] = new RawSource(
+          compilation.assets['index.html'].source().replace('<head>', `<head>${tags}`)
+        );
         callback();
       });
     });
