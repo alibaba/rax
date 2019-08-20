a6a86d1025c452ff1641f8667cfa4ff71d093dc5
Orange-C <oraaangecolin@gmail.com>
2019-08-20 10:56:19 +0800

    fix: rm multi

--- packages/universal-app-shell-loader/src/getSourceCode.js
+++ packages/universal-app-shell-loader/src/getSourceCode.js
@@ -1,41 +1,15 @@
-const getDepPath = require('./getDepPath');
-
 module.exports = (options) => {
-  let source = '';
-  if (options.isMultiPageWebApp) {
-    source = `
-    import definedApp from '${options.definedAppPath}';
-    import { render, createElement } from '${options.renderModule}';
-    import Component from '${getDepPath(options.routes[options.routeIndex].component, this.rootContext)}';
-    import DriverUniversal from 'driver-universal';
-    ${options.importMods}
-    
-    const withSSR = !!window.__INITIAL_DATA__;
-
-    ${/* Extendable app props. */''}
-    const appProps = {
-      Component,
-    };
-    
-    function Entry() {
-      const app = definedApp(appProps);
-      return app;
-    }
-    
-    ${options.appRender}
-  `;
-  } else {
-    source = `
+  return `
     import definedApp from '${options.definedAppPath}';
     import { render, createElement } from '${options.renderModule}';
     import { ${options.historyMemory} as createHistory } from 'history';
     import { _invokeAppCycle } from 'universal-app-runtime';
     import DriverUniversal from 'driver-universal';
     ${options.importMods}
-    
+
     const interopRequire = (mod) => mod && mod.__esModule ? mod.default : mod;
     const withSSR = !!window.__INITIAL_DATA__;
-    
+
     const getRouterConfig = () => {
       const routes = [];
       ${options.assembleRoutes}
@@ -49,9 +23,9 @@ module.exports = (options) => {
     const appProps = {
       routerConfig: getRouterConfig(),
     };
-    
+
     let appLaunched = false;
-    
+
     function Entry() {
       const app = definedApp(appProps);
       const startOptions = {};
@@ -63,10 +37,8 @@ module.exports = (options) => {
 
       return app;
     }
-    
+
     ${options.appRender}
   `;
-  }
-  return source;
 };
 
