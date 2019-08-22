607abb2f256df1f72269ce7a9b0b45fbd5a1d40f
BakuJun <847743331@qq.com>
2019-08-22 17:57:18 +0800

    feat: support css custom properties in props.style like react

--- packages/driver-dom/src/index.js
+++ packages/driver-dom/src/index.js
@@ -326,10 +326,10 @@ export function setAttribute(node, propKey, propValue) {
 
 export function setStyle(node, style) {
   for (let prop in style) {
-    //Support CSS custom properties
-    if(prop.indexOf('--') === 0){
-      node.style.setProperty(prop,convertUnit(style[prop]));
-    }else{
+    // Support CSS custom properties
+    if (prop.indexOf('--') === 0) {
+      node.style.setProperty(prop, convertUnit(style[prop]));
+    } else {
       node.style[prop] = convertUnit(style[prop]);
     }
   }
@@ -343,7 +343,7 @@ function recolectHydrationChild(hydrationParent) {
   const nativeLength = hydrationParent.childNodes.length;
   const vdomLength = hydrationParent[HYDRATION_INDEX] || 0;
   if (nativeLength - vdomLength > 0) {
-    for (let i = nativeLength - 1; i >= vdomLength; i-- ) {
+    for (let i = nativeLength - 1; i >= vdomLength; i--) {
       hydrationParent.removeChild(hydrationParent.childNodes[i]);
     }
   }
