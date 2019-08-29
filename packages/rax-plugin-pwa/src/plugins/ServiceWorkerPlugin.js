622d1c2db683547dbd3b5738e504451288b40e73
yangfan <18767120422@163.com>
2019-08-29 16:27:18 +0800

    feat: add rax-plugin-pwa

---
+++ packages/rax-plugin-pwa/src/plugins/ServiceWorkerPlugin.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const templateGenerator = require('lodash.template');
+const { RawSource } = require('webpack-sources');
+const swTemplate = require('../templates/serviceWorker');
+const regSwTemplate = require('../templates/registerSW');
+
+const isArray = Array.isArray;
+
+const PLUGIN_NAME = 'PWA_ServiceWorkerPlugin';
+const HTML_PATH = 'web/index.html';
+const REG_SW_FILE_PATH = 'web/regSW.pwa.js';
+const SW_FILE_PATH = 'web/sw.js';
+const DEFAULT_IGNORE_LIST = ['/regSW.pwa.js/i'];
+
+function patternToString(pattern) {
+  return pattern.toString();
+}
+
+module.exports = class ServiceWorkerPlugin {
+  constructor(options) {
+    this.options = options;
+  }
+
+  apply(compiler) {
+    const { serviceWorker, context } = this.options;
+    const { pkg } = context;
+
+    if (!serviceWorker) {
+      // Exit if no serviceWorker config
+      return;
+    }
+
+    compiler.hooks.emit.tapAsync(PLUGIN_NAME, (compilation, callback) => {
+      const data = {
+        // Precache list, specific URL required
+        preCacheUrlList: isArray(serviceWorker.preCacheUrlList)
+          ? serviceWorker.preCacheUrlList
+          : [],
+        // Ignore following assets, match thougth regExp
+        // The service worker file can not be cache
+        ignorePatternList: isArray(serviceWorker.ignorePatternList)
+          ? DEFAULT_IGNORE_LIST
+            .concat(serviceWorker.ignorePatternList)
+            .map(patternToString)
+          : DEFAULT_IGNORE_LIST,
+        // Cache following assets, match thougth regExp
+        savedCachePatternList: isArray(serviceWorker.savedCachePatternList)
+          ? serviceWorker.savedCachePatternList.map(patternToString)
+          : [],
+        // Cache id
+        cacheId: serviceWorker.cacheId || pkg.name,
+        skipWaiting: serviceWorker.skipWaiting || false,
+        clientsClaim: serviceWorker.clientsClaim || false,
+        unregister: serviceWorker.unregister || false,
+      };
+
+      const regSwCode = templateGenerator(regSwTemplate)(data);
+      const swCode = templateGenerator(swTemplate)(data);
+      const scriptCode = `<script src="/${REG_SW_FILE_PATH}" type="text/javascript" crossorigin="anonymous"></script>`;
+      const htmlCode = compilation
+        .assets[HTML_PATH]
+        .source()
+        .replace('</body>', `${scriptCode}</body>`);
+
+      compilation.assets[SW_FILE_PATH] = new RawSource(swCode);
+      compilation.assets[REG_SW_FILE_PATH] = new RawSource(regSwCode);
+      compilation.assets[HTML_PATH] = new RawSource(htmlCode);
+      callback();
+    });
+  }
+};
---
+++ packages/rax-plugin-pwa/src/templates/registerSW.js
@@ -0,0 +1,24 @@
+module.exports = `/**
+*
+* Register a service worker.
+*
+* Service worker lets your app load faster and work offline.
+* More information read https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API
+*
+*/
+;(function() {
+  if ('serviceWorker' in navigator) {
+    <% if (unregister) { %>
+    navigator.serviceWorker.ready.then(registration => {
+      registration.unregister();
+    });
+    <% } else { %>
+    navigator.serviceWorker.register('./sw.js').then((registration) => {
+      console.log('Registration successful, scope is:', registration.scope);
+    }).catch((error) => {
+      console.log('Service Worker registration failed, error:', error);
+    });
+    <% } %>
+  }
+})();
+`;
