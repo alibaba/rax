02e5119d66005f6ce9a6d8b4ebf63bda7aa27da3
Yun Dong <yundong.me@gmail.com>
2019-09-03 15:12:12 +0800

    feat: update template

--- packages/rax-plugin-api/src/index.js
+++ packages/rax-plugin-api/src/index.js
@@ -1,3 +1,5 @@
+const chalk = require('chalk');
+const path = require('path');
 const deepmerge = require('deepmerge');
 const plugin = require('rax-plugin-component');
 
@@ -7,6 +9,7 @@ module.exports = (api, options = {}) => {
   api.context.userConfig = deepmerge(defaultUserConfig, api.context.userConfig);
 
   const { command } = api.context;
+
   options = {
     ...options,
     targets: ['web', 'weex']
@@ -14,6 +17,12 @@ module.exports = (api, options = {}) => {
 
   if (command === 'dev') {
     plugin(api, options);
+
+    api.onHook('after.devCompile', () => {
+      console.log(chalk.green('[Miniapp] Use miniapp developer tools to open the following folder:'));
+      console.log('   ', chalk.underline.white(path.resolve(api.context.rootDir, 'demo/miniapp')));
+      console.log();
+    });
   }
 
   if (command === 'build') {
--- packages/rax-template/template/api/src/miniapp/index.ts.ejs
+++
@@ -1,7 +0,0 @@
-import { Options } from '../types';
-
-const func = (options: Options): Promise<any> => {
-  return Promise.resolve(options);
-};
-
-export default func;
