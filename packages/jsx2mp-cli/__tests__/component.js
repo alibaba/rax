cd98cf25a8f85b8da9209e12b500441297a08e26
fushen <fushen.jzw@alibaba-inc.com>
2019-09-04 10:10:08 +0800

    chore: lint

--- packages/jsx2mp-cli/__tests__/component.js
+++ packages/jsx2mp-cli/__tests__/component.js
@@ -35,7 +35,7 @@ afterAll(() => {
 
 describe('Component compiled result', () => {
   it('should return correct axml', () => {
-    expect(axmlContent).toEqual('<rax-view __tagId="0">Hello World!</rax-view>');
+    expect(axmlContent).toEqual('<block a:if="{{$ready}}"><rax-view __tagId="0">Hello World!</rax-view></block>');
   });
 
   it('should return correct js', () => {
