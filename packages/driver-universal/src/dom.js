d88a2c6b385bf8fa8b8876df479b42bae039dda0
BakuJun <847743331@qq.com>
2019-09-05 16:46:44 +0800

    feat: driver-universal support CSS custom properties

--- packages/driver-universal/src/dom.js
+++ packages/driver-universal/src/dom.js
@@ -49,7 +49,11 @@ const driver = Object.assign({}, DriverDOM, {
         if (Array.isArray(transformValue)) {
           for (let i = 0; i < transformValue.length; i++) node.style[prop] = transformValue[i];
         } else {
-          node.style[prop] = transformValue;
+          if (prop[0] === '-' && prop[1] === '-') {
+            node.style.setProperty(prop, transformValue);
+          } else {
+            node.style[prop] = transformValue;
+          }
         }
       }
     }
