a3c95ac930317883bdf87fffe59713f48ca59767
ChrisCindy <yxy0919@foxmail.com>
2019-09-23 20:35:56 +0800

    fix: lint error

--- packages/jsx2mp-loader/src/utils/isMiniappComponent.js
+++ packages/jsx2mp-loader/src/utils/isMiniappComponent.js
@@ -6,9 +6,9 @@ const platformConfig = require('./platformConfig');
 const partSuffix = ['.js', '.json'];
 
 // e.g file:   /root/lib/miniapp/index
-module.exports = function (filename, platform = 'ali') {
+module.exports = function(filename, platform = 'ali') {
   const xmlSuffix = platformConfig[platform].extension.xml;
   const suffix = partSuffix.slice();
   suffix.push(xmlSuffix);
   return suffix.every(s => existsSync(filename + s));
-}
+};
