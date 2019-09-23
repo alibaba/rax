572cbf3927fc39bd2f4e17de08c8cd8801593f0f
ChrisCindy <yxy0919@foxmail.com>
2019-09-23 20:26:50 +0800

    fix: handle old universal api with miniappConfig in package.json

---
+++ packages/jsx2mp-loader/src/utils/isMiniappComponent.js
@@ -0,0 +1,14 @@
+const {
+  existsSync
+} = require('fs-extra');
+const platformConfig = require('./platformConfig');
+
+const partSuffix = ['.js', '.json'];
+
+// e.g file:   /root/lib/miniapp/index
+module.exports = function (filename, platform = 'ali') {
+  const xmlSuffix = platformConfig[platform].extension.xml;
+  const suffix = partSuffix.slice();
+  suffix.push(xmlSuffix);
+  return suffix.every(s => existsSync(filename + s));
+}
