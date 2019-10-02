bddaed78c4aefb8275ec0d15afab0ed7b168fffa
yonggningfu <535802703@qq.com>
2019-10-03 01:30:18 +0800

    chore: modify test

--- packages/rax/src/vdom/__tests__/composite.js
+++ packages/rax/src/vdom/__tests__/composite.js
@@ -537,7 +537,7 @@ describe('CompositeComponent', function() {
     let container = createNodeElement('div');
     class Child extends Component {
       componentDidMount() {
-        expect(container.childNodes[0].tagName).toBe('DIV');
+        expect(container.childNodes[0].childNodes[0].childNodes[0].tagName).toBe('DIV');
       }
       render() {
         return <div />;
@@ -545,11 +545,11 @@ describe('CompositeComponent', function() {
     }
     class App extends Component {
       render() {
-        return <Child />;
+        return <div><Child /></div>;
       }
     }
 
-    const instance = render(<App />, container);
+    const instance = render(<div><App /></div>, container);
     jest.runAllTimers();
     expect(container.childNodes[0].tagName).toBe('DIV');
   });
