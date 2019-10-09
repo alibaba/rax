f59990f25bf468fa5e66bd853e417a0a283240c5
水澜 <shuilan.cj@taobao.com>
2019-10-09 08:59:37 +0800

    refactor: remove dead code

--- packages/rax-server-renderer/src/__tests__/basic.js
+++ packages/rax-server-renderer/src/__tests__/basic.js
@@ -1,6 +1,6 @@
 /* @jsx createElement */
 
-import {createElement, useState, useEffect, createContext, useContext, useReducer} from 'rax';
+import {createElement} from 'rax';
 import {renderToString} from '../index';
 
 describe('renderToString', () => {
