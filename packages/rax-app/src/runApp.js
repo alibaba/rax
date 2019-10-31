468cbf02174b7d7d1f7104f08802010182aa7184
yangfan <18767120422@163.com>
2019-10-31 17:20:05 +0800

    feat: remove preload props

--- packages/rax-app/src/runApp.js
+++ packages/rax-app/src/runApp.js
@@ -107,12 +107,6 @@ export default function runApp(config) {
     .then((initialComponent) => {
       _initialComponent = initialComponent;
       let appInstance = createElement(App, {
-        preload: () => {
-          // Only works in Web project for now.
-        },
-        prerender: () => {
-          // Only works in Web project for now.
-        },
         history,
         routes,
         InitialComponent: _initialComponent
