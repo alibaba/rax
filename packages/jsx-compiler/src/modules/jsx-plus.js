e9aac38239d1c5577ee3ff75dc267f7f3e4f91d4
huya.nzb <huya.nzb@taobao.com>
2019-11-14 19:08:39 +0800

    feat: 支持 x-for 里函数 filter 功能

--- packages/jsx-compiler/src/modules/jsx-plus.js
+++ packages/jsx-compiler/src/modules/jsx-plus.js
@@ -265,6 +265,7 @@ function transformComponentFragment(ast) {
 function transformListJSXElement(path, adapter) {
   const { node } = path;
   const { attributes } = node.openingElement;
+  const filters = [];
   if (node.__jsxlist && !node.__jsxlist.generated) {
     const { args, iterValue } = node.__jsxlist;
     path.traverse({
@@ -278,6 +279,17 @@ function transformListJSXElement(path, adapter) {
               item: args[0].name,
               parentList: node.__jsxlist
             };
+          }
+          // <View x-for={items} data-item={setDataset(item)}>
+          //   <Text class={classnames({ selected: index > 0 })}>{parse(item, index)}</Text>
+          // </View>
+          const containerPath = innerPath.findParent(p => p.isJSXExpressionContainer());
+          if (containerPath && t.isCallExpression(containerPath.node.expression)) {
+            containerPath.node.expression.__listItemFilter = {
+              item: args[0].name, // item
+              filter: '_f' + filters.length // _f0
+            };
+            filters.push(containerPath.node.expression);
           }
         }
       }
@@ -299,6 +311,20 @@ function transformListJSXElement(path, adapter) {
       const skipIds = node.skipIds = node.skipIds || new Map();
       skipIds.set(arg.name, true);
     });
+    if (filters.length) {
+      // return {
+      //   item: item,
+      //   index: index,
+      //   "_f0": setDataset(item),
+      //   "_f1": classnames({ selected: index > 0 })
+      //   "_f2": parse(item, index)
+      // }
+      const loopBody = node.__jsxlist.loopFnBody.body;
+      const properties = loopBody[loopBody.length - 1].argument.properties;
+      filters.forEach(function(f, i) {
+        properties.push(t.objectProperty(t.identifier('_f' + i), f));
+      });
+    }
 
     node.__jsxlist.generated = true;
   }
