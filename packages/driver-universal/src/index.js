3f4e179fc00cd5e008f2f22c9cc225bc274696f4
卓凌 <zhuoling.lcl@alibaba-inc.com>
2019-11-28 15:18:19 +0800

    chore: lazy exec kraken create

--- packages/driver-universal/src/index.js
+++ packages/driver-universal/src/index.js
@@ -1,7 +1,7 @@
 import { isWeex, isWeb, isKraken } from 'universal-env';
 import DriverDOM from './dom';
 import DriverWeex from './weex';
-import DriverKraken from './kraken';
+import createDriverKraken from 'driver-kraken';
 
 let DriverUniversal;
 if (isWeex) {
@@ -9,7 +9,7 @@ if (isWeex) {
 } else if (isWeb) {
   DriverUniversal = DriverDOM;
 } else if (isKraken) {
-  DriverUniversal = DriverKraken;
+  DriverUniversal = DcreateDriverKraken();
 } else {
   throw new Error('Your environment not supported by driver-universal.');
 }
--- packages/driver-universal/src/kraken.js
+++
@@ -1,5 +0,0 @@
-import createDriverKraken from 'driver-kraken';
-
-const driverKraken = createDriverKraken();
-
-export default driverKraken;
