932a1e1bbf9780322c07532a584ac18cb9800ff8
元彦 <yuanyan.cao@alibaba-inc.com>
2019-12-01 16:10:36 +0800

    refactor: dirvers

--- packages/rax-dom/src/index.js
+++ packages/rax-dom/src/index.js
@@ -5,11 +5,37 @@ import unmountComponentAtNode from 'rax-unmount-component-at-node';
 import findDOMNode from 'rax-find-dom-node';
 import createPortal from 'rax-create-portal';
 
+function normalizeEventName(node, eventName, props) {
+  const tagName = node.tagName.toLowerCase();
+
+  if (
+    eventName === 'change' &&
+    (tagName === 'textarea' ||
+      tagName === 'input' && (!props.type || props.type === 'text' || props.type === 'password'))
+  ) {
+    eventName = 'input';
+  } else if (eventName === 'doubleclick') {
+    eventName = 'dblclick';
+  }
+  return eventName;
+}
+
+const driver = Object.assign({}, DriverDOM, {
+  addEventListener(node, eventName, eventHandler, props) {
+    eventName = normalizeEventName(node, eventName, props);
+    return DriverDOM.addEventListener(node, eventName, eventHandler, props);
+  },
+  removeEventListener(node, eventName, eventHandler, props) {
+    eventName = normalizeEventName(node, eventName, props);
+    return DriverDOM.removeEventListener(node, eventName, eventHandler, props);
+  },
+});
+
 const domRender = (element, container, callback) => render(
   element,
   container,
   {
-    driver: DriverDOM,
+    driver,
   },
   callback,
 );
