7b85a38f5f830ffaa7951f3ff94c65d4d90284c1
水澜 <shuilan.cj@taobao.com>
2019-12-04 10:11:09 +0800

    refactor: pass document data by context

--- packages/rax-document/src/index.js
+++ packages/rax-document/src/index.js
@@ -1,62 +1,43 @@
-import { createElement, createContext, useContext } from 'rax';
+import { createElement } from 'rax';
 
-const DocumentContext = createContext({});
+function Root(props, context) {
+  const { __initialHtml } = context;
 
-function Document(props) {
-  return (
-    <DocumentContext.Provider value={props}>
-      {this.props.children}
-    </DocumentContext.Provider>
-  );
-}
-
-function Root() {
-  const context = useContext(DocumentContext) || {};
-
-  const { initialHtml } = context;
-
-  if (initialHtml) {
-    return <div id="root" dangerouslySetInnerHTML={{ __html: initialHtml || '' }} />;
+  if (__initialHtml) {
+    return <div id="root" dangerouslySetInnerHTML={{ __html: __initialHtml || '' }} />;
   }
 
   return <div id="root" />;
 }
 
-function Data() {
-  const context = useContext(DocumentContext) || {};
+function Data(props, context) {
+  const { __initialData } = context;
 
-  const { initialData } = context;
-
-  if (initialData) {
-    return <script data-from="server" dangerouslySetInnerHTML={{__html: 'window.__INITIAL_DATA__=' + initialData}} />;
+  if (__initialData) {
+    return <script data-from="server" dangerouslySetInnerHTML={{__html: 'window.__INITIAL_DATA__=' + __initialData}} />;
   }
 
   return null;
 }
 
 // Named by role rather than implementationm, so component name are `Style` rather than `Styles`.
-function Style() {
-  const context = useContext(DocumentContext) || {};
+function Style(props, context) {
+  const { __styles = [] } = context;
 
-  const { publicPath, styles = [] } = context;
-
-  return styles.map((src, index) => <link rel="stylesheet" href={`${publicPath}${src}`} key={`style_${index}`} />);
+  return __styles.map((src, index) => <link rel="stylesheet" href={src} key={`style_${index}`} />);
 }
 
-function Script() {
-  const context = useContext(DocumentContext) || {};
+function Script(props, context) {
+  const { __scripts = [] } = context;
 
-  const { publicPath, scripts = [] } = context;
-
-  return scripts.map(
-    (src, index) => <script src={`${publicPath}${src}`} key={`script_${index}`}>
+  return __scripts.map(
+    (src, index) => <script src={src} key={`script_${index}`}>
       {/* self-closing script element will not work in HTML */}
     </script>
   );
 }
 
 export {
-  Document,
   Root,
   Data,
   Style,
