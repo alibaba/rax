54b9d7e27f5a487156c9363d22272a5d0e6cfdef
ChrisCindy <yxy0919@foxmail.com>
2019-12-24 20:43:39 +0800

    docs(miniapp): add comments

--- packages/driver-miniapp/src/index.js
+++ packages/driver-miniapp/src/index.js
@@ -1,5 +1,6 @@
 import * as DriverDOM from 'driver-dom';
 
+// Convert Unitless To Rpx defaultly
 export default Object.assign({}, DriverDOM, {
   createElement(type, props, component) {
     return DriverDOM.createElement(type, props, component, true);
