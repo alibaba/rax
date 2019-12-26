7e47418bcc8cd80e8b3d2b19f3a43f4192613b1e
ChrisCindy <yxy0919@foxmail.com>
2019-12-26 15:28:36 +0800

    chore(miniapp): adjust variable name

--- packages/driver-universal/src/index.js
+++ packages/driver-universal/src/index.js
@@ -1,18 +1,18 @@
 import { isWeex, isWeb, isKraken, isMiniApp, isWeChatMiniProgram } from 'universal-env';
 import createDOMDriver from './dom';
-import * as DriverMiniApp from 'driver-miniapp';
-import * as DriverWeex from 'driver-weex';
+import * as MiniAppDriver from 'driver-miniapp';
+import * as WeexDriver from 'driver-weex';
 import createKrakenDriver from 'driver-kraken';
 
 let currentDriver;
 if (isWeex) {
-  currentDriver = DriverWeex;
+  currentDriver = WeexDriver;
 } else if (isWeb) {
   currentDriver = createDOMDriver();
 } else if (isKraken) {
   currentDriver = createKrakenDriver();
 } else if (isMiniApp || isWeChatMiniProgram) {
-  currentDriver = DriverMiniApp;
+  currentDriver = MiniAppDriver;
 } else {
   throw new Error('Your environment was not supported by driver-universal.');
 }
