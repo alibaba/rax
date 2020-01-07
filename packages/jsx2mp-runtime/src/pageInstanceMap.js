65c4f9fd71a1703232bd7b8d6d6f28b97949c40e
ChrisCindy <yxy0919@foxmail.com>
2020-01-07 21:27:09 +0800

    feat(jsx2mp): refactor page lifecycles

---
+++ packages/jsx2mp-runtime/src/pageInstanceMap.js
@@ -0,0 +1,17 @@
+const pageInstanceMap = {
+  // pageId -> rax page instance
+}
+
+
+export function setPageInstance(pageInstance) {
+  const pageId = pageInstance.instanceId;
+  pageInstanceMap[pageId] = pageInstance;
+}
+
+
+export function getPageInstance(pageId) {
+  if (pageInstanceMap.hasOwnProperty(pageId)) {
+    return pageInstanceMap[pageId];
+  }
+  return null;
+}
