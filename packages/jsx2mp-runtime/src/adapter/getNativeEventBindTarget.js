85b763194da6ff17582131854b610567e1251eb8
fushen <fushen.jzw@alibaba-inc.com>
2020-02-03 20:46:25 +0800

    refactor: native event bind

--- packages/jsx2mp-runtime/src/adapter/getNativeEventBindTarget.js
+++ packages/jsx2mp-runtime/src/adapter/getNativeEventBindTarget.js
@@ -1,10 +1,10 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { isMiniApp } from 'universal-env';
 
-export default function(Klass) {
+export default function(Klass, isShareAppMessage) {
   // For alibaba miniapp
   if (isMiniApp) {
-    return Klass.__config.events;
+    return isShareAppMessage ? Klass.__config : Klass.__config.events;
   } else {
     return Klass.__config;
   }
--- packages/jsx2mp-runtime/src/nativeEventListener.js
+++ packages/jsx2mp-runtime/src/nativeEventListener.js
@@ -1,29 +1,48 @@
 import { getMiniAppHistory } from './history';
-import { getPageInstance } from './pageInstanceMap';
+import { getPageInstanceById } from './pageInstanceMap';
 import getNativeEventBindTarget from './adapter/getNativeEventBindTarget';
+import { ON_SHARE_APP_MESSAGE } from './cycles';
 
 export function registerEventsInConfig(Klass, events = []) {
-  const eventBindTarget = getNativeEventBindTarget(Klass);
   if (!Klass.prototype.__nativeEventQueue) {
     Klass.prototype.__nativeEventQueue = {};
   }
   events.forEach(eventName => {
+    const isShareAppMessage = eventName === ON_SHARE_APP_MESSAGE;
+    const eventBindTarget = getNativeEventBindTarget(Klass, isShareAppMessage);
     eventBindTarget[eventName] = function(...args) {
-      Klass.prototype.__nativeEventQueue[eventName].forEach(callback => callback(...args));
+      // onShareAppMessage need receive callback execute return
+      let ret;
+      if (Klass.prototype.__nativeEventQueue[eventName]) {
+        Klass.prototype.__nativeEventQueue[eventName].forEach(callback => ret = callback(...args));
+      }
+      return ret;
     };
   });
 }
 
 export function registerNativeEventListeners(component, events) {
   component.__nativeEvents = events;
+  return component;
 }
 
 export function addNativeEventListener(eventName, callback) {
-  const history = getMiniAppHistory();
-  const pageId = history.location._pageId;
-  const pageInstance = getPageInstance(pageId);
+  const pageInstance = getPageInstance();
   if (!pageInstance.__proto__.__nativeEventQueue[eventName]) {
     pageInstance.__proto__.__nativeEventQueue[eventName] = [];
   }
   pageInstance.__proto__.__nativeEventQueue[eventName].push(callback);
+}
+
+export function removeNativeEventListener(eventName, callback) {
+  const pageInstance = getPageInstance();
+  if (pageInstance.__proto__.__nativeEventQueue[eventName]) {
+    pageInstance.__proto__.__nativeEventQueue[eventName] = pageInstance.__proto__.__nativeEventQueue[eventName].filter(fn => fn !== callback);
+  }
+}
+
+function getPageInstance() {
+  const history = getMiniAppHistory();
+  const pageId = history.location._pageId;
+  return getPageInstanceById(pageId);
 }
