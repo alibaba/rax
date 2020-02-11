5328efd03eb3fca4d3de4110bcafc8f0f270b7b3
水澜 <shuilan.cj@taobao.com>
2020-02-11 15:41:48 +0800

    ci: update eslint-config-rax version

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,22 +2,5 @@ module.exports = {
   'extends': [
     'rax'
   ],
-  'root': true,
-  'plugins': ['rax'],
-  'rules': {
-    'rax/no-implicit-dependencies': ['error', {
-      'peerDependencies': true,
-      'devDependencies': [
-        '**/scripts/*.js',
-        '**/scripts/**/*.js',
-        '**/__tests__/*.js',
-        '**/__tests__/**/*.js',
-        '**/*.config.js',
-        '**/config/*.js',
-        '**/*.conf.js',
-        '**/tests/*.test.js',
-        '**/demo/**'
-      ]
-    }]
-  }
+  'root': true
 };
