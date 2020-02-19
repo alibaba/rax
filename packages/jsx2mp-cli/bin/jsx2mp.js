fae2dbaedafc22caaea2f0dfc809eb31f4a4ee78
jamesgeorge007 <jamesgeorge998001@gmail.com>
2020-02-19 11:38:02 +0530

    feat: warn for unknown commands

--- packages/jsx2mp-cli/bin/jsx2mp.js
+++ packages/jsx2mp-cli/bin/jsx2mp.js
@@ -9,4 +9,11 @@ program
   .usage('<command> [options]')
   .command('start', 'Start a dev watch mode (hot-reload) to transform project.')
   .command('build', 'Build project in production mode')
+  .on('command:*', (cmd) => {
+    if (!program.commands.find(c => c._name == cmd[0])) {
+      console.error(`Invalid command: ${program.args.join(' ')}\n`);
+      program.outputHelp();
+      process.exit(1);
+    }
+  })
   .parse(process.argv);
