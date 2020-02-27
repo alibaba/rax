ba50eaf224efb348c4797467d66735db7f9cc1a8
狒狒神 <350740249@qq.com>
2020-02-27 13:37:37 +0800

    feat(jsx2mp): support memo&useMemo&useCallback (#1748)
    * feat: support memo
    
    * chore: bump version
    
    * chore: use isPlainObject

--- packages/jsx2mp-runtime/rollup.config.js
+++ packages/jsx2mp-runtime/rollup.config.js
@@ -1,6 +1,7 @@
 import babel from 'rollup-plugin-babel';
 import replace from 'rollup-plugin-replace';
 import filesize from 'rollup-plugin-filesize';
+import cleanup from 'rollup-plugin-cleanup';
 import { name, version, author } from './package.json';
 
 function getPropsIdentifierName(platform) {
@@ -60,6 +61,7 @@ function getRollupConfig(platform) {
       }),
       babel(getBabelConfig({ platform })),
       filesize(),
+      cleanup()
     ],
   };
 }
