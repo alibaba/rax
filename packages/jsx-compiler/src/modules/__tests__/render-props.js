7feccc75650eccac569bf13de076954a2ff92d55
ChrisCindy <yxy0919@foxmail.com>
2020-04-30 11:54:03 +0800

    test(jsx2mp): finish render props test case

--- packages/jsx-compiler/src/modules/__tests__/render-props.js
+++ packages/jsx-compiler/src/modules/__tests__/render-props.js
@@ -24,11 +24,18 @@ describe('Pass render props', () => {
     const renderFnPath = getDefaultComponentFunctionPath(ast);
     const returnPath = getReturnElementPath(ast);
     const targetAST = transformReturnPath(returnPath);
-    const { renderPropsFunctions } = _transformRenderPropsFunction(targetAST, renderFnPath);
+    const { renderPropsFunctions, renderPropsListener } = _transformRenderPropsFunction(targetAST, renderFnPath);
+    const [renderClosureFunction, callOnRenderPropsUpdate] = renderPropsListener[0];
     expect(renderPropsFunctions.map(fn => ({
       name: fn.name,
       originName: fn.originName
     }))).toEqual([{'name': 'renderCatState__temp0', 'originName': 'renderCat0'}]);
+    expect(genExpression(renderClosureFunction)).toEqual(`const renderCat0Closure = mouse => ({
+  mouse: mouse
+});`);
+    expect(genExpression(callOnRenderPropsUpdate)).toEqual(`this._onRenderPropsUpdate("renderCat0", e => {
+  this._renderCat0 = e;
+});`);
 
     expect(genExpression(targetAST)).toEqual(`<block a:if="{{$ready}}"><template name="renderCat0"><Cat mouse={mouse} /></template><View>
           <Mouse><view slot="cat"><template is="renderCat0" data="{{...renderCatState__temp0, __tagId: __tagId}}"></template></view></Mouse>
@@ -51,11 +58,20 @@ describe('Pass render props', () => {
     const renderFnPath = getDefaultComponentFunctionPath(ast);
     const returnPath = getReturnElementPath(ast);
     const targetAST = transformReturnPath(returnPath);
-    const { renderPropsFunctions } = _transformRenderPropsFunction(targetAST, renderFnPath);
+    const { renderPropsFunctions, renderPropsListener } = _transformRenderPropsFunction(targetAST, renderFnPath);
+    const [renderClosureFunction, callOnRenderPropsUpdate] = renderPropsListener[0];
     expect(renderPropsFunctions.map(fn => ({
       name: fn.name,
       originName: fn.originName
     }))).toEqual([{'name': 'renderCatState__temp0', 'originName': 'renderCat0'}]);
+    expect(genExpression(renderClosureFunction)).toEqual(`const renderCat0Closure = mouse => {
+  return {
+    mouse: mouse
+  };
+};`);
+    expect(genExpression(callOnRenderPropsUpdate)).toEqual(`this._onRenderPropsUpdate("renderCat0", e => {
+  this._renderCat0 = e;
+});`);
 
     expect(genExpression(targetAST)).toEqual(`<block a:if="{{$ready}}"><template name="renderCat0"><Cat mouse={mouse} /></template><View>
           <Mouse><view slot="cat"><template is="renderCat0" data="{{...renderCatState__temp0, __tagId: __tagId}}"></template></view></Mouse>
@@ -79,38 +95,14 @@ describe('Get render props', () => {
     const renderFnPath = getDefaultComponentFunctionPath(ast);
     const returnPath = getReturnElementPath(ast);
     const targetAST = transformReturnPath(returnPath);
-    const { renderPropsFunctions } = _transformRenderPropsFunction(targetAST, renderFnPath);
+    const { renderPropsEmitter } = _transformRenderPropsFunction(targetAST, renderFnPath);
+    expect(genExpression(renderPropsEmitter[0])).toEqual('this._emitRenderPropsUpdate(\"renderCat0\", name);');
 
     expect(genExpression(targetAST)).toEqual(`<block a:if="{{$ready}}"><View>
         <slot name="cat"></slot>
         </View></block>`);
   });
 
-  it('basic render props usage', () => {
-    const ast = parseCode(`
-    export default function Component() {
-      return (
-        <View>
-          <Mouse renderCat={mouse => (
-            <Cat mouse={mouse} />
-          )} />
-        </View>
-      );
-    }
-    `);
-    const renderFnPath = getDefaultComponentFunctionPath(ast);
-    const returnPath = getReturnElementPath(ast);
-    const targetAST = transformReturnPath(returnPath);
-    const { renderPropsFunctions } = _transformRenderPropsFunction(targetAST, renderFnPath);
-    expect(renderPropsFunctions.map(fn => ({
-      name: fn.name,
-      originName: fn.originName
-    }))).toEqual([{'name': 'renderCatState__temp0', 'originName': 'renderCat0'}]);
-
-    expect(genExpression(targetAST)).toEqual(`<block a:if="{{$ready}}"><template name="renderCat0"><Cat mouse={mouse} /></template><View>
-          <Mouse><view slot="cat"><template is="renderCat0" data="{{...renderCatState__temp0, __tagId: __tagId}}"></template></view></Mouse>
-        </View></block>`);
-  });
 });
 
 function transformReturnPath(path) {
