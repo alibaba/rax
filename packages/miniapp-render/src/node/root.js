f1d8a2aceeda2bb98c18d2b6c4cf6d80506502c8
ChrisCindy <yxy0919@foxmail.com>
2020-05-29 15:45:39 +0800

    feat(miniapp-render): simplify node in root

--- packages/miniapp-render/src/node/root.js
+++ packages/miniapp-render/src/node/root.js
@@ -1,5 +1,37 @@
 import Element from './element';
 
+const leftAttrs = ['id', 'style', 'src', 'tagName', 'textContent', 'dataset', 'attributes', 'nodeId', 'children'];
+function simplify(node) {
+  for (let attr in node) {
+    if (attr.indexOf(leftAttrs) === -1) {
+      delete node.attr;
+    }
+  }
+  if (node.style) {
+    node.style = {
+      ...node.style,
+      $_element: null
+    }
+  }
+}
+
+function traverseTree(node, action) {
+  if (!node || !node.children) {
+    return;
+  }
+  const copiedNode = Object.assign({}, node);
+	const _queue = [];
+	_queue.push(copiedNode);
+	while (_queue.length) {
+		let _curNode = _queue.shift();
+		action(_curNode);
+		if (_curNode.children.length) {
+			_queue = _queue.concat(_curNode.children);
+		}
+  }
+  return copiedNode;
+}
+
 class RootElement extends Element {
   $$init(options, tree) {
     super.$$init(options, tree);
@@ -33,6 +65,10 @@ class RootElement extends Element {
       this.pendingRender = false;
       console.log('this.renderStacks', this.renderStacks);
       // TODO: Process data from array to obj
+      // this.renderStacks: [_path, number, number, Element]
+      const ElementNode = this.renderStacks[3];
+      const simplifiedNode = traverseTree(ElementNode);
+      this.renderStacks[3] = simplifiedNode;
       this.$$trigger('render', { args: this.renderStacks });
       this.renderStacks = [];
     }, 0);
