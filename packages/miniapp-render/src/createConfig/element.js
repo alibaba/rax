264e59fa6c1ffd380f63ebd0b99e0e396b8c34a0
fushen <fushen.jzw@alibaba-inc.com>
2020-06-23 12:08:54 +0800

    chore: root node use custom component

--- packages/miniapp-render/src/createConfig/element.js
+++ packages/miniapp-render/src/createConfig/element.js
@@ -1,17 +1,27 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { isMiniApp } from 'universal-env';
 import createEventProxy from '../bridge/createEventProxy';
 
 export default function() {
-  const config = {
-    properties: {
-      r: {
-        type: Object,
-        value: {}
-      }
-    },
-    options: {
-      styleIsolation: 'shared'
-    },
-    methods: createEventProxy()
-  };
-  return config;
+  if (isMiniApp) {
+    return {
+      props: {
+        r: {}
+      },
+      methods: createEventProxy()
+    };
+  } else {
+    return {
+      properties: {
+        r: {
+          type: Object,
+          value: {}
+        }
+      },
+      options: {
+        styleIsolation: 'shared'
+      },
+      methods: createEventProxy()
+    };
+  }
 }
