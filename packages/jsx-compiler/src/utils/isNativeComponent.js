cc4368e87c123ecd55eb61eebacc251d39e9bdca
ChrisCindy <yxy0919@foxmail.com>
2020-06-28 17:43:32 +0800

    fix(jsx2mp): lint error

--- packages/jsx-compiler/src/utils/isNativeComponent.js
+++ packages/jsx-compiler/src/utils/isNativeComponent.js
@@ -7,5 +7,5 @@ module.exports = function isNativeComponent(path, platform, imported) {
 
   // Compiled rax components and native base components are recognized as native components
   // If a component is not imported, then it is recognized as native base components
-  return !!getCompiledComponents(platform)[tagName] || (imported && !imported[tagName]);
+  return !!getCompiledComponents(platform)[tagName] || imported && !imported[tagName];
 };
