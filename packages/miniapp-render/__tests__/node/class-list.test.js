16bd6387bb786b8033ff9044ad994fe1d31c15d6
NK <yxy0919@foxmail.com>
2020-06-29 10:31:55 +0800

    fix(miniapp): miniapp render test (#1931)
    * test(miniapp): fix test cases
    
    * fix(miniapp): set id/innerHTML/outerHTML
    
    * fix(miniapp): not triggerUpdate when textNodes not have parentNode
    
    * fix(miniapp): lint error

--- packages/miniapp-render/__tests__/node/class-list.test.js
+++ packages/miniapp-render/__tests__/node/class-list.test.js
@@ -8,11 +8,7 @@ beforeAll(() => {
 });
 
 test('class-list', () => {
-  let updateCount = 0;
   const element = document.createElement('div');
-  element.addEventListener('$$domNodeUpdate', () => {
-    updateCount++;
-  });
   const classList = element.classList;
   classList.$$parse('a b c');
 
@@ -27,13 +23,11 @@ test('class-list', () => {
   expect(classList.contains('b')).toBe(true);
   expect(classList.contains('c')).toBe(true);
   expect(classList.contains('d')).toBe(false);
-  expect(updateCount).toBe(1);
 
   // add
   classList.add('d');
   expect(classList.item(3)).toBe('d');
   expect(classList.contains('d')).toBe(true);
-  expect(updateCount).toBe(2);
 
   // remove
   classList.remove('b');
@@ -41,14 +35,12 @@ test('class-list', () => {
   expect(classList.item(2)).toBe('d');
   expect(classList.item(3)).toBe(undefined);
   expect(classList.contains('b')).toBe(false);
-  expect(updateCount).toBe(3);
 
   // toggle
   expect(classList.toggle('c')).toBe(false);
   expect(classList.contains('c')).toBe(false);
   expect(classList.toggle('c')).toBe(true);
   expect(classList.contains('c')).toBe(true);
-  expect(updateCount).toBe(5);
 
   expect(classList.toggle('c', true)).toBe(true);
   expect(classList.contains('c')).toBe(true);
@@ -58,12 +50,10 @@ test('class-list', () => {
   expect(classList.contains('c')).toBe(false);
   expect(classList.toggle('c', true)).toBe(true);
   expect(classList.contains('c')).toBe(true);
-  expect(updateCount).toBe(7);
 
   // toString
   expect(classList.toString()).toBe('a d c');
 
   classList.$$parse('c   b   a dd');
   expect(classList.toString()).toBe('c b a dd');
-  expect(updateCount).toBe(8);
 });
