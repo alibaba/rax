88aecb1639971ba7a138c3eb157a5ea16ff7d194
狒狒神 <350740249@qq.com>
2020-08-03 15:44:01 +0800

    chore: use middleground package instead of old logic (#1969)
    * chore: use middleground package instead old logic
    
    * fix: update package name
    
    * chore: remove test
    
    Co-authored-by: 思忠 <chenbinbin.cb@alibaba-inc.com>

--- packages/rax-app/src/app.js
+++ packages/rax-app/src/app.js
@@ -1,146 +1,71 @@
-import { isMiniAppPlatform, isWeex } from './env';
-import { SHOW, HIDE, ERROR, LAUNCH, NOT_FOUND, SHARE, TAB_ITEM_CLICK, DEFAULT_PATH_NAME } from './constants';
-import { isFunction } from './type';
-import { getHistory } from './history';
-import router from './router';
-import { emit as pageEmit } from './page';
+import { createElement, useState, useEffect, Fragment } from 'rax';
+import { useRouter } from 'rax-use-router';
+import { isWeb } from 'universal-env';
+import { Navigation, TabBar } from 'rax-pwa';
 
-export const appCycles = {};
+const initialDataFromSSR = global.__INITIAL_DATA__;
 
-/**
- * Emit life cycle callback
- * @param {string} cycle cycle name
- * @param {object} context callback's context when executed
- * @param  {...any} args callback params
- */
-export function emit(cycle, context, ...args) {
-  if (appCycles.hasOwnProperty(cycle)) {
-    const cycles = appCycles[cycle];
-    if (cycle === SHARE) {
-      // In MiniApp, it need return callback result as share info, like { title, desc, path }
-      args[0].content = context ? cycles[0].call(context, args[1]) : cycles[0](args[1]);
-    } else {
-      cycles.forEach(cycle => {
-        context ? cycle.apply(context, args) : cycle(...args);
+function _isNullableComponent(component) {
+  return !component || Array.isArray(component) && component.length === 0;
+}
+
+export default function App(props) {
+  const { appConfig, history, routes, pageProps, InitialComponent } = props;
+  const { component } = useRouter(() => ({ history, routes, InitialComponent }));
+
+  if (_isNullableComponent(component)) {
+    // Return null directly if not matched.
+    return null;
+  } else {
+    const [pageInitialProps, setPageInitialProps] = useState(
+      // If SSR is enabled, set pageInitialProps: {pagePath: pageData}
+      initialDataFromSSR ? { [initialDataFromSSR.pagePath || '']: initialDataFromSSR.pageData || {} } : {}
+    );
+
+    // If SSR is enabled, process getInitialProps method
+    if (isWeb && initialDataFromSSR && component.getInitialProps && !pageInitialProps[component.__path]) {
+      useEffect(() => {
+        const getInitialPropsPromise = component.getInitialProps();
+
+        // Check getInitialProps returns promise.
+        if (process.env.NODE_ENV !== 'production') {
+          if (!getInitialPropsPromise.then) {
+            throw new Error('getInitialProps should be async function or return a promise. See detail at "' + component.name + '".');
+          }
+        }
+
+        getInitialPropsPromise.then((nextDefaultProps) => {
+          if (nextDefaultProps) {
+            // Process pageData from SSR
+            const pageData = initialDataFromSSR && initialDataFromSSR.pagePath === component.__path ? initialDataFromSSR.pageData : {};
+            // Do not cache getInitialPropsPromise result
+            setPageInitialProps(Object.assign({}, { [component.__path]: Object.assign({}, pageData, nextDefaultProps) }));
+          }
+        }).catch((error) => {
+          // In case of uncaught promise.
+          throw error;
+        });
       });
+      // Early return null if initialProps were not get.
+      return null;
     }
-  }
-}
 
-/**
- * Add app lifecycle callback
- * @param {string} cycle cycle name
- * @param {function} callback cycle callback
- */
-export function addAppLifeCycle(cycle, callback) {
-  if (isFunction(callback)) {
-    const cycles = appCycles[cycle] = appCycles[cycle] || [];
-    cycles.push(callback);
-  }
-}
-
-// All of the following hooks will be removed when the future break change
-export function useAppLaunch(callback) {
-  addAppLifeCycle(LAUNCH, callback);
-}
-
-export function useAppShow(callback) {
-  addAppLifeCycle(SHOW, callback);
-}
-
-export function useAppHide(callback) {
-  addAppLifeCycle(HIDE, callback);
-}
-
-export function useAppError(callback) {
-  addAppLifeCycle(ERROR, callback);
-}
-
-export function usePageNotFound(callback) {
-  addAppLifeCycle(NOT_FOUND, callback);
-}
-
-export function useAppShare(callback) {
-  addAppLifeCycle('appshare', callback);
-}
-
-// Emit MiniApp App lifeCycles
-if (isMiniAppPlatform) {
-  window.addEventListener(LAUNCH, ({ options, context }) => {
-    emit(LAUNCH, context, options);
-  });
-  window.addEventListener('appshow', ({ options, context }) => {
-    emit(SHOW, context, options);
-  });
-  window.addEventListener('apphide', ({ context }) => {
-    emit(HIDE, context);
-  });
-  window.addEventListener('apperror', ({ context, error }) => {
-    emit(ERROR, context, error);
-  });
-  window.addEventListener('pagenotfound', ({ context }) => {
-    emit(NOT_FOUND, context);
-  });
-  window.addEventListener('appshare', ({ context, shareInfo, options }) => {
-    emit(SHARE, context, shareInfo, options);
-  });
-  window.addEventListener('tabitemclick', ({ options, context }) => {
-    emit(TAB_ITEM_CLICK, context, options);
-  });
-} else if (isWeex) {
-  try {
-    // https://weex.apache.org/docs/modules/globalEvent.html#addeventlistener
-    // Use __weex_require__ in Rax project.
-    const globalEvent = __weex_require__('@weex-module/globalEvent');
-    globalEvent.addEventListener('WXApplicationDidBecomeActiveEvent', function() {
-      // /index as default pathname
-      let pathname = DEFAULT_PATH_NAME;
-      if (router.current) {
-        router.current.visibiltyState = true;
-        pathname = router.current.pathname;
-      }
-      // Emit app show
-      emit(SHOW);
-      // Emit page show
-      pageEmit(SHOW, pathname);
-    });
-    globalEvent.addEventListener('WXApplicationWillResignActiveEvent', function() {
-      // /index as default pathname
-      let pathname = DEFAULT_PATH_NAME;
-      if (router.current) {
-        router.current.visibiltyState = false;
-        pathname = router.current.pathname;
-      }
-      // Emit app hide
-      emit(HIDE);
-      // Emit page hide
-      pageEmit(HIDE, pathname);
-    });
-  } catch (err) {
-    console.log('@weex-module/globalEvent error: ' + err);
-  }
-} else if (typeof document !== undefined && typeof window !== undefined) {
-  document.addEventListener('visibilitychange', function() {
-    // Get history
-    const history = getHistory();
-    // The app switches from foreground to background
-    if (router.current && history.location.pathname === router.current.pathname) {
-      router.current.visibiltyState = !router.current.visibiltyState;
-      if (router.current.visibiltyState) {
-        // Emit app show
-        emit(SHOW);
-        // Emit page show
-        pageEmit(SHOW, router.current.pathname);
-      } else {
-        // Emit app hide
-        emit(HIDE);
-        // Emit page hide
-        pageEmit(HIDE, router.current.pathname);
-      }
+    if (isWeb) {
+      return createElement(
+        Navigation,
+        Object.assign(
+          { appConfig, component, history, location: history.location, routes, InitialComponent },
+          pageInitialProps[component.__path],
+          pageProps
+        )
+      );
     }
-  });
-  // Emit error lifeCycles
-  window.addEventListener('error', event => {
-    emit(ERROR, null, event.error);
-  });
+
+    return createElement(
+      Fragment,
+      {},
+      createElement(component, Object.assign({ history, location: history.location, routes, InitialComponent }, pageInitialProps[component.__path], pageProps)),
+      createElement(TabBar, { history, config: appConfig.tabBar })
+    );
+  }
 }
--- packages/rax-app/src/index.js
+++ packages/rax-app/src/index.js
@@ -1,35 +1,104 @@
-import router from './router';
-import {
-  useAppLaunch,
-  useAppError,
-  useAppHide,
-  useAppShare,
-  useAppShow,
-  usePageNotFound
-} from './app';
-import { usePageHide, usePageShow, withPageLifeCycle } from './page';
-import runApp from './runApp';
-import {
-  registerNativeEventListeners,
-  addNativeEventListener,
-  removeNativeEventListener
-} from './nativeEventListener';
+import { createElement, useState, useEffect, Fragment } from 'rax';
+import { withRouter as raxWithRouter } from 'rax-use-router';
+import createShareAPI from 'create-app-shared';
+import raxRenderer from 'rax-renderer';
+import miniappRenderer from 'miniapp-renderer';
+import { isWeChatMiniProgram, isMiniApp, isByteDanceMicroApp, isWeb } from 'universal-env';
+import App from './App';
+import { enhanceAppLifeCycle, useAppLaunch, useAppShare, useAppError, useAppShow, useAppHide, usePageNotFound } from './appLifyCycles';
 
-const withRouter = router.withRouter;
+const initialDataFromSSR = global.__INITIAL_DATA__;
 
-export {
-  runApp,
+const {
+  getHistory,
+  createHistory,
   withRouter,
-  useAppLaunch,
-  useAppError,
-  useAppHide,
-  useAppShare,
-  useAppShow,
-  usePageNotFound,
+  withPageLifeCycle,
   usePageHide,
   usePageShow,
-  withPageLifeCycle,
+  collectAppLifeCycle,
+  emitLifeCycles,
   registerNativeEventListeners,
   addNativeEventListener,
-  removeNativeEventListener
+  removeNativeEventListener,
+  pathRedirect
+} = createShareAPI({
+  withRouter: raxWithRouter,
+  createElement,
+  useEffect
+});
+
+function runApp(staticConfig, dynamicConfig = {}) {
+  let renderer = raxRenderer;
+  let createAppInstance;
+  enhanceAppLifeCycle(dynamicConfig);
+  const appConfig = {
+    app: Object.assign({
+      rootId: 'root',
+    }, dynamicConfig),
+    router: {}
+  };
+  const { shell, routes } = staticConfig;
+  if (isWeChatMiniProgram, isMiniApp, isByteDanceMicroApp) {
+    renderer = miniappRenderer;
+  } else {
+    createAppInstance = (initialComponent) => {
+      const history = getHistory();
+      let appInstance = createElement(App, {
+        appConfig: staticConfig,
+        history,
+        routes,
+        pageProps: !dynamicConfig.dynamicConfig && dynamicConfig,
+        InitialComponent: initialComponent
+      });
+
+      if (shell) {
+        const shellData = initialDataFromSSR ? initialDataFromSSR.shellData : null;
+        appInstance = createElement(shell.component, { data: shellData }, appInstance);
+      }
+
+      return appInstance;
+    };
+  }
+
+  renderer({
+    createBaseApp: () => {
+      let type;
+      if (initialDataFromSSR) {
+        type = 'browser';
+      } else if (isWeb) {
+        type = 'hash';
+      }
+      const history = createHistory({ routes, customHistory: staticConfig.history, type });
+      appConfig.router.history = history;
+      collectAppLifeCycle(appConfig);
+      return {
+        appConfig
+      };
+    },
+    createHistory,
+    staticConfig,
+    appConfig,
+    getHistory,
+    pathRedirect,
+    createAppInstance,
+    emitLifeCycles
+  });
+}
+
+export {
+  withRouter,
+  withPageLifeCycle,
+  usePageHide,
+  usePageShow,
+  registerNativeEventListeners,
+  addNativeEventListener,
+  removeNativeEventListener,
+  runApp,
+  useAppLaunch,
+  useAppShare,
+  useAppError,
+  useAppShow,
+  useAppHide,
+  usePageNotFound
 };
