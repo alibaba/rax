2e6eaf4a09a47877ed9be76dec8fa51033077ac3
逆葵 <xianyong.yxy@alibaba-inc.com>
2020-08-18 21:23:29 +0800

    docs(miniapp): add multiple demos

--- examples/hello-world/src/index.jsx
+++
@@ -1,5 +0,0 @@
-import {createElement, render} from 'rax';
-import DomDriver from 'driver-dom';
-import Hello from './Hello';
-
-render(<Hello name="world" />, document.body, { driver: DomDriver });
---
+++ examples/with-miniapp-plugin-component/ali-miniapp-plugin/index.js
@@ -0,0 +1,6 @@
+var data = require('./api/data')
+
+module.exports = {
+  getData: data.getData,
+  setData: data.setData
+}
