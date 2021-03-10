b09bac3cbb8aad81a1af8bf8aa50781ab4ed1fd4
songzheng <bill.sz@alibaba-inc.com>
2021-03-10 12:32:30 +0800

    feat: jsx-runtime

---
+++ packages/rax-jsx-runtime/src/__test__/jsx-runtime.test.js
@@ -0,0 +1,94 @@
+import { Component, createElement, createRef } from 'rax';
+import { jsx, jsxs, jsxDEV, Fragment } from '../'
+
+describe('Support JSX-Runtime', () => {
+
+	it('should export all modules needed by importSource', () => {
+		expect(typeof jsx).toBe('function');
+		expect(typeof jsxs).toBe('function');
+		expect(typeof jsxDEV).toBe('function');
+		expect(typeof Fragment).toBe('function');
+	});
+
+	it('should add key', () => {
+		const vnode = jsx('div', null, 'foo');
+		expect(vnode.key).toBe('foo');
+	});
+
+  it('should keep ref in props', () => {
+    let ref = () => null;
+    let vnode = jsx('div', { ref });
+    expect(vnode.ref).toBe(ref);
+
+    ref = 'fooRef';
+    vnode = jsx('div', { ref });
+    expect(vnode.ref).toBe('fooRef');
+
+  });
+
+	it('should apply defaultProps', () => {
+		class Foo extends Component {
+			render() {
+				return <div />;
+			}
+		}
+
+		Foo.defaultProps = {
+			foo: 'bar',
+      fake: 'bar1',
+		};
+
+		let vnode = jsx(Foo, {}, null);
+		expect(vnode.props.foo).toBe('bar');
+
+    vnode = jsx(Foo, {fake: 'bar2'}, null);
+    expect(vnode.props.foo1).toBe('bar2');
+	});
+
+	it('should keep props over defaultProps', () => {
+		class Foo extends Component {
+			render() {
+				return <div />;
+			}
+		}
+
+		Foo.defaultProps = {
+			foo: 'bar'
+		};
+
+		const vnode = jsx(Foo, { foo: 'baz' }, null);
+		expect(vnode.props).toEqual({
+			foo: 'baz'
+		});
+	});
+
+	it('should set __source and __self', () => {
+		const vnode = jsx('div', { class: 'foo' }, 'key', 'source', 'self');
+		expect(vnode.__source).toBe('source');
+		expect(vnode.__self).toBe('self');
+	});
+
+	it('should return a vnode like createElement', () => {
+		const elementVNode = createElement('div', {
+			class: 'foo',
+			key: 'key'
+		});
+		const jsxVNode = jsx('div', { class: 'foo' }, 'key');
+		delete jsxVNode.__self;
+		delete jsxVNode.__source;
+		expect(jsxVNode).toEqual(elementVNode);
+	});
+
+	it('should remove ref from props', () => {
+		const ref = createRef();
+		const vnode = jsx('div', { ref }, null);
+		expect(vnode.props).toEqual({});
+		expect(vnode.ref).toBe(ref);
+	});
+
+  it('should receive children props', () => {
+    const fooProps = {children: []};
+    expect(Fragment(fooProps)).toEqual([]);
+  });
+
+});
---
+++ packages/rax-jsx-runtime/src/index.js
@@ -0,0 +1,49 @@
+import { shared } from 'rax';
+
+const { Host } = shared;
+
+function Fragment(props) {
+  return props.children;
+}
+
+function generateVNode(type, props, key, __source, __self) {
+  let normalizedProps = {};
+  let propName;
+
+  // The default value of key and ref of rax element is null
+  let ref = props && props.ref || null;
+  key = key ? key : null;
+
+  for (propName in props) {
+    if (propName !== 'ref') {
+      normalizedProps[propName] = props[propName];
+    }
+  }
+
+  let defaults;
+  if (typeof type === 'function' && (defaults = type.defaultProps)) {
+    for (propName in defaults)
+      if (normalizedProps[propName] === undefined) {
+        normalizedProps[propName] = defaults[propName];
+      }
+  }
+
+  return {
+    type,
+    props: normalizedProps,
+    key,
+    ref,
+    _owner: Host.owner,
+    __source,
+    __self,
+  };
+
+}
+
+export {
+  generateVNode as jsx,
+  generateVNode as jsxs,
+  generateVNode as jsxDEV,
+  Fragment
+};
+
